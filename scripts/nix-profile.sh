84e235eae8581403716a87b37e9dc6210b8ad515
Eelco Dolstra <e.dolstra@tudelft.nl>
2003-05-30 11:49:06 +0000

    * Set MANPATH and PKG_CONFIG_PATH.

--- scripts/nix-profile.sh
+++ scripts/nix-profile.sh
@@ -12,4 +12,8 @@ if test -z "$NIX_SET"; then
 
     export C_INCLUDE_PATH=$NIX_LINKS/include:$C_INCLUDE_PATH
 
+    export PKG_CONFIG_PATH=$NIX_LINKS/lib/pkgconfig:$PKG_CONFIG_PATH
+
+    export MANPATH=$NIX_LINKS/man:$MANPATH
+
 fi
