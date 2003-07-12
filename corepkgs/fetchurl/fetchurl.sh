5304a1eb3a2bbcc379924d3f5a58b64ce77f4849
Eelco Dolstra <e.dolstra@tudelft.nl>
2003-07-12 11:03:14 +0000

    * Fetchurl: check md5 checksum.

--- corepkgs/fetchurl/fetchurl.sh
+++ corepkgs/fetchurl/fetchurl.sh
@@ -1,3 +1,10 @@
 #! /bin/sh
 
-wget "$url" -O "$out"
+echo "downloading $url into $out..."
+wget "$url" -O "$out" || exit 1
+
+actual=$(md5sum -b $out | cut -c1-32)
+if ! test "$actual" == "$md5"; then
+    echo "hash is $actual, expected $md5"
+    exit 1
+fi
