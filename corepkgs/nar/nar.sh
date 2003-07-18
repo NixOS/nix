ab350eafd2c1a98ea98090fdb3bd9b7ae4f7336b
Eelco Dolstra <e.dolstra@tudelft.nl>
2003-07-18 07:42:57 +0000

    * Generate nar.sh, fetchurl.sh.

--- corepkgs/nar/nar.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/sh
-
-echo "packing $path into $out..."
-/nix/bin/nix --dump --file "$path" | bzip2 > $out || exit 1
-
--- corepkgs/nar/unnar.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/sh
-
-echo "unpacking $nar to $out..."
-bunzip2 < $nar | /nix/bin/nix --restore "$out" || exit 1
