2be8b5917a8040fac72e7970e94bbb436e8c35d6
Eelco Dolstra <e.dolstra@tudelft.nl>
2003-11-19 10:04:03 +0000

    * Use `sdftable -s' to get warnings about the grammar. * Several bug fixes in the grammar. * Allow one-line comments (#... and //...) to end in EOF.

--- src/nix-instantiate/parser.cc
+++ src/nix-instantiate/parser.cc
@@ -154,7 +154,7 @@ Expr parseExprFromFile(Path path)
     if (!imploded)
         throw Error(format("cannot implode parse tree"));
 
-    debug(format("imploded parse tree of `%1%': %2%")
+    printMsg(lvlVomit, format("imploded parse tree of `%1%': %2%")
         % path % imploded);
 
     /* Finally, clean it up. */
