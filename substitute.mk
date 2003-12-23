68f2fadb788f8d401fad6fd1db1cfac283e5e337
Eelco Dolstra <e.dolstra@tudelft.nl>
2003-12-23 22:15:12 +0000

    * nix-pull requires libexecdir to be substituted.

--- substitute.mk
+++ substitute.mk
@@ -5,6 +5,7 @@
 	 -e "s^@sysconfdir\@^$(sysconfdir)^g" \
 	 -e "s^@localstatedir\@^$(localstatedir)^g" \
 	 -e "s^@datadir\@^$(datadir)^g" \
+	 -e "s^@libexecdir\@^$(libexecdir)^g" \
 	 -e "s^@system\@^$(system)^g" \
 	 -e "s^@wget\@^$(wget)^g" \
 	 < $< > $@ || rm $@
