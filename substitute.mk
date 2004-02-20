7f0ed370da62b867d90ba5346f4b9f217fbbe10f
Eelco Dolstra <e.dolstra@tudelft.nl>
2004-02-20 11:32:30 +0000

    * Use $(storedir) instead of $(prefix)/store.

--- substitute.mk
+++ substitute.mk
@@ -6,6 +6,7 @@
 	 -e "s^@localstatedir\@^$(localstatedir)^g" \
 	 -e "s^@datadir\@^$(datadir)^g" \
 	 -e "s^@libexecdir\@^$(libexecdir)^g" \
+	 -e "s^@storedir\@^$(storedir)^g" \
 	 -e "s^@system\@^$(system)^g" \
 	 -e "s^@wget\@^$(wget)^g" \
 	 -e "s^@version\@^$(VERSION)^g" \
