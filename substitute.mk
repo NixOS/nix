03f1d1ecb5ef69c8c065c83373f9f9f749995c50
Eelco Dolstra <e.dolstra@tudelft.nl>
2004-04-06 08:18:51 +0000

    * Switched from wget to curl. * Made the dependencies on bzip2 and the shell explicit.

--- substitute.mk
+++ substitute.mk
@@ -8,7 +8,10 @@
 	 -e "s^@libexecdir\@^$(libexecdir)^g" \
 	 -e "s^@storedir\@^$(storedir)^g" \
 	 -e "s^@system\@^$(system)^g" \
-	 -e "s^@wget\@^$(wget)^g" \
+	 -e "s^@shell\@^$(shell)^g" \
+	 -e "s^@curl\@^$(curl)^g" \
+	 -e "s^@bzip2\@^$(bzip2)^g" \
+	 -e "s^@bunzip2\@^$(bunzip2)^g" \
 	 -e "s^@perl\@^$(perl)^g" \
 	 -e "s^@version\@^$(VERSION)^g" \
 	 < $< > $@ || rm $@
