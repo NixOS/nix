5087c8f645cf971ee6dfd11c5c56c6b2b752d7ad
Eelco Dolstra <e.dolstra@tudelft.nl>
2004-05-12 14:30:57 +0000

    * Use `-j0'.

--- tests/parallel.sh
+++ tests/parallel.sh
@@ -2,14 +2,7 @@ storeExpr=$($TOP/src/nix-instantiate/nix-instantiate parallel.nix)
 
 echo "store expr is $storeExpr"
 
-#for i in $(seq 1 5); do
-#    echo "WORKER $i"
-#    $TOP/src/nix-store/nix-store -rvvB "$storeExpr" &
-#done
-
-#sleep 5
-
-outPath=$($TOP/src/nix-store/nix-store -qnfvvvvvvK "$storeExpr")
+outPath=$($TOP/src/nix-store/nix-store -qnfvvvv -j0 "$storeExpr")
 
 echo "output path is $outPath"
 
