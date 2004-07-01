b584253af461b02f70ea3244a10d2569c85f2095
Eelco Dolstra <e.dolstra@tudelft.nl>
2004-07-01 11:11:16 +0000

    * Include some missing headers.

--- src/libstore/normalise.cc
+++ src/libstore/normalise.cc
@@ -7,6 +7,7 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <errno.h>
 
 #include "normalise.hh"
 #include "references.hh"
--- src/nix-env/profiles.cc
+++ src/nix-env/profiles.cc
@@ -3,6 +3,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <errno.h>
 
 
 static bool cmpGensByNumber(const Generation & a, const Generation & b)
