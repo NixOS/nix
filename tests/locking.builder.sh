deb75bb414c6c7a6d1209efd86738fdd147db13d
Eelco Dolstra <e.dolstra@tudelft.nl>
2005-09-13 15:54:36 +0000

    * Remove debugging code.

--- tests/locking.builder.sh
+++ tests/locking.builder.sh
@@ -1,17 +1,8 @@
 export PATH=/bin:/usr/bin:$PATH
 
-env
-
 sleep 3
 
 touch $out
-echo CAT1
-cat $inputs
-echo CAT2
-echo -n $(cat $inputs)$text
-echo CAT3
-
-
 # Use `>>'; without proper locking this will cause text duplication.
 echo -n $(cat $inputs)$text >> $out
 
