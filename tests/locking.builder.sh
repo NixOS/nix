116e939d5731f89821a6777fb2b458508fd85e29
Eelco Dolstra <e.dolstra@tudelft.nl>
2005-09-13 14:07:22 +0000

    * More debugging.

--- tests/locking.builder.sh
+++ tests/locking.builder.sh
@@ -5,6 +5,13 @@ env
 sleep 3
 
 touch $out
+echo CAT1
+cat $inputs
+echo CAT2
+echo -n $(cat $inputs)$text
+echo CAT3
+
+
 # Use `>>'; without proper locking this will cause text duplication.
 echo -n $(cat $inputs)$text >> $out
 
