18bbcb1214caf75c0190e610d7eb34e971366c7c
Eelco Dolstra <e.dolstra@tudelft.nl>
2005-12-11 19:25:48 +0000

    * Add a test to demonstrate the quadratic complexity of referrer   (de)registration, in particular garbage collection (NIX-23).

---
+++ tests/referrers.sh
@@ -0,0 +1,15 @@
+max=5000
+
+reference=$NIX_STORE_DIR/abcdef
+touch $reference
+(echo $reference && echo && echo 0) | $TOP/src/nix-store/nix-store --register-validity 
+
+echo "registering..."
+time for ((n = 0; n < $max; n ++)); do
+    storePath=$NIX_STORE_DIR/$n
+    touch $storePath
+    (echo $storePath && echo && echo 1 && echo $reference)
+done | $TOP/src/nix-store/nix-store --register-validity 
+
+echo "collecting garbage..."
+time $TOP/src/nix-store/nix-store --gc 2> /dev/null
