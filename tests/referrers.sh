5144f750c471cdb629750e96ddc913fb01fb9eef
Eelco Dolstra <e.dolstra@tudelft.nl>
2005-12-15 17:04:02 +0000

    * Typo.

--- tests/referrers.sh
+++ tests/referrers.sh
@@ -5,7 +5,7 @@ touch $reference
 (echo $reference && echo && echo 0) | $TOP/src/nix-store/nix-store --register-validity 
 
 echo "registering..."
-time for ((n = 0; n < $max; n ++)); do
+time for ((n = 0; n < $max; n++)); do
     storePath=$NIX_STORE_DIR/$n
     touch $storePath
     (echo $storePath && echo && echo 1 && echo $reference)
