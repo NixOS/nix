bfe19b3c3728d90cad7575c5d2571d48f0ef0d14
Eelco Dolstra <e.dolstra@tudelft.nl>
2006-08-16 10:23:02 +0000

    * A test for NIX-53.

---
+++ tests/lang/parse-okay-crlf.nix
@@ -0,0 +1,17 @@
+rec {
+
+  /* Dit is
+  een test. */
+
+  x = 
+  # Dit is een test.y;
+  
+  y = 123;
+
+  # CR or CR/LF in strings should be translated to LF (but not
+  # explicit \r's).
+  foo = "multiline
+  string
+  test\r";
+
+  z = 456;}
