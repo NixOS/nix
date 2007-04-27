a46db5d013a5c3ab5b041824bfb935e5c042886c
Eelco Dolstra <e.dolstra@tudelft.nl>
2007-04-27 23:28:44 +0000

    * Package conflict resolution through priority levels.  If there is a   user environment collission between two packages due to overlapping   file names, then a package with a higher priority will overwrite the   symlinks of a package with a lower priority.  E.g.,
        $ nix-env --set-flag priority 5 gcc
        $ nix-env --set-flag priority 10 binutils
    
      gives gcc a higher priority than binutils (higher number = lower
      priority).

--- corepkgs/buildenv/default.nix
+++ corepkgs/buildenv/default.nix
@@ -4,9 +4,11 @@ derivation {
   name = "user-environment";
   system = system;
   builder = ./builder.pl;
-  derivations = derivations;
+  
   manifest = manifest;
 
   # !!! grmbl, need structured data for passing this in a clean way.
+  paths = derivations;
   active = map (x: if x ? meta && x.meta ? active then x.meta.active else "true") derivations;
+  priority = map (x: if x ? meta && x.meta ? priority then x.meta.priority else "5") derivations;
 }
