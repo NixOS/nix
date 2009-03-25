7024a1ef076cedf4596c9f4b107e85d315242cea
Eelco Dolstra <e.dolstra@tudelft.nl>
2009-03-25 16:43:38 +0000

    * Removed the locking.sh test; it's redundant because of the extended   parallel.sh test.  Also, don't call multiple nix-builds in parallel,   since they can race creating .nix-build-tmp-derivation.

--- tests/locking.builder.sh
+++
@@ -1,6 +0,0 @@
-sleep 3
-
-# Use `>>'; without proper locking this will cause text duplication.
-echo -n $(cat $inputs)$text >> $out
-
-sleep 2
--- tests/parallel.sh
+++ tests/parallel.sh
@@ -1,5 +1,6 @@
 source common.sh
 
+
 # First, test that -jN performs builds in parallel.
 echo "testing nix-build -j..."
 
@@ -17,6 +18,7 @@ if test "$text" != "abacade"; then exit 1; fi
 if test "$(cat $SHARED.cur)" != 0; then fail "wrong current process count"; fi
 if test "$(cat $SHARED.max)" != 3; then fail "not enough parallelism"; fi
 
+
 # Second, test that parallel invocations of nix-build perform builds
 # in parallel, and don't block waiting on locks held by the others.
 echo "testing multiple nix-build -j1..."
@@ -25,7 +27,9 @@ clearStore
 
 rm -f $SHARED.cur $SHARED.max
 
-cmd="$nixbuild -j1 parallel.nix --argstr sleepTime 7"
+drvPath=$($nixinstantiate parallel.nix --argstr sleepTime 7)
+
+cmd="$nixstore -j1 -r $drvPath"
 
 $cmd &
 pid1=$!
@@ -39,9 +43,14 @@ $cmd &
 pid3=$!
 echo "pid 3 is $pid3"
 
+$cmd &
+pid4=$!
+echo "pid 4 is $pid4"
+
 wait $pid1 || fail "instance 1 failed: $?"
 wait $pid2 || fail "instance 2 failed: $?"
 wait $pid3 || fail "instance 3 failed: $?"
+wait $pid4 || fail "instance 4 failed: $?"
 
 if test "$(cat $SHARED.cur)" != 0; then fail "wrong current process count"; fi
 if test "$(cat $SHARED.max)" != 3; then fail "not enough parallelism"; fi
