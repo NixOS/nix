02934b12000d5a837ef4cac78025cb531330c2b3
Eelco Dolstra <e.dolstra@tudelft.nl>
2010-10-23 22:55:30 +0000

    * Regression test for listToAttr's behaviour if an attribute name   occurs multiple times.

--- tests/lang/eval-okay-listtoattrs.nix
+++ tests/lang/eval-okay-listtoattrs.nix
@@ -7,4 +7,5 @@ let
   a = builtins.listToAttrs list;
   b = builtins.listToAttrs ( list ++ list );
   r = builtins.listToAttrs [ (asi "result" [ a b ]) ( asi "throw" (throw "this should not be thrown")) ];
-in concat (map (x: x.a) r.result)
+  x = builtins.listToAttrs [ (asi "foo" "bla") (asi "foo" "bar") ];
+in concat (map (x: x.a) r.result) + x.foo
