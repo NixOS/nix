1876ab764f5b6e42fce67bdb46a8b03131653900
Eelco Dolstra <e.dolstra@tudelft.nl>
2011-02-09 14:13:09 +0000

    * A better fix.  $boehmgc isn't set anywhere, we should use the flags   returned by pkg-config.

--- release.nix
+++ release.nix
@@ -75,7 +75,7 @@ let
         configureFlags = ''
           --disable-init-state
           --with-bzip2=${bzip2} --with-sqlite=${sqlite}
-          ${lib.optionalString (system != "i386-sunos") "--enable-gc"}
+          --enable-gc
         '';
       };
 
