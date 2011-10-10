73fe6871c479f7670f8c93b0cc9ef7bb1a851777
Eelco Dolstra <e.dolstra@tudelft.nl>
2011-10-10 18:12:40 +0000

    * Include the Nix Perl bindings in Nix itself.  This will allow the   bindings to be used in Nix's own Perl scripts.
      The only downside is that Perl XS and Automake/libtool don't really
      like each other, so building is a bit tricky.

---
+++ perl/lib/Nix/Store.pm
@@ -0,0 +1,23 @@
+package Nix::Store;
+
+use 5.010001;
+use strict;
+use warnings;
+
+require Exporter;
+
+our @ISA = qw(Exporter);
+
+our %EXPORT_TAGS = ( 'all' => [ qw( ) ] );
+
+our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
+
+our @EXPORT = qw( );
+
+our $VERSION = '0.15';
+
+require XSLoader;
+XSLoader::load('Nix::Store', $VERSION);
+
+1;
+__END__
