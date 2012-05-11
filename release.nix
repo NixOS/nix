37fa47908bff2495755410c96891623d60735464
Eelco Dolstra <eelco.dolstra@logicblox.com>
2012-05-10 22:12:20 -0400

    Build Ubuntu 12.04 packages

--- release.nix
+++ release.nix
@@ -143,6 +143,8 @@ let
     deb_ubuntu1010x86_64 = makeDeb_x86_64 (diskImageFuns: diskImageFuns.ubuntu1010x86_64) 50;
     deb_ubuntu1110i386 = makeDeb_i686 (diskImageFuns: diskImageFuns.ubuntu1110i386) 60;
     deb_ubuntu1110x86_64 = makeDeb_x86_64 (diskImageFuns: diskImageFuns.ubuntu1110x86_64) 60;
+    deb_ubuntu1204i386 = makeDeb_i686 (diskImageFuns: diskImageFuns.ubuntu1204i386) 60;
+    deb_ubuntu1204x86_64 = makeDeb_x86_64 (diskImageFuns: diskImageFuns.ubuntu1204x86_64) 60;
 
 
     # System tests.
