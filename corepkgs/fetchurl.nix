2dd3117c2723ff08c6226b71d569bcea50d58ad1
Eelco Dolstra <eelco.dolstra@logicblox.com>
2012-07-09 15:48:55 -0400

    Inline fetchurl.sh

--- corepkgs/fetchurl.nix
+++ corepkgs/fetchurl.nix
@@ -5,13 +5,20 @@ with import <nix/config.nix>;
 assert (outputHash != "" && outputHashAlgo != "")
     || md5 != "" || sha1 != "" || sha256 != "";
 
+let
+
+  builder = builtins.toFile "fetchurl.sh"
+    ''
+      echo "downloading $url into $out"
+      ${curl} --fail --location --max-redirs 20 "$url" > "$out"
+    '';
+
+in
+    
 derivation {
   name = baseNameOf (toString url);
   builder = shell;
-  args = [ "-e" ./fetchurl.sh ];
-
-  # Compatibility with Nix <= 0.7.
-  id = md5;
+  args = [ "-e" builder ];
 
   # New-style output content requirements.
   outputHashAlgo = if outputHashAlgo != "" then outputHashAlgo else
@@ -19,7 +26,7 @@ derivation {
   outputHash = if outputHash != "" then outputHash else
       if sha256 != "" then sha256 else if sha1 != "" then sha1 else md5;
   
-  inherit system url curl;
+  inherit system url;
 
   # No need to double the amount of network traffic
   preferLocalBuild = true;
--- corepkgs/fetchurl.sh
+++
@@ -1,3 +0,0 @@
-echo "downloading $url into $out"
-
-$curl --fail --location --max-redirs 20 "$url" > "$out"
