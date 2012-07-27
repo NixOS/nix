e6ab52cdd1df207c7a007a9cba665ee8a031d94a
Eelco Dolstra <eelco.dolstra@logicblox.com>
2012-07-27 14:15:03 -0400

    Test "nix-env -qas" with the binary cache substituter

--- tests/binary-cache.sh
+++ tests/binary-cache.sh
@@ -2,6 +2,7 @@ source common.sh
 
 clearStore
 
+# Create the binary cache.
 cacheDir=$TEST_ROOT/binary-cache
 rm -rf $cacheDir
 
@@ -9,9 +10,11 @@ outPath=$(nix-build dependencies.nix --no-out-link)
 
 nix-push --dest $cacheDir $outPath
 
+# Check that downloading works.
 clearStore
-
 rm -f $NIX_STATE_DIR/binary-cache*
+
+NIX_BINARY_CACHES="file://$cacheDir" nix-env -f dependencies.nix -qas \* | grep -- "--S"
 
 NIX_BINARY_CACHES="file://$cacheDir" nix-store -r $outPath
 
