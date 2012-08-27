8b8fe6139e05f990b9d2a35652fd9bdb79189f90
Eelco Dolstra <eelco.dolstra@logicblox.com>
2012-08-27 11:28:34 -0400

    Drop dependency on List::MoreUtils

--- perl/lib/Nix/Utils.pm
+++ perl/lib/Nix/Utils.pm
@@ -6,3 +6,14 @@ sub checkURL {
     my ($url) = @_;
     die "invalid URL ‘$url’\n" unless $url =~ /^ $urlRE $ /x;
 }
+
+sub uniq {
+    my %seen;
+    my @res;
+    foreach my $name (@_) {
+        next if $seen{$name};
+        $seen{$name} = 1;
+        push @res, $name;
+    }
+    return @res;
+}
