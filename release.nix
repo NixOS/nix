8d100dbef1f2aeb90b73d659e0c9691be182d9ac
Eelco Dolstra <eelco.dolstra@logicblox.com>
2012-12-05 12:18:07 +0100

    Add a dependency on xz-utils

--- release.nix
+++ release.nix
@@ -222,7 +222,7 @@ let
       memSize = 1024;
       meta.schedulingPriority = prio;
       configureFlags = "--sysconfdir=/etc";
-      debRequires = [ "curl" "libdbd-sqlite3-perl" "libsqlite3-0" "libbz2-1.0" "bzip2" ];
+      debRequires = [ "curl" "libdbd-sqlite3-perl" "libsqlite3-0" "libbz2-1.0" "bzip2" "xz-utils" ];
       doInstallCheck = true;
     };
 
