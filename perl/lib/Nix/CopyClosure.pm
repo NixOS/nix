d56a60a6fe645ffe3ebebdfc4ed57936bb942850
Lluís Batlle i Rossell <viric@viric.name>
2013-04-11 19:52:21 +0200

    Adding ETA support to the --show-progress in nix-copy-closure
    Based on https://github.com/NixOS/nix/pull/6 from shlevy

--- perl/lib/Nix/CopyClosure.pm
+++ perl/lib/Nix/CopyClosure.pm
@@ -3,15 +3,12 @@ package Nix::CopyClosure;
 use strict;
 use Nix::Config;
 use Nix::Store;
+use List::Util qw(sum);
 
 
 sub copyTo {
     my ($sshHost, $sshOpts, $storePaths, $compressor, $decompressor,
         $includeOutputs, $dryRun, $sign, $progressViewer, $useSubstitutes) = @_;
-
-    $compressor = "$compressor |" if $compressor ne "";
-    $decompressor = "$decompressor |" if $decompressor ne "";
-    $progressViewer = "$progressViewer |" if $progressViewer ne "";
 
     # Get the closure of this path.
     my @closure = reverse(topoSortPaths(computeFSClosure(0, $includeOutputs,
@@ -28,15 +25,22 @@ sub copyTo {
     # we'll want to use ‘--from-stdin’, but we can't rely on the
     # target having this option yet.
     my @missing = ();
+    my $missingSize = 0;
     while (scalar(@closure) > 0) {
         my @ps = splice(@closure, 0, 1500);
         open(READ, "set -f; ssh $sshHost @{$sshOpts} nix-store --check-validity --print-invalid @ps|");
         while (<READ>) {
             chomp;
             push @missing, $_;
+            my ($deriver, $narHash, $time, $narSize, $refs) = queryPathInfo($_, 1);
+            $missingSize += $narSize;
         }
         close READ or die;
     }
+
+    $compressor = "$compressor |" if $compressor ne "";
+    $decompressor = "$decompressor |" if $decompressor ne "";
+    $progressViewer = "$progressViewer -s $missingSize |" if $progressViewer ne "";
 
     # Export the store paths and import them on the remote machine.
     if (scalar @missing > 0) {
