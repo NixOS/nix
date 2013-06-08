21bf3937f959b2dd2642fe9350c4ac69d8b10d22
Shea Levy <shea@shealevy.com>
2013-06-08 23:25:24 +0000

    Add test for dynamic attributes
    Signed-off-by: Shea Levy <shea@shealevy.com>

---
+++ tests/lang/eval-okay-dynamic-attrs.nix
@@ -0,0 +1,17 @@
+let
+  aString = "a";
+
+  bString = "b";
+in {
+  hasAttrs = { a.b = null; } ? ${aString}.b;
+
+  selectAttrs = { a.b = true; }.a.${bString};
+
+  selectOrAttrs = { }.${aString} or true;
+
+  binds = { ${aString}.${bString} = true }.a.b;
+
+  "inherit" = let a = true; b = false; in { inherit ${aString} b; }.a;
+
+  inheritFrom = let foo = { a = false; b = true; }; in { inherit (foo) a ${bString}; }.b;
+}
