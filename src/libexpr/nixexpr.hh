c390ed33057501b3d791221f20b7432287d3bf04
Shea Levy <shea@shealevy.com>
2013-06-09 01:03:39 +0000

    Non-dynamic AttrNames have a nameSym
    Signed-off-by: Shea Levy <shea@shealevy.com>

--- src/libexpr/nixexpr.hh
+++ src/libexpr/nixexpr.hh
@@ -50,8 +50,9 @@ struct StaticEnv;
 
 struct AttrName {
     bool dynamic;
+    Symbol nameSym;
     AttrName(Expr * e) : dynamic(true) {};
-    AttrName(const char * s) : dynamic(false) {};
+    AttrName(const char * s) : dynamic(false), nameSym(s) {};
 };
 
 /* An attribute path is a sequence of attribute names. */
