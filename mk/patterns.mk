c34f3c5ba4f353d67ec4a88a32c3aa688347aa4d
Eelco Dolstra <eelco.dolstra@logicblox.com>
2013-12-12 11:22:57 +0100

    Handle *.cpp extension

--- mk/patterns.mk
+++ mk/patterns.mk
@@ -1,5 +1,8 @@
 %.o: %.cc
 	$(QUIET) $(CXX) -o $@ -c $< $(GLOBAL_CXXFLAGS) $(CXXFLAGS) $($@_CXXFLAGS) -MMD -MF $(basename $@).dep -MP
 
+%.o: %.cpp
+	$(QUIET) $(CXX) -o $@ -c $< $(GLOBAL_CXXFLAGS) $(CXXFLAGS) $($@_CXXFLAGS) -MMD -MF $(basename $@).dep -MP
+
 %.o: %.c
 	$(QUIET) $(CC) -o $@ -c $< $(GLOBAL_CFLAGS) $(CFLAGS) $($@_CFLAGS) -MMD -MF $(basename $@).dep -MP
