e81b82a2cf0c7e460d02c554c597a6cf9a144e8e
Eelco Dolstra <eelco.dolstra@logicblox.com>
2013-12-16 16:51:05 +0100

    make dryclean: Sort names

--- mk/clean.mk
+++ mk/clean.mk
@@ -4,4 +4,4 @@ clean:
 	$(QUIET) rm -fv -- $(clean_files)
 
 dryclean:
-	@for i in $(clean_files); do if [ -e $$i ]; then echo $$i; fi; done
+	@for i in $(clean_files); do if [ -e $$i ]; then echo $$i; fi; done | sort
