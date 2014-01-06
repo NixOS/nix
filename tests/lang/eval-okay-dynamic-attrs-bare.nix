54d5d1768422b63d0c6a37aa95b0781fba984661
Shea Levy <shea@shealevy.com>
2014-01-06 10:27:26 -0500

    Allow "bare" dynamic attrs
    Now, in addition to a."${b}".c, you can write a.${b}.c (applicable
    wherever dynamic attributes are valid).
    
    Signed-off-by: Shea Levy <shea@shealevy.com>

---
+++ tests/lang/eval-okay-dynamic-attrs-bare.nix
@@ -0,0 +1,17 @@
+let
+  aString = "a";
+
+  bString = "b";
+in {
+  hasAttrs = { a.b = null; } ? ${aString}.b;
+
+  selectAttrs = { a.b = true; }.a.${bString};
+
+  selectOrAttrs = { }.${aString} or true;
+
+  binds = { ${aString}."${bString}c" = true; }.a.bc;
+
+  recBinds = rec { ${bString} = a; a = true; }.b;
+
+  multiAttrs = { ${aString} = true; ${bString} = false; }.a;
+}
