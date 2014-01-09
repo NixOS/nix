70d8e8fdded9e21995fecc3ecc68e14cf4f53be7
Eelco Dolstra <eelco.dolstra@logicblox.com>
2014-01-09 16:57:38 +0100

    Declare template_files as a simply expanded variable

--- templates.mk
+++ templates.mk
@@ -1,3 +1,5 @@
+template_files :=
+
 # Create the file $(1) from $(1).in by running config.status (which
 # substitutes all ‘@var@’ variables set by the configure script).
 define instantiate-template =
