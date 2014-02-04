143224f7cd6f6667047a1683c1ede4e1f7816843
Eelco Dolstra <eelco.dolstra@logicblox.com>
2014-02-04 10:09:45 +0100

    Add nix.spec to the distribution

--- local.mk
+++ local.mk
@@ -2,6 +2,6 @@ ifeq ($(MAKECMDGOALS), dist)
   dist-files += $(shell git ls-files) $(shell git ls-files)
 endif
 
-dist-files += configure config.h.in
+dist-files += configure config.h.in nix.spec
 
 GLOBAL_CXXFLAGS += -I . -I src -I src/libutil -I src/libstore -I src/libmain -I src/libexpr
