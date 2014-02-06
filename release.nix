2a469ad31da7cac5c4ecab6838c364956319821f
Eelco Dolstra <eelco.dolstra@logicblox.com>
2014-02-06 14:21:14 +0100

    Set a maintainer address
    Issue #202.

--- release.nix
+++ release.nix
@@ -279,6 +279,7 @@ let
       meta.schedulingPriority = prio;
       configureFlags = "--sysconfdir=/etc";
       debRequires = [ "curl" "libdbd-sqlite3-perl" "libsqlite3-0" "libbz2-1.0" "bzip2" "xz-utils" "libwww-curl-perl" ];
+      debMaintainer = "Eelco Dolstra <eelco.dolstra@logicblox.com>";
       doInstallCheck = true;
     };
 
