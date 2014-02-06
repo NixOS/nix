20d059892819d2f06f4da8c1150c91e16df1f8cd
Eelco Dolstra <eelco.dolstra@logicblox.com>
2014-02-06 13:51:57 +0100

    Drop dependency on ‘expr’
    http://hydra.nixos.org/build/8715639
    
    Not sure why this causes a failure now.

--- tests/parallel.builder.sh
+++ tests/parallel.builder.sh
@@ -7,7 +7,7 @@ while ! ln -s x $shared.lock 2> /dev/null; do
 done
 test -f $shared.cur || echo 0 > $shared.cur
 test -f $shared.max || echo 0 > $shared.max
-new=$(expr $(cat $shared.cur) + 1)
+new=$(($(cat $shared.cur) + 1))
 if test $new -gt $(cat $shared.max); then
     echo $new > $shared.max
 fi
@@ -25,5 +25,5 @@ while ! ln -s x $shared.lock 2> /dev/null; do
     sleep 1
 done
 test -f $shared.cur || echo 0 > $shared.cur
-echo $(expr $(cat $shared.cur) - 1) > $shared.cur
+echo $(($(cat $shared.cur) - 1)) > $shared.cur
 rm $shared.lock
