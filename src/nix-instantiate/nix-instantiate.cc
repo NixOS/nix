a897b583733aaf3ee7aa0efe495f9ea046567555
Eelco Dolstra <eelco.dolstra@logicblox.com>
2014-02-19 16:46:33 +0100

    nix-instantiate: Allow --dry-run as a synonym for --readonly-mode
    --dry-run is more consistent with nix-env and nix-store.

--- src/nix-instantiate/nix-instantiate.cc
+++ src/nix-instantiate/nix-instantiate.cc
@@ -139,6 +139,8 @@ void run(Strings args)
             strict = true;
         else if (arg == "--repair")
             state.repair = true;
+        else if (arg == "--dry-run")
+            settings.readOnlyMode = true;
         else if (arg[0] == '-')
             throw UsageError(format("unknown flag `%1%'") % arg);
         else
