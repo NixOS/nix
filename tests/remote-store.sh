7c7707638a446f91893cdc21b9e0638d2ebd42d3
Eelco Dolstra <eelco.dolstra@logicblox.com>
2014-02-26 22:41:29 +0100

    Doh

--- tests/remote-store.sh
+++ tests/remote-store.sh
@@ -9,7 +9,7 @@ $SHELL ./user-envs.sh
 
 nix-store --dump-db > $TEST_ROOT/d1
 NIX_REMOTE= nix-store --dump-db > $TEST_ROOT/d2
-cmp d1 d2
+cmp $TEST_ROOT/d1 $TEST_ROOT/d2
 
 nix-store --gc --max-freed 1K
 
