76cbf55a6d8953e393ba39896ccbb0948bac96d6
Eelco Dolstra <eelco.dolstra@logicblox.com>
2014-04-08 13:51:34 +0200

    Ensure that systemd units to into lib, not lib64
    http://hydra.nixos.org/build/10170940

--- misc/systemd/local.mk
+++ misc/systemd/local.mk
@@ -1,1 +1,1 @@
-$(foreach n, nix-daemon.socket nix-daemon.service, $(eval $(call install-file-in, $(d)/$(n), $(libdir)/systemd/system, 0644)))
+$(foreach n, nix-daemon.socket nix-daemon.service, $(eval $(call install-file-in, $(d)/$(n), $(prefix)/lib/systemd/system, 0644)))
