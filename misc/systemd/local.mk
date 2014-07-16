de8be7c3e06b52c313e0b452b641ad5f90dca2fe
Eelco Dolstra <eelco.dolstra@logicblox.com>
2014-07-16 11:16:54 +0200

    Install systemd and Upstart stuff only on Linux

--- misc/systemd/local.mk
+++ misc/systemd/local.mk
@@ -1,1 +1,5 @@
-$(foreach n, nix-daemon.socket nix-daemon.service, $(eval $(call install-file-in, $(d)/$(n), $(prefix)/lib/systemd/system, 0644)))
+ifeq ($(OS), Linux)
+
+  $(foreach n, nix-daemon.socket nix-daemon.service, $(eval $(call install-file-in, $(d)/$(n), $(prefix)/lib/systemd/system, 0644)))
+
+endif
--- misc/upstart/local.mk
+++ misc/upstart/local.mk
@@ -1,1 +1,5 @@
-$(foreach n, nix-daemon.conf, $(eval $(call install-file-in, $(d)/$(n), $(sysconfdir)/init, 0644)))
+ifeq ($(OS), Linux)
+
+  $(foreach n, nix-daemon.conf, $(eval $(call install-file-in, $(d)/$(n), $(sysconfdir)/init, 0644)))
+
+endif
