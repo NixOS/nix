45f9a91e186d91edbf2bed7d27b7fa7227730596
Eelco Dolstra <eelco.dolstra@logicblox.com>
2014-07-30 11:36:28 +0200

    make clean: Remove Makefile.config

--- local.mk
+++ local.mk
@@ -4,6 +4,8 @@ endif
 
 dist-files += configure config.h.in nix.spec
 
+clean-files += Makefile.config
+
 GLOBAL_CXXFLAGS += -I . -I src -I src/libutil -I src/libstore -I src/libmain -I src/libexpr
 
 $(foreach i, config.h $(call rwildcard, src/lib*, *.hh), $(eval $(call install-file-in, $(i), $(includedir)/nix, 0644)))
