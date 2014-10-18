92d7f262a71aa55c567967b7968ac2a1bdb7a605
Nicolas B. Pierron <nicolas.b.pierron@gmail.com>
2014-10-18 23:56:27 +0200

    Verify that a secret cannot be read with a build user.

--- tests/sec-exportPath.nix
+++ tests/sec-exportPath.nix
@@ -17,9 +17,9 @@ let
     };
 
   # Trivial Nix expression to build remotely.
-  expr = config: nr: pkgs.writeText "expr.nix"
+  expr = extraUtils: secret: nr: pkgs.writeText "expr.nix"
     ''
-      let utils = builtins.storePath ${config.system.build.extraUtils}; in
+      let utils = builtins.storePath ${extraUtils}; in
       derivation {
         name = "hello-${toString nr}";
         system = "${system}";
@@ -27,9 +27,37 @@ let
         builder = "''${utils}/bin/sh";
         args = [ "-c" "mkdir $out; echo Hello-${toString nr} > $out/host" ];
         outputs = [ "out" ];
-        secret = true;
+        secret = ${if secret then "true" else "false"};
       }
     '';
+
+  # This is used to attempt fetching secret files from the store. On a single
+  # user store with no build user, this will fail, but on a multiple user store
+  # we should not be able to read any other file from the store, even if it is
+  # referenced by the derivation.
+  evilLog = extraUtils: pkgs.writeText "evil.nix" ''
+    { path }:
+    let utils = builtins.storePath ${extraUtils}; in
+    derivation {
+      name = "evil-dump-log";
+      system = "${system}";
+      PATH = "''${utils}/bin";
+      builder = "''${utils}/bin/sh";
+      args = [ "-c" "mkdir $out; cat ''${path}/host; echo 1 > $out/result" ];
+    }
+  '';
+
+  evilOut = extraUtils: pkgs.writeText "evil.nix" ''
+    { path }:
+    let utils = builtins.storePath ${extraUtils}; in
+    derivation {
+      name = "hello-1";
+      system = "${system}";
+      PATH = "''${utils}/bin";
+      builder = "''${utils}/bin/sh";
+      args = [ "-c" "mkdir $out; cat ''${path}/host | tee $out/host" ];
+    }
+  '';
 
 in
 
@@ -42,24 +70,27 @@ in
           virtualisation.pathsInNixDB = [ config.system.build.extraUtils ];
           nix.package = nix;
           nix.binaryCaches = [ ];
+          environment.systemPackages = [ pkgs.diffutils ];
 
           users.extraUsers.alice =
             { createHome = true;
               home = "/home/alice";
               description = "Alice Foobar";
               extraGroups = [ "wheel" ];
+              useDefaultShell = true;
             };
         };
     };
 
   testScript = { nodes }:
+    let extraUtils = nodes.client.config.system.build.extraUtils; in
     ''
       startAll;
 
       $client->waitForUnit("multi-user.target");
 
       # Root builds the derivation
-      my $out = $client->succeed("nix-build ${expr nodes.client.config 1}");
+      my $out = $client->succeed("nix-build ${expr extraUtils true 1}");
       $out =~ s/\s+$//;
       my $outfile = $out . "/host";
 
@@ -67,8 +98,21 @@ in
       $client->succeed("cat $outfile");
       $client->succeed("nix-store --export $out");
 
+      # Verify that alice can build a derivations
+      $client->succeed("su alice -lc 'nix-build ${expr extraUtils false 2}'");
+
       # Check that alice cannot read / export the output of the derivation.
-      $client->fail("su alice -c 'cat $outfile'");
-      $client->fail("su alice -c 'nix-store --export $out'");
+      $client->fail("su alice -lc 'cat $outfile'");
+      $client->fail("su alice -lc 'nix-store --export $out'");
+
+      # Verify that we cannot spew any sescret during a realization.
+      $client->succeed("su alice -lc 'nix-build ${evilLog extraUtils} --argstr path $out | grep -v Hello-1'");
+
+      # Check that we cannot copy the content to make it publicly visible.
+      my $evilout = $client->succeed("su alice -lc 'nix-build ${evilOut extraUtils} --argstr path $out'");
+      $evilout =~ s/\s+$//;
+      $client->succeed("test $out != $evilout");
+      my $eviloutfile = $evilout . "/host";
+      $client->fail("diff $outfile $eviloutfile");
     '';
 })
