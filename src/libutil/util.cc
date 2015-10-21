af1218a86abf71595a54860b677dec30d1d53278
Jude Taylor <me@jude.bio>
2015-10-21 14:40:35 -0700

    revert libutil change

--- src/libutil/util.cc
+++ src/libutil/util.cc
@@ -167,11 +167,10 @@ string baseNameOf(const Path & path)
 
 bool isInDir(const Path & path, const Path & dir)
 {
-    return dir == "/"
-        || (path[0] == '/'
-            && string(path, 0, dir.size()) == dir
-            && path.size() >= dir.size() + 2
-            && path[dir.size()] == '/');
+    return path[0] == '/'
+        && string(path, 0, dir.size()) == dir
+        && path.size() >= dir.size() + 2
+        && path[dir.size()] == '/';
 }
 
 
