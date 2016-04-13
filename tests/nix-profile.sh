7186539711183a06d932dc24c7b26b5306ca9a3f
Shea Levy <shea@shealevy.com>
2016-04-13 10:43:33 -0400

    nix-profile test: Set USER

--- tests/nix-profile.sh
+++ tests/nix-profile.sh
@@ -1,10 +1,11 @@
 source common.sh
 
 home=$TEST_ROOT/home
+user=$(whoami)
 rm -rf $home
 mkdir -p $home
-HOME=$home $SHELL -e -c ". ../scripts/nix-profile.sh"
-HOME=$home $SHELL -e -c ". ../scripts/nix-profile.sh" # test idempotency
+HOME=$home USER=$user $SHELL -e -c ". ../scripts/nix-profile.sh"
+HOME=$home USER=$user $SHELL -e -c ". ../scripts/nix-profile.sh" # test idempotency
 
 [ -L $home/.nix-profile ]
 [ -e $home/.nix-channels ]
