3c68a661f28ffb52efbca10146a059003ff579a7
Shea Levy <shea@shealevy.com>
2016-07-21 19:00:54 -0400

    resolve-system-dependencies.pl: Only install on darwin

--- scripts/local.mk
+++ scripts/local.mk
@@ -10,10 +10,13 @@ bin-scripts += $(nix_bin_scripts)
 nix_noinst_scripts := \
   $(d)/build-remote.pl \
   $(d)/find-runtime-roots.pl \
-  $(d)/resolve-system-dependencies.pl \
   $(d)/nix-http-export.cgi \
   $(d)/nix-profile.sh \
   $(d)/nix-reduce-build
+
+ifeq ($(OS), Darwin)
+  nix_noinst_scripts += $(d)/resolve-system-dependencies.pl
+endif
 
 noinst-scripts += $(nix_noinst_scripts)
 
@@ -22,7 +25,9 @@ profiledir = $(sysconfdir)/profile.d
 $(eval $(call install-file-as, $(d)/nix-profile.sh, $(profiledir)/nix.sh, 0644))
 $(eval $(call install-program-in, $(d)/find-runtime-roots.pl, $(libexecdir)/nix))
 $(eval $(call install-program-in, $(d)/build-remote.pl, $(libexecdir)/nix))
-$(eval $(call install-program-in, $(d)/resolve-system-dependencies.pl, $(libexecdir)/nix))
+ifeq ($(OS), Darwin)
+  $(eval $(call install-program-in, $(d)/resolve-system-dependencies.pl, $(libexecdir)/nix))
+endif
 $(eval $(call install-symlink, nix-build, $(bindir)/nix-shell))
 
 clean-files += $(nix_bin_scripts) $(nix_noinst_scripts)
