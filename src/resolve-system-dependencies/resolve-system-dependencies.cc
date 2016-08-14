b083adfe82039f19c3dac9bc00b7f6db23e5e2bb
Jude Taylor <me@jude.bio>
2016-08-14 14:26:56 -0700

    implement in c++

--- src/resolve-system-dependencies/resolve-system-dependencies.cc
+++ src/resolve-system-dependencies/resolve-system-dependencies.cc
@@ -4,12 +4,14 @@
 #include "store-api.hh"
 #include <sys/utsname.h>
 #include <algorithm>
+#include <libgen.h>
 #include <iostream>
 #include <fstream>
 #include <sys/mman.h>
 #include <fcntl.h>
 #include <mach-o/loader.h>
 #include <mach-o/swap.h>
+#include <yaml-cpp/yaml.h>
 
 #define DO_SWAP(x, y) ((x) ? OSSwapInt32(y) : (y))
 
@@ -41,6 +43,11 @@ std::string findDylibName(bool should_swap, ptrdiff_t dylib_command_start) {
 }
 
 std::set<std::string> runResolver(const Path & filename) {
+    if(getFileType(filename) != DT_REG) {
+        printMsg(lvlError, format("%1%: not a regular file") % filename);
+        return std::set<string>();
+    }
+
     int fd = open(filename.c_str(), O_RDONLY);
     struct stat s;
     fstat(fd, &s);
@@ -54,8 +61,11 @@ std::set<std::string> runResolver(const Path & filename) {
         uint32_t narches = DO_SWAP(should_swap, ((struct fat_header*)obj)->nfat_arch);
 
         for(uint32_t iter = 0; iter < narches; iter++) {
-            ptrdiff_t header_offset = (ptrdiff_t)obj + sizeof(struct fat_header) * (iter + 1);
-            struct fat_arch* arch = (struct fat_arch*)header_offset;
+            ptrdiff_t arch_offset = (ptrdiff_t)obj
+                + sizeof(struct fat_header)
+                + sizeof(struct fat_arch) * iter;
+
+            struct fat_arch* arch = (struct fat_arch*)arch_offset;
             if(DO_SWAP(should_swap, arch->cputype) == CPU_TYPE_X86_64) {
                 mach64_offset = (ptrdiff_t)DO_SWAP(should_swap, arch->offset);
                 break;
@@ -75,7 +85,7 @@ std::set<std::string> runResolver(const Path & filename) {
     ptrdiff_t mach_header_offset = (ptrdiff_t)obj + mach64_offset;
     struct mach_header_64 *m_header = (struct mach_header_64 *)mach_header_offset;
 
-    bool should_swap = magic == MH_CIGAM_64;
+    bool should_swap = m_header->magic == MH_CIGAM_64;
     ptrdiff_t cmd_offset = mach_header_offset + sizeof(struct mach_header_64);
 
     std::set<string> libs;
@@ -83,6 +93,7 @@ std::set<std::string> runResolver(const Path & filename) {
         struct load_command *cmd = (struct load_command*)cmd_offset;
         switch(DO_SWAP(should_swap, cmd->cmd)) {
             case LC_LOAD_UPWARD_DYLIB:
+            case LC_LOAD_WEAK_DYLIB:
             case LC_LOAD_DYLIB:
             case LC_REEXPORT_DYLIB:
                 libs.insert(findDylibName(should_swap, cmd_offset));
@@ -107,10 +118,22 @@ Path resolveSymlink(const Path & path) {
     ssize_t len = readlink(path.c_str(), buf, sizeof(buf) - 1);
     if(len != -1) {
         buf[len] = 0;
-        return Path(buf);
+        if(buf[0] == '/')
+            return Path(buf);
+        return Path(dirname(strdup(path.c_str()))) + "/" + Path(buf);
     } else {
         throw SysError(format("readlink('%1%')") % path);
     }
+}
+
+bool isTbdFile(const Path & path) {
+    std::string ending = ".tbd";
+    return 0 == path.compare(path.length() - ending.length(), ending.length(), ending);
+}
+
+std::string resolveTbdTarget(const Path & path) {
+    YAML::Node tbd = YAML::LoadFile(path);
+    return tbd["install-name"].as<std::string>();
 }
 
 std::set<string> resolveTree(const Path & path, PathSet & deps) {
@@ -144,6 +167,11 @@ std::set<string> getPath(const Path & path) {
         paths.insert(next_path);
     }
 
+    if(isTbdFile(next_path)) {
+        next_path = resolveTbdTarget(next_path);
+        paths.insert(next_path);
+    }
+
     for(auto & t : resolveTree(next_path, deps)) {
         paths.insert(t);
     }
@@ -156,6 +184,10 @@ std::set<string> getPath(const Path & path) {
 int main(int argc, char ** argv) {
     return handleExceptions(argv[0], [&]() {
         initNix();
+
+        if(argc < 2) {
+            throw Error(format("missing derivation argument"));
+        }
 
         struct utsname _uname;
 
