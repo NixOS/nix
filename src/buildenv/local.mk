67364a5795a7ee9c17807d6e5caa5c4d3b508fc4
Eelco Dolstra <eelco.dolstra@logicblox.com>
2016-08-30 13:56:22 +0200

    Fix Fedora build

--- src/buildenv/local.mk
+++ src/buildenv/local.mk
@@ -4,6 +4,6 @@ buildenv_DIR := $(d)
 
 buildenv_INSTALL_DIR := $(libexecdir)/nix
 
-buildenv_LIBS = libmain libutil libformat
+buildenv_LIBS = libmain libstore libutil libformat
 
 buildenv_SOURCES := $(d)/buildenv.cc
