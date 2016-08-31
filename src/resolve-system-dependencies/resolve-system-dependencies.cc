6c70d3561a0189532824a4451718403747d947e9
Jude Taylor <me@jude.bio>
2016-08-31 14:47:33 -0700

    fix indentation and capitalization

--- src/resolve-system-dependencies/resolve-system-dependencies.cc
+++ src/resolve-system-dependencies/resolve-system-dependencies.cc
@@ -4,23 +4,24 @@
 #include "store-api.hh"
 #include <sys/utsname.h>
 #include <algorithm>
+#include <libgen.h>
 #include <iostream>
 #include <fstream>
-#include <libgen.h>
+#include <sys/mman.h>
+#include <fcntl.h>
 #include <mach-o/loader.h>
 #include <mach-o/swap.h>
 #include <yaml-cpp/yaml.h>
 
-using namespace nix;
+#define DO_SWAP(x, y) ((x) ? OSSwapInt32(y) : (y))
 
-typedef std::map<string, std::set<string>> SetMap;
+using namespace nix;
 
 static auto cacheDir = Path{};
 
-Path resolveCacheFile(const Path & lib) {
-    Path lib2 = Path(lib);
-    std::replace(lib2.begin(), lib2.end(), '/', '%');
-    return cacheDir + "/" + lib2;
+Path resolveCacheFile(Path lib) {
+    std::replace(lib.begin(), lib.end(), '/', '%');
+    return cacheDir + "/" + lib;
 }
 
 std::set<string> readCacheFile(const Path & file) {
@@ -36,98 +37,92 @@ void writeCacheFile(const Path & file, std::set<string> & deps) {
     fp.close();
 }
 
-std::string find_dylib_name(FILE *obj_file, struct load_command cmd) {
-  fpos_t pos;
-  fgetpos(obj_file, &pos);
-  struct dylib_command dylc;
-  dylc.cmd = cmd.cmd;
-  dylc.cmdsize = cmd.cmdsize;
-  fread(&dylc.dylib, sizeof(struct dylib), 1, obj_file);
-
-  char *dylib_name = (char*)calloc(cmd.cmdsize, sizeof(char));
-  fseek(obj_file,
-      // offset is calculated from the beginning of the load command, which is two
-      // uint32_t's backwards
-      dylc.dylib.name.offset - (sizeof(uint32_t) * 2) + pos,
-      SEEK_SET);
-  fread(dylib_name, sizeof(char), cmd.cmdsize, obj_file);
-  fseek(obj_file, pos, SEEK_SET);
-  return std::string(dylib_name);
-}
-
-bool seek_mach64_blob(FILE *obj_file, enum NXByteOrder end) {
-  struct fat_header head;
-  fread(&head, sizeof(struct fat_header), 1, obj_file);
-  swap_fat_header(&head, end);
-  for(uint32_t narches = 0; narches < head.nfat_arch; narches++) {
-    struct fat_arch arch;
-    fread(&arch, sizeof(struct fat_arch), 1, obj_file);
-    swap_fat_arch(&arch, 1, end);
-    if(arch.cputype == CPU_TYPE_X86_64) {
-      fseek(obj_file, arch.offset, SEEK_SET);
-      return true;
-    }
-  }
-  return false;
+std::string findDylibName(bool should_swap, ptrdiff_t dylib_command_start) {
+    struct dylib_command *dylc = (struct dylib_command*)dylib_command_start;
+    return std::string((char*)(dylib_command_start + DO_SWAP(should_swap, dylc->dylib.name.offset)));
 }
 
 std::set<std::string> runResolver(const Path & filename) {
-  FILE *obj_file = fopen(filename.c_str(), "rb");
-  uint32_t magic;
-  fread(&magic, sizeof(uint32_t), 1, obj_file);
-  fseek(obj_file, 0, SEEK_SET);
-  enum NXByteOrder endianness;
-  if(magic == 0xBEBAFECA) {
-    endianness = NX_BigEndian;
-    if(!seek_mach64_blob(obj_file, endianness)) {
-      std::cerr << "Could not find any mach64 blobs in file " << filename << ", continuing..." << std::endl;
-      return std::set<string>();
+    if(getFileType(filename) != DT_REG) {
+        printMsg(lvlError, format("%1%: not a regular file") % filename);
+        return std::set<string>();
     }
-  }
-  struct mach_header_64 header;
-  fread(&header, sizeof(struct mach_header_64), 1, obj_file);
-  if(!(header.magic == MH_MAGIC_64 || header.magic == MH_CIGAM_64)) {
-    std::cerr << "Not a mach-o object file: " << filename << std::endl;
-    return std::set<string>();
-  }
-  std::set<string> libs;
-  for(uint32_t i = 0; i < header.ncmds; i++) {
-    struct load_command cmd;
-    fread(&cmd.cmd, sizeof(uint32_t), 1, obj_file);
-    fread(&cmd.cmdsize, sizeof(uint32_t), 1, obj_file);
-    switch(cmd.cmd) {
-      case LC_LOAD_DYLIB:
-      case LC_REEXPORT_DYLIB:
-        libs.insert(find_dylib_name(obj_file, cmd));
-        break;
+
+    int fd = open(filename.c_str(), O_RDONLY);
+    struct stat s;
+    fstat(fd, &s);
+    void *obj = mmap(NULL, s.st_size, PROT_READ, MAP_SHARED, fd, 0);
+
+    ptrdiff_t mach64_offset = 0;
+
+    uint32_t magic = ((struct mach_header_64*)obj)->magic;
+    if(magic == FAT_CIGAM || magic == FAT_MAGIC) {
+        bool should_swap = magic == FAT_CIGAM;
+        uint32_t narches = DO_SWAP(should_swap, ((struct fat_header*)obj)->nfat_arch);
+
+        for(uint32_t iter = 0; iter < narches; iter++) {
+            ptrdiff_t arch_offset = (ptrdiff_t)obj
+                + sizeof(struct fat_header)
+                + sizeof(struct fat_arch) * iter;
+
+            struct fat_arch* arch = (struct fat_arch*)arch_offset;
+            if(DO_SWAP(should_swap, arch->cputype) == CPU_TYPE_X86_64) {
+                mach64_offset = (ptrdiff_t)DO_SWAP(should_swap, arch->offset);
+                break;
+            }
+        }
+        if (mach64_offset == 0) {
+            printMsg(lvlError, format("Could not find any mach64 blobs in file ‘%1%’, continuing...") % filename);
+            return std::set<string>();
+        }
+    } else if (magic == MH_MAGIC_64 || magic == MH_CIGAM_64) {
+        mach64_offset = 0;
+    } else {
+        printMsg(lvlError, format("Object file has unknown magic number ‘%1%’, skipping it...") % magic);
+        return std::set<string>();
     }
-    fseek(obj_file, cmd.cmdsize - (sizeof(uint32_t) * 2), SEEK_CUR);
-  }
-  fclose(obj_file);
-  libs.erase(filename);
-  return libs;
+
+    ptrdiff_t mach_header_offset = (ptrdiff_t)obj + mach64_offset;
+    struct mach_header_64 *m_header = (struct mach_header_64 *)mach_header_offset;
+
+    bool should_swap = m_header->magic == MH_CIGAM_64;
+    ptrdiff_t cmd_offset = mach_header_offset + sizeof(struct mach_header_64);
+
+    std::set<string> libs;
+    for(uint32_t i = 0; i < DO_SWAP(should_swap, m_header->ncmds); i++) {
+        struct load_command *cmd = (struct load_command*)cmd_offset;
+        switch(DO_SWAP(should_swap, cmd->cmd)) {
+            case LC_LOAD_UPWARD_DYLIB:
+            case LC_LOAD_WEAK_DYLIB:
+            case LC_LOAD_DYLIB:
+            case LC_REEXPORT_DYLIB:
+                libs.insert(findDylibName(should_swap, cmd_offset));
+                break;
+        }
+        cmd_offset += DO_SWAP(should_swap, cmd->cmdsize);
+    }
+
+    return libs;
 }
 
 bool isSymlink(const Path & path) {
-  struct stat st;
-  if(lstat(path.c_str(), &st))
-      throw SysError(format("getting attributes of path ‘%1%’") % path);
+    struct stat st;
+    if(lstat(path.c_str(), &st))
+        throw SysError(format("getting attributes of path ‘%1%’") % path);
 
-  return S_ISLNK(st.st_mode);
+    return S_ISLNK(st.st_mode);
 }
 
 Path resolveSymlink(const Path & path) {
     char buf[PATH_MAX];
     ssize_t len = readlink(path.c_str(), buf, sizeof(buf) - 1);
     if(len != -1) {
-      buf[len] = 0;
-      if(buf[0] == '/') {
-        return Path(buf);
-      } else {
+        buf[len] = 0;
+        if(buf[0] == '/')
+            return Path(buf);
         return Path(dirname(strdup(path.c_str()))) + "/" + Path(buf);
-      }
     } else {
-      throw SysError(format("readlink('%1%')") % path);
+        throw SysError(format("readlink('%1%')") % path);
     }
 }
 
@@ -141,66 +136,73 @@ std::string resolveTbdTarget(const Path & path) {
     return tbd["install-name"].as<std::string>();
 }
 
-std::set<string> resolve_tree(const Path & path, PathSet & deps) {
-  std::set<string> results;
-  if(deps.find(path) != deps.end()) {
-    return std::set<string>();
-  }
-  deps.insert(path);
-  for (auto & lib : runResolver(path)) {
-    results.insert(lib);
-    for (auto & p : resolve_tree(lib, deps)) {
-      results.insert(p);
+std::set<string> resolveTree(const Path & path, PathSet & deps) {
+    std::set<string> results;
+    if(deps.find(path) != deps.end()) {
+        return std::set<string>();
     }
-  }
-  return results;
+    deps.insert(path);
+    for (auto & lib : runResolver(path)) {
+        results.insert(lib);
+        for (auto & p : resolveTree(lib, deps)) {
+            results.insert(p);
+        }
+    }
+    return results;
 }
 
-std::set<string> get_path(const Path & path) {
-  Path cacheFile = resolveCacheFile(path);
-  if(pathExists(cacheFile)) {
-    return readCacheFile(cacheFile);
-  }
+std::set<string> getPath(const Path & path) {
+    Path cacheFile = resolveCacheFile(path);
+    if(pathExists(cacheFile)) {
+        return readCacheFile(cacheFile);
+    }
 
-  std::set<string> deps;
-  std::set<string> paths;
-  paths.insert(path);
+    std::set<string> deps;
+    std::set<string> paths;
+    paths.insert(path);
 
-  Path next_path = Path(path);
-  while(isSymlink(next_path)) {
-    next_path = resolveSymlink(next_path);
-    paths.insert(next_path);
-  }
+    Path next_path = Path(path);
+    while(isSymlink(next_path)) {
+        next_path = resolveSymlink(next_path);
+        paths.insert(next_path);
+    }
 
-  if(isTbdFile(next_path)) {
-    next_path = resolveTbdTarget(next_path);
-    paths.insert(next_path);
-  }
+    if(isTbdFile(next_path)) {
+        next_path = resolveTbdTarget(next_path);
+        paths.insert(next_path);
+    }
 
-  for(auto & t : resolve_tree(next_path, deps)) {
-    paths.insert(t);
-  }
+    for(auto & t : resolveTree(next_path, deps)) {
+        paths.insert(t);
+    }
 
-  writeCacheFile(cacheFile, paths);
+    writeCacheFile(cacheFile, paths);
 
-  return paths;
+    return paths;
 }
 
 int main(int argc, char ** argv) {
     return handleExceptions(argv[0], [&]() {
         initNix();
 
+        if(argc < 2) {
+            throw Error(format("missing derivation argument"));
+        }
+
         struct utsname _uname;
 
         uname(&_uname);
 
-        cacheDir = (format("%1%/dependency-maps/%2%-%3%-%4%")
-            % settings.nixStateDir
-            % _uname.machine
-            % _uname.sysname
-            % _uname.release).str();
+        auto cacheParentDir = (format("%1%/dependency-maps") % settings.nixStateDir).str();
 
-        mkdir(cacheDir.c_str(), 0644);
+        cacheDir = (format("%1%/%2%-%3%-%4%")
+                % cacheParentDir
+                % _uname.machine
+                % _uname.sysname
+                % _uname.release).str();
+
+        mkdir(cacheParentDir.c_str(), 0755);
+        mkdir(cacheDir.c_str(), 0755);
 
         auto store = openStore();
 
@@ -210,14 +212,14 @@ int main(int argc, char ** argv) {
         std::set<string> all_paths;
 
         for (auto & path : impurePaths) {
-            for(auto & p : get_path(path)) {
-              all_paths.insert(p);
+            for(auto & p : getPath(path)) {
+                all_paths.insert(p);
             }
         }
 
         std::cout << "extra-chroot-dirs" << std::endl;
         for(auto & path : all_paths) {
-          std::cout << path << std::endl;
+            std::cout << path << std::endl;
         }
         std::cout << std::endl;
     });
