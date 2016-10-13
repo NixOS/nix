NixOS/nix

commit 86e8c67efc33cf756500a1dec7fd6313658f2664

Fix build with older versions of libcurl

src/libstore/download.cc
@@ -188,9 +188,13 @@ struct CurlDownloader : public Downloader
             curl_easy_setopt(req, CURLOPT_FOLLOWLOCATION, 1L);
             curl_easy_setopt(req, CURLOPT_NOSIGNAL, 1);
             curl_easy_setopt(req, CURLOPT_USERAGENT, ("Nix/" + nixVersion).c_str());
+            #ifdef CURLOPT_PIPEWAIT
             curl_easy_setopt(req, CURLOPT_PIPEWAIT, 1);
+            #endif
+            #ifdef CURL_HTTP_VERSION_2TLS
             if (downloader.enableHttp2)
                 curl_easy_setopt(req, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_2TLS);
+            #endif
             curl_easy_setopt(req, CURLOPT_WRITEFUNCTION, DownloadItem::writeCallbackWrapper);
             curl_easy_setopt(req, CURLOPT_WRITEDATA, this);
             curl_easy_setopt(req, CURLOPT_HEADERFUNCTION, DownloadItem::headerCallbackWrapper);
@@ -297,7 +301,9 @@ struct CurlDownloader : public Downloader
 
         curlm = curl_multi_init();
 
+        #ifdef CURLPIPE_MULTIPLEX
         curl_multi_setopt(curlm, CURLMOPT_PIPELINING, CURLPIPE_MULTIPLEX);
+        #endif
         curl_multi_setopt(curlm, CURLMOPT_MAX_TOTAL_CONNECTIONS,
             settings.get("binary-caches-parallel-connections", 25));
 

commit 5e61b422c58baac26b232233d39f5814cc35d52a

Fix HTTP/2 support
Commit 86e8c67efc33cf756500a1dec7fd6313658f2664 broke it, because
CURL_* are not actually #defines.

src/libstore/download.cc
@@ -188,10 +188,10 @@ struct CurlDownloader : public Downloader
             curl_easy_setopt(req, CURLOPT_FOLLOWLOCATION, 1L);
             curl_easy_setopt(req, CURLOPT_NOSIGNAL, 1);
             curl_easy_setopt(req, CURLOPT_USERAGENT, ("Nix/" + nixVersion).c_str());
-            #ifdef CURLOPT_PIPEWAIT
+            #if CURL_AT_LEAST_VERSION(7, 43, 0)
             curl_easy_setopt(req, CURLOPT_PIPEWAIT, 1);
             #endif
-            #ifdef CURL_HTTP_VERSION_2TLS
+            #if CURL_AT_LEAST_VERSION(7, 47, 0)
             if (downloader.enableHttp2)
                 curl_easy_setopt(req, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_2TLS);
             #endif
@@ -303,7 +303,7 @@ struct CurlDownloader : public Downloader
 
         curlm = curl_multi_init();
 
-        #ifdef CURLPIPE_MULTIPLEX
+        #if CURL_AT_LEAST_VERSION(7, 43, 0) // correct?
         curl_multi_setopt(curlm, CURLMOPT_PIPELINING, CURLPIPE_MULTIPLEX);
         #endif
         curl_multi_setopt(curlm, CURLMOPT_MAX_TOTAL_CONNECTIONS,