09191caea8a0bf59daef553ca251a8d1d7e1d507
Eelco Dolstra <edolstra@gmail.com>
2016-11-28 16:13:23 +0100

    Add shell.nix

---
+++ shell.nix
@@ -0,0 +1,33 @@
+with import <nixpkgs> {};
+
+stdenv.mkDerivation {
+  name = "nix";
+
+  buildInputs =
+    [ curl bison flex perl libxml2 libxslt bzip2 xz
+      pkgconfig sqlite libsodium boehmgc
+      docbook5 docbook5_xsl
+      autoconf-archive
+      (aws-sdk-cpp.override {
+        apis = ["s3"];
+        customMemoryManagement = false;
+      })
+      autoreconfHook
+    ];
+
+  configureFlags =
+    [ "--disable-init-state"
+      "--enable-gc"
+      "--with-dbi=${perlPackages.DBI}/${perl.libPrefix}"
+      "--with-dbd-sqlite=${perlPackages.DBDSQLite}/${perl.libPrefix}"
+    ];
+
+  enableParallelBuilding = true;
+
+  installFlags = "sysconfdir=$(out)/etc";
+
+  shellHook =
+    ''
+      configureFlags+=" --prefix=$(pwd)/inst"
+    '';
+}
