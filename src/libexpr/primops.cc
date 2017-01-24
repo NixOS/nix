4e6a2fbc561f3e692a5644b3816a205ba769f39e
Eelco Dolstra <edolstra@gmail.com>
2017-01-24 15:31:54 +0100

    Revert "Propagate path context via builtins.readFile"
    This reverts commit f7f0116dd727ac954fb04d9ef9b9fe7ec034e563.
    
    Issue #1174.

--- src/libexpr/primops.cc
+++ src/libexpr/primops.cc
@@ -779,7 +779,7 @@ static void prim_readFile(EvalState & state, const Pos & pos, Value * * args, Va
     string s = readFile(state.checkSourcePath(path));
     if (s.find((char) 0) != string::npos)
         throw Error(format("the contents of the file ‘%1%’ cannot be represented as a Nix string") % path);
-    mkString(v, s.c_str(), context);
+    mkString(v, s.c_str());
 }
 
 
