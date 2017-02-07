27dc76c1a5dbe654465245ff5f6bc22e2c8902da
Eelco Dolstra <edolstra@gmail.com>
2017-02-07 18:49:17 +0100

    Remove build-remote.pl.in

--- scripts/local.mk
+++ scripts/local.mk
@@ -4,7 +4,6 @@ nix_bin_scripts := \
 bin-scripts += $(nix_bin_scripts)
 
 nix_noinst_scripts := \
-  $(d)/build-remote.pl \
   $(d)/nix-http-export.cgi \
   $(d)/nix-profile.sh \
   $(d)/nix-reduce-build
@@ -14,6 +13,5 @@ noinst-scripts += $(nix_noinst_scripts)
 profiledir = $(sysconfdir)/profile.d
 
 $(eval $(call install-file-as, $(d)/nix-profile.sh, $(profiledir)/nix.sh, 0644))
-$(eval $(call install-program-in, $(d)/build-remote.pl, $(libexecdir)/nix))
 
 clean-files += $(nix_bin_scripts) $(nix_noinst_scripts)
