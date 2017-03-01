56e19d970db33de6b7aef088cde57817b5a61d61
Eelco Dolstra <edolstra@gmail.com>
2017-03-01 14:47:52 +0100

    nix-store --import: Fix importing unsigned paths

--- src/nix-store/nix-store.cc
+++ src/nix-store/nix-store.cc
@@ -724,7 +724,7 @@ static void opImport(Strings opFlags, Strings opArgs)
     if (!opArgs.empty()) throw UsageError("no arguments expected");
 
     FdSource source(STDIN_FILENO);
-    Paths paths = store->importPaths(source, 0);
+    Paths paths = store->importPaths(source, nullptr, true);
 
     for (auto & i : paths)
         cout << format("%1%\n") % i << std::flush;
