73d7a51ee6942f681db468dc8e3c631b4d3daa4e
Eelco Dolstra <edolstra@gmail.com>
2017-03-13 14:39:18 +0100

    Remove dependency on "curl" binary

--- src/nix-store/local.mk
+++ src/nix-store/local.mk
@@ -7,5 +7,3 @@ nix-store_SOURCES := $(wildcard $(d)/*.cc)
 nix-store_LIBS = libmain libstore libutil libformat
 
 nix-store_LDFLAGS = -lbz2 -pthread $(SODIUM_LIBS)
-
-nix-store_CXXFLAGS = -DCURL=\"$(curl)\"
