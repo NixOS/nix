d0877003475ecb3ea18a256239a9948fe0f28035
Eelco Dolstra <edolstra@gmail.com>
2017-03-31 15:31:34 +0200

    Fix perl build

--- perl/local.mk
+++ perl/local.mk
@@ -10,38 +10,34 @@ nix_perl_modules := $(nix_perl_sources:.in=)
 
 $(foreach x, $(nix_perl_modules), $(eval $(call install-data-in, $(x), $(perllibdir)/Nix)))
 
-ifeq ($(perlbindings), yes)
-
-  lib/Nix/Store.cc: lib/Nix/Store.xs
+lib/Nix/Store.cc: lib/Nix/Store.xs
 	$(trace-gen) xsubpp $^ -output $@
 
-  libraries += Store
+libraries += Store
 
-  Store_DIR := lib/Nix
+Store_DIR := lib/Nix
 
-  Store_SOURCES := $(Store_DIR)/Store.cc
+Store_SOURCES := $(Store_DIR)/Store.cc
 
-  Store_CXXFLAGS = \
-    -I$(shell $(perl) -e 'use Config; print $$Config{archlibexp};')/CORE \
-    -D_FILE_OFFSET_BITS=64 \
-    -Wno-unknown-warning-option -Wno-unused-variable -Wno-literal-suffix \
-    -Wno-reserved-user-defined-literal -Wno-duplicate-decl-specifier -Wno-pointer-bool-conversion \
-    $(NIX_CFLAGS)
+Store_CXXFLAGS = \
+  -I$(shell $(perl) -e 'use Config; print $$Config{archlibexp};')/CORE \
+  -D_FILE_OFFSET_BITS=64 \
+  -Wno-unknown-warning-option -Wno-unused-variable -Wno-literal-suffix \
+  -Wno-reserved-user-defined-literal -Wno-duplicate-decl-specifier -Wno-pointer-bool-conversion \
+  $(NIX_CFLAGS)
 
-  Store_LDFLAGS := $(SODIUM_LIBS) $(NIX_LIBS)
+Store_LDFLAGS := $(SODIUM_LIBS) $(NIX_LIBS)
 
-  ifeq (CYGWIN,$(findstring CYGWIN,$(OS)))
-    archlib = $(shell perl -E 'use Config; print $$Config{archlib};')
-    libperl = $(shell perl -E 'use Config; print $$Config{libperl};')
-    Store_LDFLAGS += $(shell find ${archlib} -name ${libperl})
-  endif
-
-  Store_ALLOW_UNDEFINED = 1
-
-  Store_FORCE_INSTALL = 1
-
-  Store_INSTALL_DIR = $(perllibdir)/auto/Nix/Store
-
+ifeq (CYGWIN,$(findstring CYGWIN,$(OS)))
+  archlib = $(shell perl -E 'use Config; print $$Config{archlib};')
+  libperl = $(shell perl -E 'use Config; print $$Config{libperl};')
+  Store_LDFLAGS += $(shell find ${archlib} -name ${libperl})
 endif
+
+Store_ALLOW_UNDEFINED = 1
+
+Store_FORCE_INSTALL = 1
+
+Store_INSTALL_DIR = $(perllibdir)/auto/Nix/Store
 
 clean-files += lib/Nix/Config.pm lib/Nix/Store.cc Makefile.config
