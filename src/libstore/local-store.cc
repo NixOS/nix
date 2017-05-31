c48697d617c07d280901cf6e1a11ef6fe6d5b6f3
Eelco Dolstra <edolstra@gmail.com>
2017-05-31 20:43:47 +0200

    Remove listxattr assertion
    It appears that sometimes, listxattr() returns a different value for
    the query case (i.e. when the buffer size is 0).
    
    (cherry picked from commit 52fec8dde862264874a4f19be329124ac46adb81)

--- src/libstore/local-store.cc
+++ src/libstore/local-store.cc
@@ -595,8 +595,6 @@ static void canonicalisePathMetaData_(const Path & path, uid_t fromUid, InodesSe
         for (auto & eaName: tokenizeString<Strings>(std::string(eaBuf.data(), eaSize), std::string("\000", 1)))
             if (lremovexattr(path.c_str(), eaName.c_str()) == -1)
                 throw SysError("removing extended attribute ‘%s’ from ‘%s’", eaName, path);
-
-        assert(llistxattr(path.c_str(), nullptr, 0) == 0);
     }
 #endif
 
