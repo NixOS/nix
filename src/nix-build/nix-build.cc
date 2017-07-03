0a5a867758285ee07334a44ede336ae75fd51c50
Eelco Dolstra <edolstra@gmail.com>
2017-07-03 11:54:30 +0200

    nix-shell: Respect --dry-run
    Fixes #824.

--- src/nix-build/nix-build.cc
+++ src/nix-build/nix-build.cc
@@ -390,6 +390,8 @@ int main(int argc, char ** argv)
                     maybePrintExecError(e);
                 }
 
+                if (dryRun) return;
+
                 // Set the environment.
                 auto env = getEnv();
 
