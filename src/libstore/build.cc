d5e1bffd2a5b5c39f14944fe3ba25414dcd0d508
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2017-07-18 17:51:50 -0400

    Do not try to fill fd_set with fd>=FD_SETSIZE
    This is UB and causes buffer overflow and crash on linux.

--- src/libstore/build.cc
+++ src/libstore/build.cc
@@ -3835,6 +3835,9 @@ void Worker::waitForInput()
     int fdMax = 0;
     for (auto & i : children) {
         for (auto & j : i.fds) {
+            if (j >= FD_SETSIZE) {
+                throw BuildError("reached FD_SETSIZE limit");
+            }
             FD_SET(j, &fds);
             if (j >= fdMax) fdMax = j + 1;
         }
