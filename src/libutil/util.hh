6bb4e3e8fe30a09d174a04295ba074ee006ebb4f
Andy Wingo <wingo@igalia.com>
2017-08-18 11:07:00 +0200

    Remove unused decodeOctalEscaped
    Besides being unused, this function has a bug that it will incorrectly
    decode the path component Ubuntu\04016.04.2\040LTS\040amd64 as
    "Ubuntu.04.2 LTS amd64" instead of "Ubuntu 16.04.2 LTS amd64".

--- src/libutil/util.hh
+++ src/libutil/util.hh
@@ -349,12 +349,6 @@ bool hasSuffix(const string & s, const string & suffix);
 std::string toLower(const std::string & s);
 
 
-/* Escape a string that contains octal-encoded escape codes such as
-   used in /etc/fstab and /proc/mounts (e.g. "foo\040bar" decodes to
-   "foo bar"). */
-string decodeOctalEscaped(const string & s);
-
-
 /* Exception handling in destructors: print an error message, then
    ignore the exception. */
 void ignoreException();
