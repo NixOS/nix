b8d446829eb9a0f65e314039e0f910a3aceaa143
Eelco Dolstra <edolstra@gmail.com>
2017-11-14 14:37:39 +0100

    Add some tests

--- tests/nar-access.sh
+++ tests/nar-access.sh
@@ -5,19 +5,30 @@ storePath="$(nix-build nar-access.nix -A a --no-out-link)"
 
 cd "$TEST_ROOT"
 
-echo "dumping path to nar"
+# Dump path to nar.
 narFile="$TEST_ROOT/path.nar"
 nix-store --dump $storePath > $narFile
 
-echo "check that find and ls-nar match"
+# Check that find and ls-nar match.
 ( cd $storePath; find . | sort ) > files.find
 nix ls-nar -R -d $narFile "" | sort > files.ls-nar
 diff -u files.find files.ls-nar
 
-echo "check that file contents of data match"
+# Check that file contents of data match.
 nix cat-nar $narFile /foo/data > data.cat-nar
 diff -u data.cat-nar $storePath/foo/data
 
-echo "check that file contents of baz match"
+# Check that file contents of baz match.
 nix cat-nar $narFile /foo/baz > baz.cat-nar
 diff -u baz.cat-nar $storePath/foo/baz
+
+nix cat-store $storePath/foo/baz > baz.cat-nar
+diff -u baz.cat-nar $storePath/foo/baz
+
+# Test --json.
+[[ $(nix ls-nar --json $narFile /) = '{"type":"directory","entries":{"foo":{},"foo-x":{},"qux":{},"zyx":{}}}' ]]
+[[ $(nix ls-nar --json -R $narFile /foo) = '{"type":"directory","entries":{"bar":{"type":"regular","size":0},"baz":{"type":"regular","size":0},"data":{"type":"regular","size":58}}}' ]]
+[[ $(nix ls-nar --json -R $narFile /foo/bar) = '{"type":"regular","size":0}' ]]
+[[ $(nix ls-store --json $storePath) = '{"type":"directory","entries":{"foo":{},"foo-x":{},"qux":{},"zyx":{}}}' ]]
+[[ $(nix ls-store --json -R $storePath/foo) = '{"type":"directory","entries":{"bar":{"type":"regular","size":0},"baz":{"type":"regular","size":0},"data":{"type":"regular","size":58}}}' ]]
+[[ $(nix ls-store --json -R $storePath/foo/bar) = '{"type":"regular","size":0}' ]]
