9304fde8de3b9a54200025701199dcb51725c8b1
Eelco Dolstra <edolstra@gmail.com>
2018-01-22 16:56:10 +0100

    Don't access tarballs.nixos.org in a test
    https://hydra.nixos.org/build/67806811

--- tests/check.sh
+++ tests/check.sh
@@ -16,17 +16,17 @@ nix-build dependencies.nix --no-out-link --repeat 3
 (! nix-build check.nix -A nondeterministic --no-out-link --repeat 1 2> $TEST_ROOT/log)
 grep 'differs from previous round' $TEST_ROOT/log
 
-path=$(nix-build check.nix -A fetchurl --no-out-link)
+path=$(nix-build check.nix -A fetchurl --no-out-link --hashed-mirrors '')
 
 chmod +w $path
 echo foo > $path
 chmod -w $path
 
-nix-build check.nix -A fetchurl --no-out-link --check
+nix-build check.nix -A fetchurl --no-out-link --check --hashed-mirrors ''
 
 # Note: "check" doesn't repair anything, it just compares to the hash stored in the database.
 [[ $(cat $path) = foo ]]
 
-nix-build check.nix -A fetchurl --no-out-link --repair
+nix-build check.nix -A fetchurl --no-out-link --repair --hashed-mirrors ''
 
 [[ $(cat $path) != foo ]]
