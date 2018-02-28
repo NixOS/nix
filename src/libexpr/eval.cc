7e989da790426ea2af7d63b8a64dfa8c28492ea8
Shea Levy <shea@shealevy.com>
2018-02-28 06:19:40 -0500

    Actually fix nixDataDir in non-canonical path
    (cherry picked from commit 14ca85688cdd17dfcf4b2bd8c324f21d6f3d1dde)

--- src/libexpr/eval.cc
+++ src/libexpr/eval.cc
@@ -313,7 +313,7 @@ EvalState::EvalState(const Strings & _searchPath, ref<Store> store)
         for (auto & i : _searchPath) addToSearchPath(i);
         for (auto & i : paths) addToSearchPath(i);
     }
-    addToSearchPath("nix=" + canonPath(settings.nixDataDir + "/nix/corepkgs"));
+    addToSearchPath("nix=" + canonPath(settings.nixDataDir + "/nix/corepkgs", true));
 
     if (settings.restrictEval || settings.pureEval) {
         allowedPaths = PathSet();
