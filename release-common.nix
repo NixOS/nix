5ee4472b8bd5a3bda25599eb710f2c979e9c8876
Aleksandr Pashkov <pashkov.alex+github@gmail.com>
2018-06-05 23:13:51 +0300

    tests: more robust check for user namespaces availability (canUseSandbox)
    Issue https://github.com/NixOS/nix/issues/2165

--- release-common.nix
+++ release-common.nix
@@ -57,7 +57,7 @@ rec {
       git
       mercurial
     ]
-    ++ lib.optional stdenv.isLinux libseccomp
+    ++ lib.optional stdenv.isLinux [libseccomp pkgs.utillinux]
     ++ lib.optional (stdenv.isLinux || stdenv.isDarwin) libsodium
     ++ lib.optional (stdenv.isLinux || stdenv.isDarwin)
       (aws-sdk-cpp.override {
