bb6e6923f25841874b6a915d234d884ddd4c92dd
Eelco Dolstra <edolstra@gmail.com>
2019-04-11 23:04:13 +0200

    Add environment variable NIX_SHOW_SYMBOLS for dumping the symbol table

--- src/libexpr/eval.cc
+++ src/libexpr/eval.cc
@@ -1811,6 +1811,7 @@ void EvalState::printStats()
             gc.attr("totalBytes", totalBytes);
         }
 #endif
+
         if (countCalls) {
             {
                 auto obj = topObj.object("primops");
@@ -1845,6 +1846,11 @@ void EvalState::printStats()
                     obj.attr("count", i.second);
                 }
             }
+        }
+
+        if (getEnv("NIX_SHOW_SYMBOLS", "0") != "0") {
+            auto list = topObj.list("symbols");
+            symbols.dump([&](const std::string & s) { list.elem(s); });
         }
     }
 }
