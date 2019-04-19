3b9c9d34e5ede028860e919d72cc5ae33dd95443
Eelco Dolstra <edolstra@gmail.com>
2019-04-19 14:41:06 +0200

    Shut up clang warning
    (cherry picked from commit 3392f1b77869269580b58e4931b7a79f44799ce0)

--- src/nix/main.cc
+++ src/nix/main.cc
@@ -104,7 +104,7 @@ struct NixArgs : virtual MultiCommand, virtual MixCommonArgs
             "--help-config' for a list of configuration settings.\n";
     }
 
-    void printHelp(const string & programName, std::ostream & out)
+    void printHelp(const string & programName, std::ostream & out) override
     {
         MultiCommand::printHelp(programName, out);
 
