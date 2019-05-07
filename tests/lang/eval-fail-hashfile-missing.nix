a83486187616fd2de877ce72f5fd419385a2c541
Will Dietz <w@wdtz.org>
2019-05-07 17:32:01 -0500

    fix hashfile test that wasn't failing due to eval laziness
    See:
    https://github.com/NixOS/nix/commit/7becb1bf1c2ec1544a5374580a97b36273506baf#r33450554

--- tests/lang/eval-fail-hashfile-missing.nix
+++ tests/lang/eval-fail-hashfile-missing.nix
@@ -1,5 +1,5 @@
 let
   paths = [ ./this-file-is-definitely-not-there-7392097 "/and/neither/is/this/37293620" ];
 in
-  builtins.concatLists (map (hash: map (builtins.hashFile hash) paths) ["md5" "sha1" "sha256" "sha512"])
+  toString (builtins.concatLists (map (hash: map (builtins.hashFile hash) paths) ["md5" "sha1" "sha256" "sha512"]))
 
