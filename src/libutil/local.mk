a96006d97fc87c5073f9a39db841625bdb7c401c
Niklas Hambüchen <mail@nh2.me>
2019-07-03 02:47:27 +0200

    Get BOOST_LDFLAGS from autoconf, fix Ubuntu 16.04 build.
    Our use of boost::coroutine2 depends on -lboost_context,
    which in turn depends on `-lboost_thread`, which in turn depends
    on `-lboost_system`.
    
    I suspect that this builds on nix only because of low-level hacks
    like NIX_LDFLAGS.
    
    This commit passes the proper linker flags, thus fixing bootstrap
    builds on non-nix distributions like Ubuntu 16.04.
    
    With these changes, I can build Nix on Ubuntu 16.04 using:
    
        ./bootstrap.sh
        ./configure --prefix=$HOME/editline-prefix \
          --disable-doc-gen \
          CXX=g++-7 \
          --with-boost=$HOME/boost-prefix \
          EDITLINE_CFLAGS=-I$HOME/editline-prefix/include \
          EDITLINE_LIBS=-leditline \
          LDFLAGS=-L$HOME/editline-prefix/lib
        make
    
    where
    
    * g++-7 comes from gcc-7 from
      https://launchpad.net/~ubuntu-toolchain-r/+archive/ubuntu/test,
    * editline 1.14 from https://github.com/troglobit/editline/releases/tag/1.14.0
    	was installed into `$HOME/editline-prefix`
      (because Ubuntu 16.04's `editline` is too old to have the function nix uses),
    * boost 1.66 from
    	https://www.boost.org/doc/libs/1_66_0/more/getting_started/unix-variants.html
    	was installed into $HOME/boost-prefix (because Ubuntu 16.04 only has 1.58)

--- src/libutil/local.mk
+++ src/libutil/local.mk
@@ -6,4 +6,4 @@ libutil_DIR := $(d)
 
 libutil_SOURCES := $(wildcard $(d)/*.cc)
 
-libutil_LDFLAGS = $(LIBLZMA_LIBS) -lbz2 -pthread $(OPENSSL_LIBS) $(LIBBROTLI_LIBS) -lboost_context
+libutil_LDFLAGS = $(LIBLZMA_LIBS) -lbz2 -pthread $(OPENSSL_LIBS) $(LIBBROTLI_LIBS) $(BOOST_LDFLAGS) -lboost_context
