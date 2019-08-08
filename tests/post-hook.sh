2053ac774797f237a5335c4232bc1ed8902686cf
Eelco Dolstra <edolstra@gmail.com>
2019-08-08 12:18:46 +0200

    Rename file for consistency

--- tests/post-hook.sh
+++ tests/post-hook.sh
@@ -5,7 +5,7 @@ clearStore
 export REMOTE_STORE=$TEST_ROOT/remote_store
 
 # Build the dependencies and push them to the remote store
-nix-build dependencies.nix --post-build-hook $PWD/push_to_store.sh
+nix-build dependencies.nix --post-build-hook $PWD/push-to-store.sh
 
 clearStore
 
