73728874ab89d77bce3b103acb239d4d5eec14b1
Eelco Dolstra <edolstra@gmail.com>
2019-08-27 17:01:54 +0200

    Hopefully fix post-hook test on macOS
    https://hydra.nixos.org/build/99262744

--- tests/push-to-store.sh
+++ tests/push-to-store.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 echo Pushing "$@" to "$REMOTE_STORE"
-echo -n "$OUT_PATHS" | xargs -d: nix copy --to "$REMOTE_STORE" --no-require-sigs
+printf "%s" "$OUT_PATHS" | xargs -d: nix copy --to "$REMOTE_STORE" --no-require-sigs
