8d7e99fcade7a2eaca628d6260a15a4c32f39165
John Ericson <git@JohnEricson.me>
2019-10-23 15:53:55 -0400

    Handle conditional windows dep on libbacktrace in the build system

--- src/libutil/local.mk
+++ src/libutil/local.mk
@@ -6,10 +6,12 @@ libutil_DIR := $(d)
 
 libutil_SOURCES := $(wildcard $(d)/*.cc)
 
+libutil_LDFLAGS = $(LIBLZMA_LIBS) -lbz2 -pthread $(OPENSSL_LIBS) $(LIBBROTLI_LIBS)
+
 ifeq (MINGW,$(findstring MINGW,$(OS)))
-libutil_LDFLAGS = $(LIBLZMA_LIBS) -lbz2 -pthread $(OPENSSL_LIBS) $(LIBBROTLI_LIBS) -lbacktrace -lboost_context-mt
+libutil_LDFLAGS += -lbacktrace -lboost_context-mt
 else
-libutil_LDFLAGS = $(LIBLZMA_LIBS) -lbz2 -pthread $(OPENSSL_LIBS) $(LIBBROTLI_LIBS) -lboost_context
+libutil_LDFLAGS += -lboost_context
 endif
 
 libutil_LIBS = libformat
