48f0a76372d5e5be86f6522c760ae1f6c12dbd65
Eelco Dolstra <edolstra@gmail.com>
2019-11-07 18:31:16 +0100

    Fix installerScript job
    https://hydra.nixos.org/build/105961653

--- release.nix
+++ release.nix
@@ -300,7 +300,7 @@ let
 
           substitute ${./scripts/install.in} $out/install \
             ${pkgs.lib.concatMapStrings
-              (system: "--replace '@binaryTarball_${system}@' $(nix hash-file --base16 --type sha256 ${binaryTarball.${system}}/*.tar.xz) ")
+              (system: "--replace '@binaryTarball_${system}@' $(nix --experimental-features nix-command hash-file --base16 --type sha256 ${binaryTarball.${system}}/*.tar.xz) ")
               [ "x86_64-linux" "i686-linux" "x86_64-darwin" "aarch64-linux" ]
             } \
             --replace '@nixVersion@' ${build.x86_64-linux.src.version}
