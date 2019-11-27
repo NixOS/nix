f553a8bdea96afe18b97dee6eba91c0bbc545b79
Eelco Dolstra <edolstra@gmail.com>
2019-11-27 14:18:57 +0100

    When OPTIMIZE=0, build rust code in debug mode

--- nix-rust/local.mk
+++ nix-rust/local.mk
@@ -1,9 +1,19 @@
-libnixrust_PATH := $(d)/target/release/libnixrust.a
+ifeq ($(OPTIMIZE), 1)
+  RUST_MODE = --release
+  RUST_DIR = release
+else
+  RUST_MODE =
+  RUST_DIR = debug
+endif
+
+libnixrust_PATH := $(d)/target/$(RUST_DIR)/libnixrust.a
 libnixrust_INSTALL_PATH := $(libnixrust_PATH)
-libnixrust_LDFLAGS_USE := -L$(d)/target/release -lnixrust -ldl
+libnixrust_LDFLAGS_USE := -L$(d)/target/$(RUST_DIR) -lnixrust -ldl
 libnixrust_LDFLAGS_USE_INSTALLED := $(libnixrust_LDFLAGS_USE)
 
-$(d)/target/release/libnixrust.a: $(wildcard $(d)/src/*.rs) $(d)/Cargo.toml
-	$(trace-gen) cd nix-rust && CARGO_HOME=$$(if [[ -d vendor ]]; then echo vendor; fi) cargo build --release $$(if [[ -d vendor ]]; then echo --offline; fi) && touch target/release/libnixrust.a
+$(d)/target/$(RUST_DIR)/libnixrust.a: $(wildcard $(d)/src/*.rs) $(d)/Cargo.toml
+	$(trace-gen) cd nix-rust && CARGO_HOME=$$(if [[ -d vendor ]]; then echo vendor; fi) \
+	cargo build $(RUST_MODE) $$(if [[ -d vendor ]]; then echo --offline; fi) \
+	&& touch target/$(RUST_DIR)/libnixrust.a
 
 dist-files += $(d)/vendor
