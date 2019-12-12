a045f93396a676bf5510700c9ac19f5c16bd69e8
Eelco Dolstra <edolstra@gmail.com>
2019-12-12 15:07:31 +0100

    Temporarily revert to using 'tar' until we have gzip support

--- src/libstore/download.cc
+++ src/libstore/download.cc
@@ -910,6 +910,7 @@ CachedDownloadResult Downloader::downloadCached(
             printInfo("unpacking '%s'...", url);
             Path tmpDir = createTempDir();
             AutoDelete autoDelete(tmpDir, true);
+#if 0
             unpackTarfile(store->toRealPath(store->printStorePath(*storePath)), tmpDir, std::string(baseNameOf(url)));
             auto members = readDirectory(tmpDir);
             if (members.size() != 1)
@@ -917,6 +918,12 @@ CachedDownloadResult Downloader::downloadCached(
             auto topDir = tmpDir + "/" + members.begin()->name;
             result.lastModified = lstat(topDir).st_mtime;
             unpackedStorePath = store->addToStore(name, topDir, true, htSHA256, defaultPathFilter, NoRepair);
+#else
+            // FIXME: this requires GNU tar for decompression.
+            runProgram("tar", true, {"xf", store->toRealPath(store->printStorePath(*storePath)), "-C", tmpDir, "--strip-components", "1"});
+            result.lastModified = lstat(tmpDir).st_mtime;
+            unpackedStorePath = store->addToStore(name, tmpDir, true, htSHA256, defaultPathFilter, NoRepair);
+#endif
         }
         // Store the last-modified date of the tarball in the symlink
         // mtime. This saves us from having to store it somewhere
