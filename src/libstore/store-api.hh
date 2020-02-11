777e21e596ec85f18eddf8b286d9ba4995010835
Eelco Dolstra <edolstra@gmail.com>
2020-02-11 23:50:16 +0100

    nix path-info --json: Print hash in SRI format
    (cherry picked from commit 442e665d6d3fcbdee7dece2f62a597142f8784b1)

--- src/libstore/store-api.hh
+++ src/libstore/store-api.hh
@@ -561,6 +561,7 @@ public:
        each path is included. */
     void pathInfoToJSON(JSONPlaceholder & jsonOut, const StorePathSet & storePaths,
         bool includeImpureInfo, bool showClosureSize,
+        Base hashBase = Base32,
         AllowInvalidFlag allowInvalid = DisallowInvalid);
 
     /* Return the size of the closure of the specified path, that is,
