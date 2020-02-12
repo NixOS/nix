9659940659b36ffab3cbd1eff9f343c9ada2491b
Eelco Dolstra <edolstra@gmail.com>
2020-02-12 10:33:16 +0100

    Test narHash mismatch

--- tests/tarball.sh
+++ tests/tarball.sh
@@ -30,6 +30,7 @@ test_tarball() {
     nix-build -o $TEST_ROOT/result -E "import (fetchTree file://$tarball)"
     nix-build -o $TEST_ROOT/result -E "import (fetchTree { type = \"tarball\"; url = file://$tarball; })"
     nix-build -o $TEST_ROOT/result -E "import (fetchTree { type = \"tarball\"; url = file://$tarball; narHash = \"$hash\"; })"
+    nix-build -o $TEST_ROOT/result -E "import (fetchTree { type = \"tarball\"; url = file://$tarball; narHash = \"sha256-xdKv2pq/IiwLSnBBJXW8hNowI4MrdZfW+SYqDQs7Tzc=\"; })" 2>&1 | grep 'NAR hash mismatch in input'
 
     nix-instantiate --eval -E '1 + 2' -I fnord=file://no-such-tarball.tar$ext
     nix-instantiate --eval -E 'with <fnord/xyzzy>; 1 + 2' -I fnord=file://no-such-tarball$ext
