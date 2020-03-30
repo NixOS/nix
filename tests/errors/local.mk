28d073e810d5e692a4f6c57d23afc011e6fcc153
Ben Burdette <bburdette@gmail.com>
2020-03-30 09:15:21 -0600

    remove cruft

--- tests/errors/local.mk
+++ tests/errors/local.mk
@@ -8,10 +8,3 @@ error-test_SOURCES := \
 error-test_LIBS = libutil
 
 error-test_LDFLAGS = -pthread $(SODIUM_LIBS) $(EDITLINE_LIBS) $(BOOST_LDFLAGS) -lboost_context -lboost_thread -lboost_system
-
-# $(foreach name, \
-#   nix-build nix-channel nix-collect-garbage nix-copy-closure nix-daemon nix-env nix-hash nix-instantiate nix-prefetch-url nix-shell nix-store, \
-#   $(eval $(call install-symlink, nix, $(bindir)/$(name))))
-# $(eval $(call install-symlink, $(bindir)/nix, $(libexecdir)/nix/build-remote))
-
-# src/nix-env/user-env.cc: src/nix-env/buildenv.nix.gen.hh
