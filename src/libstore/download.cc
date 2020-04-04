c976cb0b8ab5d0f2c4ab8c9826fc7db56e2f1b3e
Cole Helbling <cole.e.helbling@outlook.com>
2020-04-04 12:27:39 -0700

    Don't retry on "unsupported protocol" error
    When encountering an unsupported protocol, there's no need to retry.
    Chances are, it won't suddenly be supported between retry attempts;
    error instead. Otherwise, you see something like the following:
    
        $ nix-env -i -f git://git@github.com/foo/bar
        warning: unable to download 'git://git@github.com/foo/bar': Unsupported protocol (1); retrying in 335 ms
        warning: unable to download 'git://git@github.com/foo/bar': Unsupported protocol (1); retrying in 604 ms
        warning: unable to download 'git://git@github.com/foo/bar': Unsupported protocol (1); retrying in 1340 ms
        warning: unable to download 'git://git@github.com/foo/bar': Unsupported protocol (1); retrying in 2685 ms
    
    With this change, you now see:
    
        $ nix-env -i -f git://git@github.com/foo/bar
        error: unable to download 'git://git@github.com/foo/bar': Unsupported protocol (1)

--- src/libstore/download.cc
+++ src/libstore/download.cc
@@ -390,6 +390,7 @@ struct CurlDownloader : public Downloader
                         case CURLE_SSL_CACERT_BADFILE:
                         case CURLE_TOO_MANY_REDIRECTS:
                         case CURLE_WRITE_ERROR:
+                        case CURLE_UNSUPPORTED_PROTOCOL:
                             err = Misc;
                             break;
                         default: // Shut up warnings
