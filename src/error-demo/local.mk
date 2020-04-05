1221ae3dd07959d47d9f27e9d2271671003d2bed
Ben Burdette <bburdette@gmail.com>
2020-04-05 07:12:16 -0600

    libexpr

--- src/error-demo/local.mk
+++ src/error-demo/local.mk
@@ -5,6 +5,6 @@ error-demo_DIR := $(d)
 error-demo_SOURCES := \
   $(wildcard $(d)/*.cc) \
 
-error-demo_LIBS = libutil
+error-demo_LIBS = libutil libexpr
 
 error-demo_LDFLAGS = -pthread $(SODIUM_LIBS) $(EDITLINE_LIBS) $(BOOST_LDFLAGS) -lboost_context -lboost_thread -lboost_system
