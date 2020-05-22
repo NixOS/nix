a219981360d0cbba422c8982475db3a1fc274130
Michael Bishop <cleverca22@gmail.com>
2020-05-22 15:59:09 -0300

    fix segfault and type changing weirdness caused by use-after-free bugs, caused by lack of GC roots

--- src/nix/repl.cc
+++ src/nix/repl.cc
@@ -97,12 +97,14 @@ NixRepl::NixRepl(const Strings & searchPath, nix::ref<Store> store)
     , historyFile(getDataDir() + "/nix/repl-history")
 {
     curDir = absPath(".");
+    GC_add_roots(this, ((void*)this) + sizeof(NixRepl));
 }
 
 
 NixRepl::~NixRepl()
 {
     write_history(historyFile.c_str());
+    GC_remove_roots(this, ((void*)this) + sizeof(NixRepl));
 }
 
 static NixRepl * curRepl; // ugly
