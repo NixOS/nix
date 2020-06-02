75d2581390e70c9f5820311ff25ae13521ad07e2
Carlo Nucera <carlo.nucera@protonmail.com>
2020-06-02 16:21:18 -0400

    Typo

--- src/libstore/content-address.cc
+++ src/libstore/content-address.cc
@@ -51,7 +51,7 @@ ContentAddress parseContentAddress(std::string_view rawCa) {
             }
             return TextHash { hash };
         } else if (prefix == "fixed") {
-            // This has to be an inverse to makeFixedOutputCA
+            // This has to be an inverse of makeFixedOutputCA
             auto methodAndHash = rawCa.substr(prefixSeparator+1, string::npos);
             if (methodAndHash.substr(0,2) == "r:") {
                 std::string_view hashRaw = methodAndHash.substr(2,string::npos);
