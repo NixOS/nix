8a3c07326bb5e9ea79c4088729c23ba6ec4e7a21
Carlo Nucera <carlo.nucera@protonmail.com>
2020-06-11 17:18:08 -0400

    Complete the tests for ipfs

--- tests/ipfs.sh
+++ tests/ipfs.sh
@@ -1,5 +1,9 @@
 source common.sh
 
+# This are for ./fixed.nix
+export IMPURE_VAR1=foo
+export IMPURE_VAR2=bar
+
 ################################################################################
 ## Check that the ipfs daemon is enabled in your environment
 ################################################################################
@@ -18,6 +22,7 @@ fi
 ################################################################################
 
 IPFS_TESTS=$TEST_ROOT/ipfs_tests
+mkdir $IPFS_TESTS
 
 # Here we define some store locations, one for the initial store we upload, and
 # the other three for the destination stores to which we'll copy (one for each
@@ -33,9 +38,9 @@ IPFS_DST_IPNS_STORE=$IPFS_TESTS/ipfs_dest_ipns_store
 ## Generate the keys to sign the store
 ################################################################################
 
-SIGNING_KEY_NAME='nixcache.for.ipfs-1'
-SIGNING_KEY_PRI_FILE='nix-cache-key.sec'
-SIGNING_KEY_PUB_FILE='nix-cache-key.pub'
+SIGNING_KEY_NAME=nixcache.for.ipfs-1
+SIGNING_KEY_PRI_FILE=$IPFS_TESTS/nix-cache-key.sec
+SIGNING_KEY_PUB_FILE=$IPFS_TESTS/nix-cache-key.pub
 
 nix-store --generate-binary-cache-key $SIGNING_KEY_NAME $SIGNING_KEY_PRI_FILE $SIGNING_KEY_PUB_FILE
 
@@ -44,104 +49,68 @@ nix-store --generate-binary-cache-key $SIGNING_KEY_NAME $SIGNING_KEY_PRI_FILE $S
 ################################################################################
 
 mkdir -p $IPFS_SRC_STORE
-# BUILD_COMMAND="nix-build '<nixpkgs>' -A hello.src"
+# BUILD_COMMAND="nix-build ./dependencies.nix -A input1_drv"
+BUILD_COMMAND="nix-build ./fixed.nix -A good"
 
 nix copy --to file://$IPFS_SRC_STORE \
-    --experimental-features nix-command \
-    $(nix-build ./simple-derivation.nix)
+    $($BUILD_COMMAND)
 
 nix sign-paths --store file://$IPFS_SRC_STORE \
-    -k ~/nix-cache-key.sec \
+    -k $SIGNING_KEY_PRI_FILE \
     $($BUILD_COMMAND) -r
 
 IPFS_HASH=$(ipfs add -r $IPFS_SRC_STORE 2>/dev/null | tail -n 1 | awk '{print $2}')
 
+################################################################################
+## Create the local http store and download the derivation there
+################################################################################
 
-# ################################################################################
-# ## Create the http store and download the derivation there
-# ################################################################################
+mkdir $IPFS_DST_HTTP_LOCAL_STORE
 
-# mkdir IPFS_DST_HTTP_STORE
+IPFS_HTTP_LOCAL_PREFIX='http://localhost:8080/ipfs'
 
-# IPFS_HTTP_PREFIX='https://gateway.ipfs.io/ipfs'
+DOWNLOAD_LOCATION=$(NIX_REMOTE=local $BUILD_COMMAND \
+  --option substituters $IPFS_HTTP_LOCAL_PREFIX/$IPFS_HASH \
+  --store $IPFS_DST_HTTP_LOCAL_STORE \
+  --no-out-link \
+  -j0 \
+  --option trusted-public-keys $(cat $SIGNING_KEY_PUB_FILE))
+
+################################################################################
+## Create the ipfs store and download the derivation there
+################################################################################
+
+mkdir $IPFS_DST_IPFS_STORE
+
+IPFS_IPFS_PREFIX='/ipfs'
+
+DOWNLOAD_LOCATION=$(NIX_REMOTE=local $BUILD_COMMAND \
+  --option substituters $IPFS_IPFS_PREFIX/$IPFS_HASH \
+  --store $IPFS_DST_IPFS_STORE \
+  --no-out-link \
+  -j0 \
+  --option trusted-public-keys $(cat $SIGNING_KEY_PUB_FILE))
 
 
-# DOWNLOAD_LOCATION=$(NIX_REMOTE=local $BUILD_COMMAND \
-# --option substituters $IPFS_HTTP_PREFIX/$IPFS_HASH \
-# --store $IPFS_DST_HTTP_STORE \
-# --option trusted-public-keys $(cat $SIGNING_KEY_PUB_FILE) \
-# | tail -2 | head -1 | awk '{print $5}')
+################################################################################
+## Create the ipns store and download the derivation there
+################################################################################
 
-# ### TODO
-# ### Check that the download location coincides
+# First I have to publish:
+IPNS_ID=$(ipfs name publish $IPFS_HASH | awk '{print substr($3,1,length($3)-1)}')
 
-# ################################################################################
-# ## Create the local http store and download the derivation there
-# ################################################################################
+mkdir $IPFS_DST_IPNS_STORE
+IPFS_IPNS_PREFIX='/ipns'
 
-# mkdir IPFS_DST_HTTP_LOCAL_STORE
-
-# IPFS_HTTP_LOCAL_PREFIX='http://localhost:8080/ipfs'
-
-# DOWNLOAD_LOCATION=$(NIX_REMOTE=local $BUILD_COMMAND \
-# --option substituters $IPFS_HTTP_LOCAL_PREFIX/$IPFS_HASH \
-# --store $IPFS_DST_HTTP_LOCAL_STORE \
-# --option trusted-public-keys $(cat $SIGNING_KEY_PUB_FILE) \
-# | tail -2 | head -1 | awk '{print $5}')
-
-# ### TODO
-# ### Check that the download location coincides
-
-# ################################################################################
-# ## Create the ipfs store and download the derivation there
-# ################################################################################
-
-# mkdir IPFS_DST_IPFS_STORE
-
-# IPFS_IPFS_PREFIX='/ipfs'
-
-# DOWNLOAD_LOCATION=$(NIX_REMOTE=local $BUILD_COMMAND \
-# --option substituters $IPFS_IPFS_PREFIX/$IPFS_HASH \
-# --store $IPFS_DST_IPFS_STORE \
-# --option trusted-public-keys $(cat $SIGNING_KEY_PUB_FILE) \
-# | tail -2 | head -1 | awk '{print $5}')
-
-# ### TODO
-# ### Check that the download location coincides
-
-# ################################################################################
-# ## Create the ipns store and download the derivation there
-# ################################################################################
-
-# # First I have to publish:
-# IPNS_ID=$(ipfs name publish $IPFS_HASH | awk '{print substr($3,1,length($3)-1)}')
-
-# mkdir IPFS_DST_IPNS_STORE
-# IPFS_IPNS_PREFIX='/ipns'
-
-# DOWNLOAD_LOCATION=$(NIX_REMOTE=local $BUILD_COMMAND \
-# --option substituters $IPFS_IPNS_PREFIX/$IPNS_ID \
-# --store $IPFS_DST_IPFS_STORE \
-# --option trusted-public-keys $(cat $SIGNING_KEY_PUB_FILE) \
-# | tail -2 | head -1 | awk '{print $5}')
-
-# ### TODO
-# ### Check that the download location coincides
+DOWNLOAD_LOCATION=$(NIX_REMOTE=local $BUILD_COMMAND \
+  --option substituters $IPFS_IPNS_PREFIX/$IPNS_ID \
+  --store $IPFS_DST_IPNS_STORE \
+  --no-out-link \
+  -j0 \
+  --option trusted-public-keys $(cat $SIGNING_KEY_PUB_FILE))
 
 ################################################################################
 ## Cleanup
 ################################################################################
 
-# Delete the keys used to sign the store
-rm $SIGNING_KEY_PRI_FILE $SIGNING_KEY_PUB_FILE
-
-# Remove all the stores
-rm -rf $IPFS_SRC_STORE \
-    $IPFS_DST_HTTP_STORE $IPFS_DST_HTTP_LOCAL_STORE \
-    $IPFS_DST_IPFS_STORE $IPFS_DST_IPNS_STORE
-
-# Remove the result of the build
-rm result
-
-
-exit 1
+# The cleanup is done automatically by nix/tests/init.sh
--- tests/local.mk
+++ tests/local.mk
@@ -1,42 +1,39 @@
-nix_tests = init.sh check.sh ipfs.sh
-
-# nix_tests = add.sh
-
-# nix_tests = \
-#   init.sh hash.sh lang.sh add.sh simple.sh dependencies.sh \
-#   config.sh \
-#   gc.sh \
-#   gc-concurrent.sh \
-#   gc-auto.sh \
-#   referrers.sh user-envs.sh logging.sh nix-build.sh misc.sh fixed.sh \
-#   gc-runtime.sh check-refs.sh filter-source.sh \
-#   remote-store.sh export.sh export-graph.sh \
-#   timeout.sh secure-drv-outputs.sh nix-channel.sh \
-#   multiple-outputs.sh import-derivation.sh fetchurl.sh optimise-store.sh \
-#   binary-cache.sh nix-profile.sh repair.sh dump-db.sh case-hack.sh \
-#   check-reqs.sh pass-as-file.sh tarball.sh restricted.sh \
-#   placeholders.sh nix-shell.sh \
-#   linux-sandbox.sh \
-#   build-dry.sh \
-#   build-remote.sh \
-#   nar-access.sh \
-#   structured-attrs.sh \
-#   fetchGit.sh \
-#   fetchGitRefs.sh \
-#   fetchGitSubmodules.sh \
-#   fetchMercurial.sh \
-#   signing.sh \
-#   shell.sh \
-#   brotli.sh \
-#   pure-eval.sh \
-#   check.sh \
-#   plugins.sh \
-#   search.sh \
-#   nix-copy-ssh.sh \
-#   post-hook.sh \
-#   function-trace.sh \
-#   recursive.sh
-#   # parallel.sh
+nix_tests = \
+  init.sh hash.sh lang.sh add.sh simple.sh dependencies.sh \
+  config.sh \
+  gc.sh \
+  gc-concurrent.sh \
+  gc-auto.sh \
+  referrers.sh user-envs.sh logging.sh nix-build.sh misc.sh fixed.sh \
+  gc-runtime.sh check-refs.sh filter-source.sh \
+  remote-store.sh export.sh export-graph.sh \
+  timeout.sh secure-drv-outputs.sh nix-channel.sh \
+  multiple-outputs.sh import-derivation.sh fetchurl.sh optimise-store.sh \
+  binary-cache.sh nix-profile.sh repair.sh dump-db.sh case-hack.sh \
+  check-reqs.sh pass-as-file.sh tarball.sh restricted.sh \
+  placeholders.sh nix-shell.sh \
+  linux-sandbox.sh \
+  build-dry.sh \
+  build-remote.sh \
+  nar-access.sh \
+  structured-attrs.sh \
+  fetchGit.sh \
+  fetchGitRefs.sh \
+  fetchGitSubmodules.sh \
+  fetchMercurial.sh \
+  signing.sh \
+  shell.sh \
+  brotli.sh \
+  pure-eval.sh \
+  check.sh \
+  plugins.sh \
+  search.sh \
+  nix-copy-ssh.sh \
+  post-hook.sh \
+  function-trace.sh \
+  recursive.sh \
+  ipfs.sh
+  # parallel.sh
 
 install-tests += $(foreach x, $(nix_tests), tests/$(x))
 
