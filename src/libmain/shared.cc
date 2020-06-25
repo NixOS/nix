6359d71d6b6110ce3608cfd289cae5143e78f123
Ben Burdette <bburdette@gmail.com>
2020-06-24 18:28:20 -0600

    re-enable --show-trace check

--- src/libmain/shared.cc
+++ src/libmain/shared.cc
@@ -328,8 +328,8 @@ int handleExceptions(const string & programName, std::function<void()> fun)
         //     printError(e.prefix());
         logError(e.info());
         // TODO fix to detect non-empty trace here.
-        // if (e.prefix() != "" && !settings.showTrace)
-        //     printError("(use '--show-trace' to show detailed location information)");
+        if (e.hasTrace() && !settings.showTrace)
+            printError("(use '--show-trace' to show detailed location information)");
         return e.status;
     } catch (std::bad_alloc & e) {
         printError(error + "out of memory");
