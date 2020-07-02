5101ed18bca509a8cf43668b0701afad90c5c9c4
regnat <rg@regnat.ovh>
2020-07-02 17:15:02 +0200

    Fix the test dependencies
    Reuse the pre-existing list rather than the one written as part of #3777

--- mk/tests.mk
+++ mk/tests.mk
@@ -1,10 +1,13 @@
 # Run program $1 as part of ‘make installcheck’.
+
+test-deps =
+
 define run-install-test
 
   installcheck: $1.test
 
   .PHONY: $1.test
-  $1.test: $1 tests/common.sh tests/init.sh
+  $1.test: $1 $(test-deps)
 	@env TEST_NAME=$1 TESTS_ENVIRONMENT="$(tests-environment)" mk/run_test.sh $1
 
 endef
