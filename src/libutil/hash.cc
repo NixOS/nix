b5e42536977d84359f5fea3f42cfefdd46799eb1
Eelco Dolstra <edolstra@gmail.com>
2020-07-02 18:24:11 +0200

    Fix abort in 'nix develop'

--- src/libutil/hash.cc
+++ src/libutil/hash.cc
@@ -370,7 +370,7 @@ string printHashType(HashType ht)
     default:
         // illegal hash type enum value internally, as opposed to external input
         // which should be validated with nice error message.
-        abort();
+        assert(false);
     }
 }
 
