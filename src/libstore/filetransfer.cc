0ca9744694a5294e995fcddc11f5f195c84036a4
Carlo Nucera <carlo.nucera@protonmail.com>
2020-07-20 15:56:52 -0400

    Use heuristics to decide when to show the response
    Due to https://github.com/NixOS/nix/issues/3841 we don't know how print
    different messages for different verbosity levels.

--- src/libstore/filetransfer.cc
+++ src/libstore/filetransfer.cc
@@ -845,8 +845,11 @@ FileTransferError::FileTransferError(FileTransfer::Error error, std::shared_ptr<
     : Error(args...), error(error), response(response)
 {
     const auto hf = hintfmt(args...);
-    if (response) {
-        err.hint = hintfmt("%1%\n\nresponse body:\n\n%2%", normaltxt(hf.str()), *response);
+    // FIXME: Due to https://github.com/NixOS/nix/issues/3841 we don't know how
+    // to print different messages for different verbosity levels. For now
+    // we add some heuristics for detecting when we want to show the response.
+    if (response && (response->size() < 1024 || response->find("<html>") != string::npos)) {
+            err.hint = hintfmt("%1%\n\nresponse body:\n\n%2%", normaltxt(hf.str()), *response);
     } else {
         err.hint = hf;
     }
