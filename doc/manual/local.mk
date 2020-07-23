efff6cf163fb911689b41d7c2970efd8b17876e1
Eelco Dolstra <edolstra@gmail.com>
2020-07-23 13:09:30 +0200

    Install all manpages

--- doc/manual/local.mk
+++ doc/manual/local.mk
@@ -12,20 +12,21 @@ dist-files += $(d)/version.txt
 
 # Generate man pages.
 man-pages := $(foreach n, \
-  nix-copy-closure.1 \
-  nix.conf.5, \
+  nix-env.1 nix-build.1 nix-shell.1 nix-store.1 nix-instantiate.1 \
+  nix-collect-garbage.1 \
+  nix-prefetch-url.1 nix-channel.1 \
+  nix-hash.1 nix-copy-closure.1 \
+  nix.conf.5 nix-daemon.8, \
   $(d)/$(n))
-#  nix-env.1 nix-build.1 nix-shell.1 nix-store.1 nix-instantiate.1 \
-#  nix-collect-garbage.1, \
-#  nix-prefetch-url.1 nix-channel.1 \
-#  nix-hash.1 nix-copy-closure.1 \
-#  nix.conf.5 nix-daemon.8, \
 
 clean-files += $(d)/*.1 $(d)/*.5 $(d)/*.8
 
 dist-files += $(man-pages)
 
-$(d)/nix-copy-closure.1: $(d)/src/command-ref/nix-copy-closure.md
+$(d)/%.1: $(d)/src/command-ref/%.md
+	$(trace-gen) lowdown -sT man $^ -o $@
+
+$(d)/%.8: $(d)/src/command-ref/%.md
 	$(trace-gen) lowdown -sT man $^ -o $@
 
 $(d)/nix.conf.5: $(d)/src/command-ref/conf-file.md
