b2d6c6161e61c47f4d8a3faa5799160d100f4e8c
Eelco Dolstra <edolstra@gmail.com>
2020-07-24 18:44:43 +0200

    Move 'nix hash-*' and 'nix to-*' to 'nix hash'
    From the 'nix' UX review.

--- tests/pure-eval.sh
+++ tests/pure-eval.sh
@@ -15,7 +15,7 @@ nix eval --expr 'assert 1 + 2 == 3; true'
 
 [[ $(nix eval --impure --expr "(import (builtins.fetchurl { url = file://$(pwd)/pure-eval.nix; })).x") == 123 ]]
 (! nix eval --expr "(import (builtins.fetchurl { url = file://$(pwd)/pure-eval.nix; })).x")
-nix eval --expr "(import (builtins.fetchurl { url = file://$(pwd)/pure-eval.nix; sha256 = \"$(nix hash-file pure-eval.nix --type sha256)\"; })).x"
+nix eval --expr "(import (builtins.fetchurl { url = file://$(pwd)/pure-eval.nix; sha256 = \"$(nix hash file pure-eval.nix --type sha256)\"; })).x"
 
 rm -rf $TEST_ROOT/eval-out
 nix eval --store dummy:// --write-to $TEST_ROOT/eval-out --expr '{ x = "foo" + "bar"; y = { z = "bla"; }; }'
