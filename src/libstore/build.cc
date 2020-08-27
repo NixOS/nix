6c5827c2116f89143cb3a30deb5047d1e53c72a7
John Ericson <John.Ericson@Obsidian.Systems>
2020-08-27 23:54:43 +0000

    Don't dereference `maintainExpectedNar` uncondititonally

--- src/libstore/build.cc
+++ src/libstore/build.cc
@@ -4654,7 +4654,8 @@ void SubstitutionGoal::finished()
         worker.doneDownloadSize += fileSize;
     }
 
-    worker.doneNarSize += maintainExpectedNar->delta;
+    if (maintainExpectedNar)
+        worker.doneNarSize += maintainExpectedNar->delta;
     maintainExpectedNar.reset();
 
     worker.updateProgress();
