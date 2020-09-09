7bf6cdb003b3b5c18e608449d1b154dd91adf0e6
John Ericson <John.Ericson@Obsidian.Systems>
2020-09-09 03:37:35 +0000

    Fix and outline some comparison operators

--- src/libstore/content-address.hh
+++ src/libstore/content-address.hh
@@ -12,24 +12,16 @@ enum struct FileIngestionMethod : uint8_t {
 
 struct TextHash {
     Hash hash;
-    bool operator ==(TextHash otherHash) const noexcept {
-        return hash == otherHash.hash;
-    };
-    bool operator !=(TextHash otherHash) const noexcept {
-        return hash != otherHash.hash;
-    };
+    bool operator ==(const TextHash & otherHash) const noexcept;
+    bool operator !=(const TextHash & otherHash) const noexcept;
 };
 
 /// Pair of a hash, and how the file system was ingested
 struct FixedOutputHash {
     FileIngestionMethod method;
     Hash hash;
-    bool operator ==(FixedOutputHash otherHash) const noexcept {
-        return method == otherHash.method && hash == otherHash.hash;
-    };
-    bool operator !=(FixedOutputHash otherHash) const noexcept {
-        return method != otherHash.method && hash == otherHash.hash;
-    };
+    bool operator ==(const FixedOutputHash & therHash) const noexcept;
+    bool operator !=(const FixedOutputHash & therHash) const noexcept;
     std::string printMethodAlgo() const;
 };
 
