e40772cd35adcd158d30727f7f294b823df8010a
Marwan Aljubeh <marwan.aljubeh@gmail.com>
2020-09-18 17:18:45 +0100

    Lint issue: replacing tabs with spaces

--- src/libstore/globals.hh
+++ src/libstore/globals.hh
@@ -885,11 +885,11 @@ public:
     Setting<bool> ignoreSymlinkStore{
         this, false, "ignore-symlink-store",
         R"(
-	  If set to `true`, Nix will stop complaining if the store directory
-	  (typically /nix/store) contains symlink components.
+          If set to `true`, Nix will stop complaining if the store directory
+          (typically /nix/store) contains symlink components.
 
-	  This risks making some builds "impure" because builders sometimes
-	  "canonicalise" paths by resolving all symlink components. Problems
+          This risks making some builds "impure" because builders sometimes
+          "canonicalise" paths by resolving all symlink components. Problems
           occur if those builds are then deployed to machines where /nix/store
           resolves to a different location from that of the build machine. You
           can enable this setting if you are sure you're not going to do that.
