d723fa3bc349c1f9af5724a20a06e79d07f45a98
Travis A. Everett <travis.a.everett@gmail.com>
2020-11-04 19:57:11 -0600

    prepare darwin-multi for create-volume

--- scripts/install-darwin-multi-user.sh
+++ scripts/install-darwin-multi-user.sh
@@ -3,36 +3,62 @@
 set -eu
 set -o pipefail
 
-readonly PLIST_DEST=/Library/LaunchDaemons/org.nixos.nix-daemon.plist
+readonly NIX_DAEMON_DEST=/Library/LaunchDaemons/org.nixos.nix-daemon.plist
+# create by default; set 0 to DIY, use a symlink, etc.
+readonly NIX_VOLUME_CREATE=${NIX_VOLUME_CREATE:-1} # now default
+
+# TODO: I'm trying to decide between how well-contained the darwin-volume
+# stuff should stay here. Current goal is to source create-darwin-volume.sh
+# and leave most of the code there, which accomplishes my main goals:
+# 1. take advantage of the UI/X affordances here
+# 2. retain some degree of invokability (a kindness to anyone who has a script
+#    set up to download and run it)?
+. "$EXTRACTED_NIX_PATH/create-darwin-volume.sh" "no-main"
 
 dsclattr() {
     /usr/bin/dscl . -read "$1" \
         | awk "/$2/ { print \$2 }"
 }
 
+test_nix_daemon_installed() {
+  test -e "$NIX_DAEMON_DEST"
+}
+
 poly_validate_assumptions() {
     if [ "$(uname -s)" != "Darwin" ]; then
         failure "This script is for use with macOS!"
     fi
+    writable="$(diskutil info -plist / | xmllint --xpath "name(/plist/dict/key[text()='Writable']/following-sibling::*[1])" -)"
+
+    if ! [ -e $NIX_ROOT ] && [ "$writable" = "false" ] && [ "$NIX_VOLUME_CREATE" = 1 ]; then
+        # we need to create a Nix volume
+        darwin_volume_validate_assumptions
+    fi
 }
 
 poly_service_installed_check() {
-    [ -e "$PLIST_DEST" ]
+    test_nix_daemon_installed || test_nix_volume_mountd_installed
 }
 
 poly_service_uninstall_directions() {
-        cat <<EOF
-$1. Delete $PLIST_DEST
+    echo "$1. Remove macOS-specific components:"
+    darwin_volume_uninstall_directions
+    if test_nix_daemon_installed; then
+        nix_daemon_uninstall_directions
+    fi
+}
 
-  sudo launchctl unload $PLIST_DEST
-  sudo rm $PLIST_DEST
-
-EOF
+poly_service_uninstall_prompts() {
+    echo "$1. Remove macOS-specific components:"
+    darwin_volume_uninstall_prompts
+    if test_nix_daemon_installed; then
+        nix_daemon_uninstall_prompt
+    fi
 }
 
 poly_service_setup_note() {
     cat <<EOF
- - load and start a LaunchDaemon (at $PLIST_DEST) for nix-daemon
+ - load and start a LaunchDaemon (at $NIX_DAEMON_DEST) for nix-daemon
 
 EOF
 }
@@ -46,7 +72,7 @@ poly_extra_setup_instructions(){
 
 poly_configure_nix_daemon_service() {
     _sudo "to set up the nix-daemon as a LaunchDaemon" \
-          cp -f "/nix/var/nix/profiles/default$PLIST_DEST" "$PLIST_DEST"
+          cp -f "/nix/var/nix/profiles/default$NIX_DAEMON_DEST" "$NIX_DAEMON_DEST"
 
     _sudo "to load the LaunchDaemon plist for nix-daemon" \
           launchctl load /Library/LaunchDaemons/org.nixos.nix-daemon.plist
@@ -148,4 +174,8 @@ poly_create_build_user() {
     _sudo "Creating the Nix build user (#$builder_num), $username" \
           /usr/bin/dscl . create "/Users/$username" \
           UniqueID "${uid}"
+}
+
+poly_prepare_to_install() {
+    setup_darwin_volume
 }
