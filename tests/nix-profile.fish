415dcdf3700efda26075d9fc910056fd0f417051
kineolyan <kineolyan@protonmail.com>
2021-04-11 14:59:55 +0200

    Remove test for fish profile.

--- tests/nix-profile.fish
+++
@@ -1,9 +0,0 @@
-source common.sh
-
-sed -e "s|@localstatedir@|$TEST_ROOT/profile-var|g" -e "s|@coreutils@|$coreutils|g" < ../scripts/nix-profile.fish.in > $TEST_ROOT/nix-profile.fish
-
-user=$(whoami)
-rm -rf $TEST_HOME $TEST_ROOT/profile-var
-mkdir -p $TEST_HOME
-USER=$user $SHELL -e -c ". $TEST_ROOT/nix-profile.fish; set"
-USER=$user $SHELL -e -c ". $TEST_ROOT/nix-profile.fish" # test idempotency
