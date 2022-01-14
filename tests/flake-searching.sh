e3690ab39382498eaabbd07e696335e17c9f209c
Alexander Bantyev <balsoft@balsoft.ru>
2022-01-14 17:15:45 +0300

    Add more tests for flake upward searching

--- tests/flake-searching.sh
+++ tests/flake-searching.sh
@@ -4,8 +4,11 @@ clearStore
 
 cp ./simple.nix ./simple.builder.sh ./config.nix $TEST_HOME
 cd $TEST_HOME
+mkdir -p foo/subdir
+echo '{ outputs = _: {}; }' > foo/flake.nix
 cat <<EOF > flake.nix
 {
+    inputs.foo.url = "$PWD/foo";
     outputs = a: {
        defaultPackage.$system = import ./simple.nix;
        packages.$system.test = import ./simple.nix;
@@ -13,12 +16,19 @@ cat <<EOF > flake.nix
 }
 EOF
 mkdir subdir
-cd subdir
+pushd subdir
 
-for i in "" . "$PWD" .# .#test; do
+for i in "" . .# .#test ../subdir ../subdir#test "$PWD"; do
     nix build $i || fail "flake should be found by searching up directories"
 done
 
 for i in "path:$PWD"; do
     ! nix build $i || fail "flake should not search up directories when using 'path:'"
 done
+
+popd
+
+nix build --override-input foo . || fail "flake should search up directories when not an installable"
+
+sed "s,$PWD/foo,$PWD/foo/subdir,g" -i flake.nix
+! nix build || fail "flake should not search upwards when part of inputs"
