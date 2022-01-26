3dc1418216d30274695c51ba47e05cf92789d4df
regnat <rg@regnat.ovh>
2022-01-26 12:49:16 +0100

    Fix the `store ping` test

--- tests/store-ping.sh
+++ tests/store-ping.sh
@@ -4,8 +4,9 @@ STORE_INFO=$(nix store ping 2>&1)
 
 echo "$STORE_INFO" | grep "Store URL: ${NIX_REMOTE}"
 
-if isDaemonNewer "2.7pre20220126"; then
-    echo "$STORE_INFO" | grep "Version: $($NIX_DAEMON_PACKAGE/bin/nix-daemon --version)"
+if [[ -v NIX_DAEMON_PACKAGE ]] && isDaemonNewer "2.7.0pre20220126"; then
+    DAEMON_VERSION=$($NIX_DAEMON_PACKAGE/bin/nix-daemon --version | cut -d' ' -f3)
+    echo "$STORE_INFO" | grep "Version: $DAEMON_VERSION"
 fi
 
 expect 127 NIX_REMOTE=unix:$PWD/store nix store ping || \
