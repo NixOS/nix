cfbf9ee2ce35b64ced1822c132bd46bd4fd848b2
Guillaume Desforges <guillaume.desforges.pro@gmail.com>
2022-02-24 13:50:50 +0100

    Anchor with prefix

--- doc/manual/generate-builtins.nix
+++ doc/manual/generate-builtins.nix
@@ -6,7 +6,7 @@ builtins:
 concatStrings (map
   (name:
     let builtin = builtins.${name}; in
-    "<dt id=\"${name}\"><a href=\"#${name}\"><code>${name} "
+    "<dt id=\"builtins-${name}\"><a href=\"#builtins-${name}\"><code>${name} "
     + concatStringsSep " " (map (s: "<var>${s}</var>") builtin.args)
     + "</code></a></dt>"
     + "<dd>\n\n"
