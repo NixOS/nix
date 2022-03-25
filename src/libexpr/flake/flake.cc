8c363eb3eb3db96287a10eb5fcde23986a96164f
Eelco Dolstra <edolstra@gmail.com>
2022-03-25 14:19:55 +0100

    Document getFlake
    Fixes #5523.

--- src/libexpr/flake/flake.cc
+++ src/libexpr/flake/flake.cc
@@ -724,6 +724,24 @@ static void prim_getFlake(EvalState & state, const Pos & pos, Value * * args, Va
 static RegisterPrimOp r2({
     .name =  "__getFlake",
     .args = {"args"},
+    .doc = R"(
+      Fetch a flake from a flake reference, and return its output attributes and some metadata. For example:
+
+      ```nix
+      (builtins.getFlake "nix/55bc52401966fbffa525c574c14f67b00bc4fb3a").packages.x86_64-linux.nix
+      ```
+
+      Unless impure evaluation is allowed (`--impure`), the flake reference
+      must be "locked", e.g. contain a Git revision or content hash. An
+      example of an unlocked usage is:
+
+      ```nix
+      (builtins.getFlake "github:edolstra/dwarffs").rev
+      ```
+
+      This function is only available if you enable the experimental feature
+      `flakes`.
+    )",
     .fun = prim_getFlake,
     .experimentalFeature = Xp::Flakes,
 });
