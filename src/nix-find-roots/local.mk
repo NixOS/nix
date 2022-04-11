63159dd67249bc1c9e4137aebe94f28136fb4015
Théophane Hufschmitt <theophane.hufschmitt@tweag.io>
2022-04-11 10:20:36 +0200

    nix-find-roots: Fully disable on darwin
    Until I can understand why it’s not working

--- src/nix-find-roots/local.mk
+++ src/nix-find-roots/local.mk
@@ -1,3 +1,4 @@
+ifndef HOST_DARWIN
 programs += nix-find-roots
 
 nix-find-roots_DIR := $(d)
@@ -5,7 +6,4 @@ nix-find-roots_DIR := $(d)
 nix-find-roots_SOURCES := $(wildcard $(d)/*.cc)
 
 nix-find-roots_INSTALL_DIR := $(libexecdir)/nix
-
-ifdef HOST_DARWIN
-	nix-find-roots_LDFLAGS = -lc++fs
 endif
