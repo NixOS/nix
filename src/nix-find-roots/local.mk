aadf585ea366f4c976c73fca04dab0696bda4385
Théophane Hufschmitt <theophane.hufschmitt@tweag.io>
2022-04-11 10:20:36 +0200

    Fix build on darwin

--- src/nix-find-roots/local.mk
+++ src/nix-find-roots/local.mk
@@ -5,3 +5,7 @@ nix-find-roots_DIR := $(d)
 nix-find-roots_SOURCES := $(wildcard $(d)/*.cc)
 
 nix-find-roots_INSTALL_DIR := $(libexecdir)/nix
+
+ifdef HOST_DARWIN
+	nix-find-roots_LDFLAGS = -lc++fs
+endif
