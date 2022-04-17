e5c934cd48c25eff28c956e414af9b5ff1c6523a
Alex Ameen <alex.ameen.tx@gmail.com>
2022-04-17 18:17:37 -0500

    doc: rephrase connect-timeout help message
    Co-authored-by: Cole Helbling <cole.e.helbling@outlook.com>

--- src/libstore/filetransfer.hh
+++ src/libstore/filetransfer.hh
@@ -31,7 +31,7 @@ struct FileTransferSettings : Config
         R"(
           The timeout (in seconds) for establishing connections in the
           binary cache substituter. It corresponds to `curl`’s
-          `--connect-timeout` option. The default 0 means no limit.
+          `--connect-timeout` option. A value of 0 means no limit.
         )"};
 
     Setting<unsigned long> stalledDownloadTimeout{
