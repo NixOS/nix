54457382f948bff30e2879a7d9047616e134ac5b
Eelco Dolstra <edolstra@gmail.com>
2022-05-11 11:36:56 +0200

    Fix static build
    https://hydra.nixos.org/build/176211267

--- src/libexpr/tests/local.mk
+++ src/libexpr/tests/local.mk
@@ -10,6 +10,6 @@ libexpr-tests_SOURCES := $(wildcard $(d)/*.cc)
 
 libexpr-tests_CXXFLAGS += -I src/libexpr -I src/libutil -I src/libstore -I src/libexpr/tests
 
-libexpr-tests_LIBS = libexpr libutil libstore
+libexpr-tests_LIBS = libexpr libutil libstore libfetchers
 
 libexpr-tests_LDFLAGS := $(GTEST_LIBS) -lgmock
