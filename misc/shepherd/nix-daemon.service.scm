a894729460c47c5917ebe9a94e956c63c142ed71
unrooted <konrad.root.klawikowski@gmail.com>
2022-06-03 20:38:50 +0200

    add shepherd service file

---
+++ misc/shepherd/nix-daemon.service.scm
@@ -0,0 +1,11 @@
+define nix-daemon
+  (make <service>
+    #:provides '(nix-daemon)
+    #:docstring "nix-daemon, the nix package manager's daemon"
+    #:start (make-forkexec-constructor
+              '("/nix/var/nix/profiles/default/bin/nix-daemon"))
+    #:stop (make-kill-destructor)
+    #:respawn? #t))
+(register-services nix-daemon)
+
+(start nix-daemon)
