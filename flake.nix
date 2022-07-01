19d7d7ac42930f39fc39eaa9ca82d3c94ef60411
Théophane Hufschmitt <theophane.hufschmitt@tweag.io>
2022-07-01 10:59:46 +0200

    switch nixpkgs to nixos-22.05

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "The purely functional package manager";
 
-  inputs.nixpkgs.url = "nixpkgs/nixos-21.05-small";
+  inputs.nixpkgs.url = "nixpkgs/nixos-22.05-small";
   inputs.nixpkgs-regression.url = "nixpkgs/215d4d0fd80ca5163643b03a33fde804a29cc1e2";
   inputs.lowdown-src = { url = "github:kristapsdz/lowdown"; flake = false; };
 
