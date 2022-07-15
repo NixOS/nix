bbce0404d9230945682ec595c632e3223e657d6f
Nick Cao <nickcao@nichi.co>
2022-07-15 10:48:05 +0800

    mk/run_test.sh: apply the workaround for random test failures on darwin also to linux

--- mk/run_test.sh
+++ mk/run_test.sh
@@ -27,7 +27,6 @@ run_test "$1"
 # appear randomly without anyone knowing why.
 # See https://github.com/NixOS/nix/issues/3605 for more info
 if [[ $status -ne 0 && $status -ne 99 && \
-    "$(uname)" == "Darwin" && \
     "$log" =~ "unexpected EOF reading a line" \
 ]]; then
     echo "$post_run_msg [${yellow}FAIL$normal] (possibly flaky, so will be retried)"
