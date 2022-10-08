6c609b37cf7f02a6e17b6dedda5745984a83ea31
Tom McLaughlin <tom@codedown.io>
2022-10-07 20:50:24 -0600

    Remove a couple legacy logger->stop() calls

--- src/nix-build/nix-build.cc
+++ src/nix-build/nix-build.cc
@@ -569,8 +569,6 @@ static void main_nix_build(int argc, char * * argv)
 
         restoreProcessContext();
 
-        logger->stop();
-
         execvp(shell->c_str(), argPtrs.data());
 
         throw SysError("executing shell '%s'", *shell);
@@ -628,8 +626,6 @@ static void main_nix_build(int argc, char * * argv)
 
             outPaths.push_back(outputPath);
         }
-
-        logger->stop();
 
         for (auto & path : outPaths)
             std::cout << store->printStorePath(path) << '\n';
