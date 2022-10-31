5cd0373f3b9295f1db3a9f4cf610ba1fe93be085
Yorick van Pelt <yorick@yorickvanpelt.nl>
2022-10-31 18:17:57 +0100

    json-to-value: use SimdJSON for parsing
    In some usecases, Nix spends a significant amount of time parsing
    JSON. This library is a lot (at least 10x) faster, and
    decreases real world eval times 15-25%, mostly when using
    json-heavy code like nix-pypi-fetcher or haskell.nix.

--- src/libexpr/json-to-value.cc
+++ src/libexpr/json-to-value.cc
@@ -1,11 +1,112 @@
 #include "json-to-value.hh"
 
 #include <variant>
+
+#ifdef HAVE_LIBSIMDJSON
+
+#include <iterator>
+#include <simdjson.h>
+
+using namespace simdjson;
+
+namespace nix {
+
+static void parse_json(EvalState &state, const dom::element &doc, Value &v) {
+  switch (doc.type()) {
+  case dom::element_type::OBJECT: {
+    dom::object obj = doc.get_object().value_unsafe();
+    size_t len = obj.size();
+    if (simdjson_unlikely(len == 0xFFFFFF)) {
+        len = 0;
+        for (auto _x : obj) len++;
+    }
+    BindingsBuilder builder(state, state.allocBindings(len));
+    for (dom::key_value_pair field : obj) {
+      /* JSON strings can contain null bytes.
+         Putting them in nix symbols can have unforeseen consequences. */
+      if (simdjson_unlikely(field.key.find((char)0) != std::string::npos)) {
+        field.key = field.key.substr(0, field.key.find((char)0));
+      }
+      Value &v2 = builder.alloc(field.key);
+      parse_json(state, field.value, v2);
+    }
+    // TODO: handle duplicate values
+    v.mkAttrs(builder);
+  }; break;
+  case dom::element_type::ARRAY: {
+    dom::array arr = doc.get_array().value_unsafe();
+    size_t len = arr.size();
+    if (simdjson_unlikely(len == 0xFFFFFF)) {
+        len = 0;
+        for (auto _x : arr) len++;
+    }
+    state.mkList(v, len);
+    size_t i = 0;
+    for (dom::element x : arr) {
+      Value *v2 = state.allocValue();
+      v.listElems()[i++] = v2;
+      parse_json(state, x, *v2);
+    }
+  }; break;
+  case dom::element_type::STRING: {
+    /* Can't use mkString(std::string_view) since it calls strlen,
+       but our strings aren't zero-terminated so this is really slow */
+    // todo: handle null byte
+    std::string_view str = doc.get_string().value_unsafe();
+    char *buf = (char *)GC_MALLOC_ATOMIC(str.length() + 1);
+    str.copy(buf, -1);
+    buf[str.length()] = 0;
+    v.mkString(buf);
+  }; break;
+  case dom::element_type::BOOL:
+    v.mkBool(doc.get_bool().value_unsafe());
+    break;
+  case dom::element_type::NULL_VALUE:
+    v.mkNull();
+    break;
+  case dom::element_type::UINT64: case dom::element_type::INT64:
+    v.mkInt(doc.get_int64().value_unsafe());
+    break;
+  case dom::element_type::DOUBLE:
+    v.mkFloat(doc.get_double().value_unsafe());
+    break;
+  default:
+    assert(false);
+  }
+}
+
+void parseJSON(EvalState & state, const std::string_view & s_, Value & v)
+{
+  try {
+      //using namespace std::chrono;
+    //high_resolution_clock::time_point t1 = high_resolution_clock::now();
+
+    static thread_local dom::parser parser;
+
+    /* simdjson needs 64 readable bytes after the string.
+       Since there's no way to guarantee this, copy the string to a temporary buffer
+       This doesn't seem to be slower than calling GC_REALLOC on the string. */
+    padded_string json_padded = padded_string(s_);
+    auto doc = parser.parse(json_padded);
+    parse_json(state, doc.value(), v);
+    //high_resolution_clock::time_point t2 = high_resolution_clock::now();
+    //duration<double> time_span = duration_cast<duration<double>>(t2 - t1);
+
+    //printMsg(lvlTalkative, format("json parse: %1%s") % time_span.count());
+  } catch(simdjson_error &e) {
+      throw JSONParseError(e.what());
+  }
+}
+
+}
+
+#else
 #include <nlohmann/json.hpp>
 
 using json = nlohmann::json;
 
 namespace nix {
+
 
 // for more information, refer to
 // https://github.com/nlohmann/json/blob/master/include/nlohmann/detail/input/json_sax.hpp
@@ -172,3 +273,4 @@ void parseJSON(EvalState & state, const std::string_view & s_, Value & v)
 }
 
 }
+#endif /* ndef HAVE_LIB_SIMDJSON */
