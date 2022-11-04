40911d7dec75541a400fe8f556d4c70a7f845fac
Eelco Dolstra <edolstra@gmail.com>
2022-11-04 13:30:35 +0100

    Remove stray tab

--- src/libutil/experimental-features.cc
+++ src/libutil/experimental-features.cc
@@ -14,8 +14,8 @@ std::map<ExperimentalFeature, std::string> stringifiedXpFeatures = {
     { Xp::NoUrlLiterals, "no-url-literals" },
     { Xp::FetchClosure, "fetch-closure" },
     { Xp::ReplFlake, "repl-flake" },
-	{ Xp::AutoAllocateUids, "auto-allocate-uids" },
-	{ Xp::SystemdCgroup, "systemd-cgroup" },
+    { Xp::AutoAllocateUids, "auto-allocate-uids" },
+    { Xp::SystemdCgroup, "systemd-cgroup" },
 };
 
 const std::optional<ExperimentalFeature> parseExperimentalFeature(const std::string_view & name)
