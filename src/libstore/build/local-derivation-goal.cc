609a7dc05974c9f86b2e7304762b9e01c5879380
Eelco Dolstra <edolstra@gmail.com>
2023-01-04 04:36:07 -0800

    Include macOS sandbox files in the Nix binary
    This basically reverts 6e5165b77370c76bfa39d4b55e9f83673f3bd466.
    It fixes errors like
    
      sandbox-exec: <internal init prelude>:292:47: unable to open sandbox-minimal.sb: not found
    
    when trying to run a development Nix installed in a user's home
    directory.
    
    Also, we're trying to minimize the number of installed files
    to make it possible to deploy Nix as a single statically-linked
    binary.

--- src/libstore/build/local-derivation-goal.cc
+++ src/libstore/build/local-derivation-goal.cc
@@ -2050,10 +2050,14 @@ void LocalDerivationGoal::runChild()
                     sandboxProfile += "(deny default (with no-log))\n";
                 }
 
-                sandboxProfile += "(import \"sandbox-defaults.sb\")\n";
+                sandboxProfile +=
+                    #include "sandbox-defaults.sb"
+                    ;
 
                 if (!derivationType.isSandboxed())
-                    sandboxProfile += "(import \"sandbox-network.sb\")\n";
+                    sandboxProfile +=
+                        #include "sandbox-network.sb"
+                        ;
 
                 /* Add the output paths we'll use at build-time to the chroot */
                 sandboxProfile += "(allow file-read* file-write* process-exec\n";
@@ -2096,7 +2100,9 @@ void LocalDerivationGoal::runChild()
 
                 sandboxProfile += additionalSandboxProfile;
             } else
-                sandboxProfile += "(import \"sandbox-minimal.sb\")\n";
+                sandboxProfile +=
+                    #include "sandbox-minimal.sb"
+                    ;
 
             debug("Generated sandbox profile:");
             debug(sandboxProfile);
@@ -2121,8 +2127,6 @@ void LocalDerivationGoal::runChild()
                 args.push_back(sandboxFile);
                 args.push_back("-D");
                 args.push_back("_GLOBAL_TMP_DIR=" + globalTmpDir);
-                args.push_back("-D");
-                args.push_back("IMPORT_DIR=" + settings.nixDataDir + "/nix/sandbox/");
                 if (allowLocalNetworking) {
                     args.push_back("-D");
                     args.push_back(std::string("_ALLOW_LOCAL_NETWORKING=1"));
