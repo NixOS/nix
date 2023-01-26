f4f0f8ae3fbfc5da400dbb2566bc8659d0580034
Eelco Dolstra <edolstra@gmail.com>
2023-01-26 16:36:10 +0100

    Remove toString deprecation warning

--- src/libexpr/paths.cc
+++ src/libexpr/paths.cc
@@ -48,8 +48,6 @@ SourcePath EvalState::decodePath(std::string_view s, PosIdx pos)
 
             SourcePath path {accessor->second, CanonPath(s)};
 
-            warn("applying 'toString' to path '%s' and then accessing it is deprecated, at %s", path, positions[pos]);
-
             return path;
         } catch (std::invalid_argument & e) {
             fail();
