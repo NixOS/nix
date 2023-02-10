cec23f5ddab31fb7704aa7d822a508690e416ab6
Et7f3 <cadeaudeelie@gmail.com>
2023-02-11 00:37:23 +0100

    ExprOpHasAttr,ExprSelect,stripIndentation,binds,formals: delete losts objects
    We are looking for *$ because it indicate that it was constructed with a new but
    not release. De-referencing shallow copy so deleting as whole might create
    dangling pointer that's why we move it so we delete a empty containers + the
    nice perf boost.

--- src/libexpr/nixexpr.hh
+++ src/libexpr/nixexpr.hh
@@ -233,7 +233,7 @@ struct ExprSelect : Expr
     PosIdx pos;
     Expr * e, * def;
     AttrPath attrPath;
-    ExprSelect(const PosIdx & pos, Expr * e, const AttrPath & attrPath, Expr * def) : pos(pos), e(e), def(def), attrPath(attrPath) { };
+    ExprSelect(const PosIdx & pos, Expr * e, const AttrPath && attrPath, Expr * def) : pos(pos), e(e), def(def), attrPath(std::move(attrPath)) { };
     ExprSelect(const PosIdx & pos, Expr * e, Symbol name) : pos(pos), e(e), def(0) { attrPath.push_back(AttrName(name)); };
     PosIdx getPos() const override { return pos; }
     COMMON_METHODS
@@ -243,7 +243,7 @@ struct ExprOpHasAttr : Expr
 {
     Expr * e;
     AttrPath attrPath;
-    ExprOpHasAttr(Expr * e, const AttrPath & attrPath) : e(e), attrPath(attrPath) { };
+    ExprOpHasAttr(Expr * e, const AttrPath && attrPath) : e(e), attrPath(std::move(attrPath)) { };
     PosIdx getPos() const override { return e->getPos(); }
     COMMON_METHODS
 };
