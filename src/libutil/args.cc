a537095e1f42d3de726e3c4d8bfc018a4c43ad86
Philipp Jungkamp <p.jungkamp@gmx.net>
2023-02-10 18:03:19 +0100

    Infer short completion descriptions for commandline flags
    Descriptions for commandline flags may not include newlines and should
    be rather short for display in a shell. Truncate the description string
    of a flag on '\n' or '.' to and add an ellipsis if needed.

--- src/libutil/args.cc
+++ src/libutil/args.cc
@@ -29,7 +29,16 @@ void Args::removeFlag(const std::string & longName)
 
 void Completions::add(std::string completion, std::string description)
 {
-    assert(description.find('\n') == std::string::npos);
+    // strip whitespace/empty lines from the front of the description
+    description.erase(0, description.find_first_not_of(" \t\n"));
+    // ellipsize overflowing content on the back of the description
+    auto end_index = description.find_first_of(".\n");
+    if (end_index != std::string::npos) {
+        auto needs_ellipsis = end_index != description.size() - 1;
+        description.resize(end_index);
+        if (needs_ellipsis)
+            description.append(" [...]");
+    }
     insert(Completion {
         .completion = completion,
         .description = description
