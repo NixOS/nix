39700c5cbeeb8005bfbe052ea79ababe46d7f072
Théophane Hufschmitt <7226587+thufschmitt@users.noreply.github.com>
2023-03-08 20:47:52 +0100

    Revert "Disable GC during coroutine execution + test"

--- src/libutil/serialise.hh
+++ src/libutil/serialise.hh
@@ -501,10 +501,4 @@ struct StackAllocator {
     static StackAllocator *defaultAllocator;
 };
 
-/* Disabling GC when entering a coroutine (without the boehm patch).
-   mutable to avoid boehm gc dependency in libutil.
- */
-extern std::shared_ptr<void> (*create_coro_gc_hook)();
-
-
 }
