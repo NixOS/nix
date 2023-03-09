85df7e7ea24b9f7badbcec06a54e144a0cf1baf5
Alexander Bantyev <balsoft@balsoft.ru>
2023-03-09 18:11:01 +0400

    Logger, ProgressBar: add a way to pause/resume
    Add new virtual methods pause and resume to the Logger class, and
    implement them in ProgressBar to allow to pause the bar refreshing.

--- src/libmain/progress-bar.cc
+++ src/libmain/progress-bar.cc
@@ -72,6 +72,7 @@ private:
         uint64_t corruptedPaths = 0, untrustedPaths = 0;
 
         bool active = true;
+        bool paused = false;
         bool haveUpdate = true;
     };
 
@@ -118,6 +119,17 @@ public:
             quitCV.notify_one();
         }
         updateThread.join();
+    }
+
+    void pause() override {
+        state_.lock()->paused = true;
+        writeToStderr("\r\e[K");
+    }
+
+    void resume() override {
+        state_.lock()->paused = false;
+        writeToStderr("\r\e[K");
+        updateCV.notify_one();
     }
 
     bool isVerbose() override
@@ -338,6 +350,7 @@ public:
     {
         auto nextWakeup = std::chrono::milliseconds::max();
 
+        if (state.paused) return nextWakeup;
         state.haveUpdate = false;
         if (!state.active) return nextWakeup;
 
