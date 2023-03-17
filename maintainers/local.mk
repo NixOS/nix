bc2f4b12b0f5fe90e9182484b3f01cf0b86aafd9
Robert Hensing <robert@roberthensing.nl>
2023-03-17 16:52:09 +0100

    Add `make format`

---
+++ maintainers/local.mk
@@ -0,0 +1,10 @@
+
+.PHONY: format
+print-top-help += echo '  format: Format source code'
+
+format:
+	@if ! type -p pre-commit &>/dev/null; then \
+	  echo "make format: pre-commit not found. Please use \`nix develop\`."; \
+	  exit 1; \
+	fi; \
+	pre-commit run --all-files
