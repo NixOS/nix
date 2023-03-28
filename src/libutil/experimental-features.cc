b25835346f74a38d5bb7752eaf11fe60a46e59bf
Andrea Bedini <andrea.bedini@tweag.io>
2023-03-28 12:37:27 +0800

    Parse TOML timestamps
    Currently fromTOML throws an exception when encountering a timestamp
    since the nix language lacks a way to represent them.
    
    This patch changes this beaviour and makes fromTOML parse timestamps as
    attrsets of the format
    
      { _type = "timestamp"; value = "1979-05-27T07:32:00Z"; }

--- src/libutil/experimental-features.cc
+++ src/libutil/experimental-features.cc
@@ -12,7 +12,7 @@ struct ExperimentalFeatureDetails
     std::string_view description;
 };
 
-constexpr std::array<ExperimentalFeatureDetails, 13> xpFeatureDetails = {{
+constexpr std::array<ExperimentalFeatureDetails, 14> xpFeatureDetails = {{
     {
         .tag = Xp::CaDerivations,
         .name = "ca-derivations",
@@ -212,6 +212,13 @@ constexpr std::array<ExperimentalFeatureDetails, 13> xpFeatureDetails = {{
                 derivations that are themselves derivations outputs.
         )",
     },
+    {
+        .tag = Xp::ParseTomlTimestamps,
+        .name = "parse-toml-timestamps",
+        .description = R"(
+            Allow parsing of timestamps in builtins.fromTOML.
+        )",
+    },
 }};
 
 static_assert(
@@ -246,7 +253,7 @@ std::string_view showExperimentalFeature(const ExperimentalFeature tag)
     return xpFeatureDetails[(size_t)tag].name;
 }
 
-nlohmann::json documentExperimentalFeatures() 
+nlohmann::json documentExperimentalFeatures()
 {
     StringMap res;
     for (auto & xpFeature : xpFeatureDetails)
