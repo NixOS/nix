d86c7d55a25bca8081d5eba1dc80b9efb5ad7bb3
Robert Hensing <robert@roberthensing.nl>
2023-04-07 17:31:16 +0200

    make format: Always use up to date hooks

--- maintainers/local.mk
+++ maintainers/local.mk
@@ -2,9 +2,13 @@
 .PHONY: format
 print-top-help += echo '  format: Format source code'
 
-format:
+# This uses the cached .pre-commit-hooks.yaml file
+format-quick:
 	@if ! type -p pre-commit &>/dev/null; then \
 	  echo "make format: pre-commit not found. Please use \`nix develop\`."; \
 	  exit 1; \
 	fi; \
 	pre-commit run --all-files --hook-stage manual
+
+format:
+	nix develop --command -- pre-commit run --all-files --hook-stage manual
