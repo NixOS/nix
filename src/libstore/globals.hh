9c423459581906057df37580b8813c2f889f4c93
Alexander Bantyev <balsoft@balsoft.ru>
2023-06-02 09:39:33 +0000

    Apply suggestions from @fricklerhandwerk
    Co-authored-by: Valentin Gagarin <valentin.gagarin@tweag.io>

--- src/libstore/globals.hh
+++ src/libstore/globals.hh
@@ -974,9 +974,7 @@ public:
           | `~/.nix-defexpr`  | `$XDG_STATE_HOME/nix/defexpr`  |
           | `~/.nix-channels` | `$XDG_STATE_HOME/nix/channels` |
 
-          If you already have Nix installed and are using Profiles or Channels, you should migrate manually when you enable this option.
-          To do so, create `$XDG_STATE_HOME/nix`, then move `.nix-profile`, `.nix-defexpr` and `.nix-channels` as specified in the table above.
-          If `$XDG_STATE_HOME` is not set, use `$HOME/.local/state/nix` instead of `$XDG_STATE_HOME/nix`.
+          If you already have Nix installed and are using profiles or channels, you should migrate manually when enabling this option.
           This can be achieved with the following shell commands:
 
           ```sh
