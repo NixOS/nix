a53954804c8acb98efc62479adb519e3ec3c8af7
Valentin Gagarin <valentin.gagarin@tweag.io>
2023-06-02 16:59:36 +0200

    use inline links

--- src/libstore/globals.hh
+++ src/libstore/globals.hh
@@ -974,7 +974,7 @@ public:
           | `~/.nix-defexpr`  | `$XDG_STATE_HOME/nix/defexpr`  |
           | `~/.nix-channels` | `$XDG_STATE_HOME/nix/channels` |
 
-          If you already have Nix installed and are using [Profiles] or [Channels], you should migrate manually when you enable this option.
+          If you already have Nix installed and are using [profiles](@docroot@/package-management/profiles.md) or [channels](@docroot@/package-management/channels.md), you should migrate manually when you enable this option.
           If `$XDG_STATE_HOME` is not set, use `$HOME/.local/state/nix` instead of `$XDG_STATE_HOME/nix`.
           This can be achieved with the following shell commands:
 
@@ -985,8 +985,6 @@ public:
           mv $HOME/.nix-defexpr $nix_state_home/defexpr
           mv $HOME/.nix-channels $nix_state_home/channels
           ```
-          [Profiles]: @docroot@/package-management/profiles.md
-          [Channels]: @docroot@/package-management/channels.md
         )"
     };
 };
