08089fdd3284fcb27bcec347d6bf88ec2a79a8a0
Tom Bereknyei <tomberek@gmail.com>
2023-06-11 13:33:38 -0400

    fix: Do not apply default installables when using --stdin

--- src/libcmd/installables.cc
+++ src/libcmd/installables.cc
@@ -730,9 +730,9 @@ void RawInstallablesCommand::run(ref<Store> store)
         while (std::cin >> word) {
             rawInstallables.emplace_back(std::move(word));
         }
+    } else {
+        applyDefaultInstallables(rawInstallables);
     }
-
-    applyDefaultInstallables(rawInstallables);
     run(store, std::move(rawInstallables));
 }
 
