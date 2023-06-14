88f9043e42126c4c9d04bd2173cb7792a7ce0b1d
John Ericson <git@JohnEricson.me>
2023-06-14 16:14:04 -0400

    Ooops need abs link to glossary here

--- src/libstore/globals.hh
+++ src/libstore/globals.hh
@@ -683,7 +683,7 @@ public:
         "substituters",
         R"(
           A list of [URLs of Nix stores](@docroot@/command-ref/new-cli/nix3-help-stores.md#store-url-format) to be used as substituters, separated by whitespace.
-          A substituter is an additional [store]{#gloss-store} from which Nix can obtain [store objects](@docroot@/glossary.md#gloss-store-object) instead of building them.
+          A substituter is an additional [store]{@docroot@/glossary.md##gloss-store} from which Nix can obtain [store objects](@docroot@/glossary.md#gloss-store-object) instead of building them.
 
           Substituters are tried based on their priority value, which each substituter can set independently.
           Lower value means higher priority.
