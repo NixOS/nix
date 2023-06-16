f695a74751c314cc426ff7bbc67ce5de8b58bbfd
Valentin Gagarin <valentin.gagarin@tweag.io>
2023-06-16 17:58:01 +0200

    Update src/libstore/globals.hh

--- src/libstore/globals.hh
+++ src/libstore/globals.hh
@@ -711,7 +711,7 @@ public:
         this, {}, "trusted-substituters",
         R"(
           A list of [Nix store URLs](@docroot@/command-ref/new-cli/nix3-help-stores.md#store-url-format), separated by whitespace.
-          These are not used by default, but can be enabled by users of the Nix daemon by specifying [`substituters`](#conf-substituters).
+          These are not used by default, but users of the Nix daemon can enable them by specifying [`substituters`](#conf-substituters).
 
           Unprivileged users (those set in only [`allowed-users`](#conf-allowed-users) but not [`trusted-users`](#conf-trusted-users)) can pass as `substituters` only those URLs listed in `trusted-substituters`.
         )",
