c35af4fba1510eb146c97ede076103512334edb5
Eelco Dolstra <edolstra@gmail.com>
2023-06-19 16:54:05 +0200

    LocalStore::addTempRoot(): Handle ENOENT
    If the garbage collector has acquired the global GC lock, but hasn't
    created the GC socket yet, then a client attempting to connect would
    get ENOENT. Note that this only happens when the GC runs for the first
    time on a machine. Subsequently clients will get ECONNREFUSED which
    was already handled.
    
    Fixes #7370.

--- tests/functional/gc-non-blocking.sh
+++ tests/functional/gc-non-blocking.sh
@@ -9,15 +9,20 @@ clearStore
 fifo=$TEST_ROOT/test.fifo
 mkfifo "$fifo"
 
+fifo2=$TEST_ROOT/test2.fifo
+mkfifo "$fifo2"
+
 dummy=$(nix store add-path ./simple.nix)
 
 running=$TEST_ROOT/running
 touch $running
 
-(_NIX_TEST_GC_SYNC=$fifo nix-store --gc -vvvvv; rm $running) &
+(_NIX_TEST_GC_SYNC=$fifo _NIX_TEST_GC_SYNC_2=$fifo2 nix-store --gc -vvvvv; rm $running) &
 pid=$!
 
 sleep 2
+
+(sleep 1; echo > $fifo2) &
 
 outPath=$(nix-build --max-silent-time 60 -o "$TEST_ROOT/result" -E "
   with import ./config.nix;
