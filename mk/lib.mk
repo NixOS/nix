735a672e1f3bbad8e32b211ce33fa4a308ae355a
John Ericson <John.Ericson@Obsidian.Systems>
2023-07-09 22:24:51 -0400

    Introduce notion of a test group, use for CA tests
    Grouping our tests should make it easier to understand the intent than
    one long poorly-arranged list. It also is convenient for running just
    the tests for a specific component when working on that component.
    
    We need at least one test group so this isn't dead code; I decided to
    collect the tests for the `ca-derivations` and `dynamic-derivations`
    experimental features in groups. Do
    ```bash
    make ca.test-group -jN
    ```
    and
    ```bash
    make dyn-drv.test-group -jN
    ```
    to try running just them.
    
    I originally did this as part of #8397 for being able to just the local
    overlay store alone. I am PRing it separately now so we can separate
    general infra from new features.

--- mk/lib.mk
+++ mk/lib.mk
@@ -10,6 +10,7 @@ bin-scripts :=
 noinst-scripts :=
 man-pages :=
 install-tests :=
+install-tests-groups :=
 
 ifdef HOST_OS
   HOST_KERNEL = $(firstword $(subst -, ,$(HOST_OS)))
@@ -121,7 +122,16 @@ $(foreach script, $(bin-scripts), $(eval $(call install-program-in,$(script),$(b
 $(foreach script, $(bin-scripts), $(eval programs-list += $(script)))
 $(foreach script, $(noinst-scripts), $(eval programs-list += $(script)))
 $(foreach template, $(template-files), $(eval $(call instantiate-template,$(template))))
-$(foreach test, $(install-tests), $(eval $(call run-install-test,$(test))))
+$(foreach test, $(install-tests), \
+  $(eval $(call run-install-test,$(test))) \
+  $(eval installcheck: $(test).test))
+$(foreach test-group, $(install-tests-groups), \
+  $(eval $(call run-install-test-group,$(test-group))) \
+  $(eval installcheck: $(test-group).test-group) \
+  $(foreach test, $($(test-group)-tests), \
+    $(eval $(call run-install-test,$(test))) \
+    $(eval $(test-group).test-group: $(test).test)))
+
 $(foreach file, $(man-pages), $(eval $(call install-data-in, $(file), $(mandir)/man$(patsubst .%,%,$(suffix $(file))))))
 
 
