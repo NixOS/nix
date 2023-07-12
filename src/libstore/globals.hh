0309f6b5b8ca354e1a43d320928d3d546ce7f878
Ben Radford <104896700+benradf@users.noreply.github.com>
2023-07-12 12:32:57 +0100

    Update src/libstore/globals.hh
    Co-authored-by: Valentin Gagarin <valentin.gagarin@tweag.io>

--- src/libstore/globals.hh
+++ src/libstore/globals.hh
@@ -530,8 +530,8 @@ public:
           Nix will attempt to drop supplementary groups when building with sandboxing.
 
           However this can fail under some circumstances.
-          For example, if the user lacks the CAP_SETGID capability.
-          Search setgroups(2) for EPERM to find more detailed information on this.
+          For example, if the user lacks the `CAP_SETGID` capability.
+          Search `setgroups(2)` for `EPERM` to find more detailed information on this.
 
           If you encounter such a failure, setting this option to `false` will let you ignore it and continue.
           But before doing so, you should consider the security implications carefully.
