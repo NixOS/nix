8cdbb4dcbe34d304e33e4cd01f8100aedc91bc78
Ben Burdette <>
2023-09-03 12:55:45 -0600

    cleanup

--- tests/debugger.sh
+++ tests/debugger.sh
@@ -8,16 +8,6 @@ replCmds="
 :q
 "
 
-# replFailingCmds="
-# failing = import $testDir/simple-failing.nix
-# :b failing
-# :log failing
-# "
-
-# replUndefinedVariable="
-# import $testDir/undefined-variable.nix
-# "
-
 echo "nix version: $(nix --version)"
 
 nixArgs=("$@")
@@ -27,9 +17,6 @@ na="${nixArgs[@]}"
 testRepl () {
     fname=$1
     local replOutput=$(nix eval ${na} --debugger -f $testDir/${fname}.nix <<< "$replCmds")
-    # local replOutput=$((nix eval ${na} --debugger -f $testDir/${fname}.nix <<< "$replCmds") 2>&1)
-    # echo -e "$replOutput" | grep "^.\[34\;1m[0-9]" > "/home/bburdette/testout/${fname}.txt"
-    # local replFiltered=$(echo -e "$replOutput" | { grep "^.\[34\;1m[0-9]" || echo "" ; } | grep -v "file")
     local replFiltered=$(echo -e "$replOutput" | { grep "^.\[34\;1m[0-9]" || echo "" ; } )
 
     echo -e "$replFiltered" > "/home/bburdette/testout/${fname}.txt"
@@ -42,12 +29,6 @@ testRepl () {
     if [ "${ex2}" != "${ro2}" ]; then
         fail "ex2 output doesn't match for file: $fname"
     fi
-    # if [ "${expected}" != "${replOutput}" ]; then
-    #     echo "expected: ${expected}"
-    #     echo "replOutput: ${replOutput}"
-
-    #     fail "expected output doesn't match"
-    # fi
 }
 
 # Simple test, try building a drv
