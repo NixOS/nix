e3f3be9c594ed74ae24ece5f7427e6083683075e
Eli Kogan-Wang <elikowa@gmail.com>
2023-09-10 15:46:24 +0200

    nix-env -> nix profile priority: fix test

--- tests/nix-profile.sh
+++ tests/nix-profile.sh
@@ -46,10 +46,11 @@ printf false > $flake1Dir/ca.nix
 cp ./config.nix $flake1Dir/
 
 # Test upgrading from nix-env.
-nix-env -f ./user-envs.nix -i foo-1.0
+nix-env -f ./user-envs.nix -i foo-1.0 foo-0.1
 nix profile list | grep -A2 'Index:.*0' | grep 'Store paths:.*foo-1.0'
+nix profile list | grep -A2 'Index:.*1' | grep 'Store paths:.*foo-0.1'
 nix profile install $flake1Dir -L
-nix profile list | grep -A4 'Index:.*1' | grep 'Locked flake URL:.*narHash'
+nix profile list | grep -A4 'Index:.*2' | grep 'Locked flake URL:.*narHash'
 grep '"priority":10' $TEST_HOME/.nix-profile/manifest.json
 [[ $($TEST_HOME/.nix-profile/bin/hello) = "Hello World" ]]
 [ -e $TEST_HOME/.nix-profile/share/man ]
@@ -61,7 +62,7 @@ nix profile diff-closures | grep 'env-manifest.nix: ε → ∅'
 # Test XDG Base Directories support
 
 export NIX_CONFIG="use-xdg-base-directories = true"
-nix profile remove 1
+nix profile remove 1 2
 nix profile install $flake1Dir
 [[ $($TEST_HOME/.local/state/nix/profile/bin/hello) = "Hello World" ]]
 unset NIX_CONFIG
