7e24dc606b1cf7fd4e4dfd7027ba1469a59784d5
Tom Bereknyei <tomberek@gmail.com>
2023-09-22 13:05:55 -0400

    fix(tests): fix assumption that string.s is a char*

--- src/libexpr/tests/libexpr.hh
+++ src/libexpr/tests/libexpr.hh
@@ -71,7 +71,7 @@ namespace nix {
         if (arg.type() != nString) {
             return false;
         }
-        return std::string_view(arg.string.s) == s;
+        return std::string_view(arg.c_str()) == s;
     }
 
     MATCHER_P(IsIntEq, v, fmt("The string is equal to \"%1%\"", v)) {
@@ -106,8 +106,8 @@ namespace nix {
             if (arg.type() != nPath) {
                 *result_listener << "Expected a path got " << arg.type();
                 return false;
-            } else if (std::string_view(arg.string.s) != p) {
-                *result_listener << "Expected a path that equals \"" << p << "\" but got: " << arg.string.s;
+            } else if (std::string_view(arg._path) != p) {
+                *result_listener << "Expected a path that equals \"" << p << "\" but got: " << arg.c_str();
                 return false;
             }
             return true;
