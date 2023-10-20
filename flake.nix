84867fa212fe08feeb27b668c4c78bc9eaf74e17
Cole Helbling <cole.e.helbling@outlook.com>
2023-10-20 10:14:27 -0700

    Update flake.nix
    Co-authored-by: Greg Pfeil <greg@technomadic.org>

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
   inputs.nixpkgs-regression.url = "github:NixOS/nixpkgs/215d4d0fd80ca5163643b03a33fde804a29cc1e2";
   inputs.lowdown-src = { url = "github:kristapsdz/lowdown"; flake = false; };
   inputs.flake-compat = { url = "github:edolstra/flake-compat"; flake = false; };
-  inputs.flake-schemas.url = github:DeterminateSystems/flake-schemas;
+  inputs.flake-schemas.url = "github:DeterminateSystems/flake-schemas";
 
   outputs = { self, nixpkgs, nixpkgs-regression, lowdown-src, flake-compat, flake-schemas }:
 
