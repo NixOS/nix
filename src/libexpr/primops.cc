f269911641fcc6cef836a07393feae9d067096a8
Silvan Mosberger <silvan.mosberger@tweag.io>
2023-10-24 11:22:02 +0200

    Document builtins.substring negative length behavior (#9226)

--- src/libexpr/primops.cc
+++ src/libexpr/primops.cc
@@ -3720,10 +3720,11 @@ static RegisterPrimOp primop_substring({
     .doc = R"(
       Return the substring of *s* from character position *start*
       (zero-based) up to but not including *start + len*. If *start* is
-      greater than the length of the string, an empty string is returned,
-      and if *start + len* lies beyond the end of the string, only the
-      substring up to the end of the string is returned. *start* must be
-      non-negative. For example,
+      greater than the length of the string, an empty string is returned.
+      If *start + len* lies beyond the end of the string or *len* is `-1`,
+      only the substring up to the end of the string is returned.
+      *start* must be non-negative.
+      For example,
 
       ```nix
       builtins.substring 0 3 "nixos"
