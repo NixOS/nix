c25db0c93e08847a4804111dea4ce76cd2917acb
Robert Hensing <robert@roberthensing.nl>
2023-11-01 23:09:12 +0100

    Add nix::fetchers::parsers::Attrs etc

--- src/libfetchers/parser.hh
+++ src/libfetchers/parser.hh
@@ -1,6 +1,9 @@
 #pragma once
 
 #include "attrs.hh"
+#include "error.hh"
+#include "schema.hh"
+#include "map.hh"
 #include <cstdint>
 #include <memory>
 
@@ -48,6 +51,166 @@ namespace nix::fetchers {
             bool parse(Attr in) override;
         };
 
-    }
+        template <typename Out>
+        class Attr : public Parser<std::optional<nix::fetchers::Attr>, Out>{
+        public:
+            const std::string name;
+            Attr(std::string name) : name(name) {}
 
-}
+            virtual Out parse(std::optional<nix::fetchers::Attr> in) override = 0;
+
+            virtual bool isRequired() = 0;
+
+            std::shared_ptr<Schema> getSchema() override {
+                // Attributes aren't first class, so we won't be using this method.
+                // Perhaps use new superclass of Parser? Type parameter?
+                throw Error("not implemented");
+            }
+
+            virtual std::shared_ptr<Schema> getAttrValueSchema() = 0;
+
+            Schema::Attrs::Attr getAttrSchema() {
+                Schema::Attrs::Attr attrSchema;
+                attrSchema.type = getAttrValueSchema();
+                attrSchema.required = isRequired();
+                return attrSchema;
+            }
+        };
+
+        template <typename T>
+        T parseAttr(const Attrs & attrs, Attr<T> * parser) {
+            try {
+                return parser->parse(maybeGet(attrs, parser->name));
+            }
+            catch (Error & e) {
+                e.addTrace(nullptr, "while checking fetcher attribute '%s'", parser->name);
+                throw e;
+            }
+        }
+
+        template <typename Parser>
+        class OptionalAttr : public Attr<std::optional<typename Parser::Out>> {
+            Parser parser;
+
+        public:
+            OptionalAttr(std::string name, Parser parser)
+                : Attr<std::optional<typename Parser::Out>>(name), parser(parser) {}
+
+            bool isRequired() override { return false; }
+
+            std::optional<typename Parser::Out> parse(std::optional<nix::fetchers::Attr> in) override {
+                // "map"
+                if (in) {
+                    return parser.parse(*in);
+                } else {
+                    return std::nullopt;
+                }
+            }
+
+            std::shared_ptr<Schema> getAttrValueSchema() override {
+                return parser.getSchema();
+            }
+        };
+
+        template <typename Parser>
+        class RequiredAttr : public Attr<typename Parser::Out> {
+            Parser parser;
+
+        public:
+            RequiredAttr(std::string name, Parser parser)
+                : Attr<typename Parser::Out>(name), parser(parser) {}
+
+            bool isRequired() override { return true; }
+
+            typename Parser::Out parse(std::optional<nix::fetchers::Attr> in) override {
+                if (in) {
+                    return parser.parse(*in);
+                } else {
+                    throw Error("required attribute '%s' not found", this->name);
+                }
+            }
+
+            std::shared_ptr<Schema> getAttrValueSchema() override {
+                return parser.getSchema();
+            }
+        };
+
+
+        /**
+            Perform a side effect for each item in a tuple. `f` must be callable for each item.
+        */
+        template <typename F, typename Tuple>
+        void traverse_(F f, Tuple&& t) {
+            std::apply([&](auto&&... args) { (f(args), ...); }, t);
+        }
+
+        /** Accepts an 'Attrs'. Composes 'Attr' (singular) parsers. */
+        template <typename Callable, typename... AttrParsers>
+        class Attrs
+            : public Parser<
+                nix::fetchers::Attrs,
+                std::invoke_result_t<Callable, typename AttrParsers::Out ...>> {
+            Callable lambda;
+            std::tuple<AttrParsers *...> parsers;
+            std::shared_ptr<Schema> schema;
+            Schema::Attrs * attrSchema;
+
+            void checkUnknownAttrs(nix::fetchers::Attrs input) {
+                // Zip by key linearly. (Avoids the extra log term of find.)
+                auto iActual = input.begin();
+                auto iExpected = attrSchema->attrs.begin();
+                while (iExpected != attrSchema->attrs.end() && iActual != input.end()) {
+                    if (iActual->first == iExpected->first) {
+                        iExpected++;
+                        iActual++;
+                    } else if (iActual->first > iExpected->first) {
+                        // (do nothing; .required is checked by the individual parser later)
+                        iExpected++;
+                    } else {
+                        throw Error("unexpected attribute '%s'", iActual->first);
+                    }
+                }
+                if (iActual != input.end()) {
+                    throw Error("unexpected attribute '%s'", iActual->first);
+                }
+            }
+
+        public:
+            Attrs(Callable lambda, AttrParsers *... parsers)
+                : lambda(lambda), parsers(parsers...) {
+                Schema::Attrs attrSchema;
+
+                traverse_(
+                    [this, &attrSchema](auto * parser) {
+                        attrSchema.attrs[parser->name] = parser->getAttrSchema();
+                    },
+                    this->parsers
+                );
+
+                schema = std::make_shared<Schema>(Schema{attrSchema});
+                this->attrSchema = std::get_if<Schema::Attrs>(&schema->choice);
+                assert(this->attrSchema);
+            }
+
+            std::invoke_result_t<Callable, typename AttrParsers::Out ...> parse(nix::fetchers::Attrs input) override {
+
+                checkUnknownAttrs(input);
+
+                return std::apply(
+                    [this, &input](auto *... parser) {
+                        return lambda(
+                            parseAttr(input, parser)...
+                        );
+                    },
+                    parsers
+                );
+            }
+
+            std::shared_ptr<Schema> getSchema() override {
+                return schema;
+            }
+        };
+
+    } // namespace parsers
+
+} // namespace nix::fetchers
--- src/libfetchers/tests/parser.cc
+++ src/libfetchers/tests/parser.cc
@@ -55,4 +55,148 @@ namespace nix::fetchers {
             ASSERT_THAT(e.what(), HasSubstr("expected a bool, but value is of type string"));
         }
     }
+
+    auto attrsParser1 = parsers::Attrs(
+        [](auto a, auto b, auto c) {
+            return std::make_tuple(a, b, c);
+        },
+        new RequiredAttr("a", String{}),
+        new OptionalAttr("b", Int{}),
+        new RequiredAttr("c", Bool{})
+    );
+
+    TEST(Attrs, schema_attrsParser1) {
+        ASSERT_EQ(
+            *(attrsParser1.getSchema()),
+            Schema {
+                Schema::Attrs({
+                    {
+                        std::string{"a"},
+                        Schema::Attrs::Attr {
+                            true,
+                            std::make_shared<Schema>(Schema::Primitive::String)
+                        }
+                    },
+                    {
+                        std::string{"b"},
+                        Schema::Attrs::Attr {
+                            false,
+                            std::make_shared<Schema>(Schema::Primitive::Int)
+                        }
+                    },
+                    {
+                        std::string{"c"},
+                        Schema::Attrs::Attr {
+                            true,
+                            std::make_shared<Schema>(Schema::Primitive::Bool)
+                        }
+                    }
+                })
+            }
+        );
+    }
+    TEST(Attrs, parse_attrsParser1) {
+        ASSERT_EQ(
+            attrsParser1.parse(
+                nix::fetchers::Attrs{
+                    { "a", "hi" },
+                    { "b", 101U },
+                    { "c", Explicit<bool>{true} }
+                }
+            ),
+            std::make_tuple("hi", 101U, true)
+        );
+    }
+    TEST(Attrs, parse_attrsParser1_missingOptional) {
+        ASSERT_EQ(
+            attrsParser1.parse(
+                nix::fetchers::Attrs{
+                    { "a", "hi" },
+                    { "c", Explicit<bool>{true} }
+                }
+            ),
+            std::make_tuple("hi", std::nullopt, true)
+        );
+    }
+    TEST(Attrs, parse_attrsParser1_missingRequired) {
+        try {
+            attrsParser1.parse(
+                nix::fetchers::Attrs{
+                    { "b", 101U },
+                    { "c", Explicit<bool>{true} }
+                }
+            );
+            FAIL();
+        } catch (Error & e) {
+            ASSERT_THAT(filterANSIEscapes(e.what(), true),
+                HasSubstr("while checking fetcher attribute 'a'"));
+            ASSERT_THAT(filterANSIEscapes(e.what(), true),
+                HasSubstr("required attribute 'a' not found"));
+        }
+    }
+    TEST(Attrs, parse_attrsParser1_wrongType) {
+        try {
+            attrsParser1.parse(
+                nix::fetchers::Attrs{
+                    { "a", "hi" },
+                    { "b", "hi" },
+                    { "c", Explicit<bool>{true} }
+                }
+            );
+            FAIL();
+        } catch (Error & e) {
+            ASSERT_THAT(filterANSIEscapes(e.what(), true),
+                HasSubstr("while checking fetcher attribute 'b'"));
+            ASSERT_THAT(filterANSIEscapes(e.what(), true),
+                HasSubstr("expected an int, but value is of type string"));
+        }
+    }
+    TEST(Attrs, parse_attrsParser1_extra_before) {
+        try {
+            attrsParser1.parse(
+                nix::fetchers::Attrs{
+                    { "0", "hi" },
+                    { "a", "hi" },
+                    { "b", 101U },
+                    { "c", Explicit<bool>{true} }
+                }
+            );
+            FAIL();
+        } catch (Error & e) {
+            ASSERT_THAT(filterANSIEscapes(e.what(), true),
+                HasSubstr("unexpected attribute '0'"));
+        }
+    }
+    TEST(Attrs, parse_attrsParser1_extra_after) {
+        try {
+            attrsParser1.parse(
+                nix::fetchers::Attrs{
+                    { "a", "hi" },
+                    { "b", 101U },
+                    { "c", Explicit<bool>{true} },
+                    { "d", "hi" }
+                }
+            );
+            FAIL();
+        } catch (Error & e) {
+            ASSERT_THAT(filterANSIEscapes(e.what(), true),
+                HasSubstr("unexpected attribute 'd'"));
+        }
+    }
+    TEST(Attrs, parse_attrsParser1_extra_between) {
+        try {
+            attrsParser1.parse(
+                nix::fetchers::Attrs{
+                    { "a", "hi" },
+                    { "aa", "hi" },
+                    { "b", 101U },
+                    { "c", Explicit<bool>{true} }
+                }
+            );
+            FAIL();
+        } catch (Error & e) {
+            ASSERT_THAT(filterANSIEscapes(e.what(), true),
+                HasSubstr("unexpected attribute 'aa'"));
+        }
+    }
 }
