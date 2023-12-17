a47fabff0dbcd63e2645db7336dde5865a1995c4
Julia Evans <julia@jvns.ca>
2023-12-17 12:14:55 -0500

    use params.isFetchGit instead to check if it came from fetchGit

--- src/libexpr/primops/fetchTree.cc
+++ src/libexpr/primops/fetchTree.cc
@@ -167,7 +167,7 @@ static void fetchTree(
         input = lookupInRegistries(state.store, input).first;
 
     if (evalSettings.pureEval && !input.isLocked())
-        if (type == "git")
+        if (params.isFetchGit)
             state.debugThrowLastTrace(EvalError("in pure evaluation mode, 'fetchGit' requires a locked input, at %s", state.positions[pos]));
         else
             state.debugThrowLastTrace(EvalError("in pure evaluation mode, 'fetchTree' requires a locked input, at %s", state.positions[pos]));
