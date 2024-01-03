bf42bf4e30b01947794c2c6477d41c8221826d13
Raito Bezarius <masterancpp@gmail.com>
2024-01-03 17:40:16 +0100

    fixup! chore: move signature code from libstore to libutil

--- src/libutil/signature/signer.cc
+++ src/libutil/signature/signer.cc
@@ -6,91 +6,91 @@
 #include <sodium.h>
 
 namespace nix {
-    // Splits a signature, that is `$publicKey:$signatureDigest` into a pair (publicKey, signatureDigest)
-    static std::pair<std::string_view, std::string_view> split(std::string_view s)
-    {
-        size_t colon = s.find(':');
-        if (colon == std::string::npos || colon == 0)
-            return {"", ""};
-        return {s.substr(0, colon), s.substr(colon + 1)};
+// Splits a signature, that is `$publicKey:$signatureDigest` into a pair (publicKey, signatureDigest)
+static std::pair<std::string_view, std::string_view> split(std::string_view s)
+{
+    size_t colon = s.find(':');
+    if (colon == std::string::npos || colon == 0)
+        return {"", ""};
+    return {s.substr(0, colon), s.substr(colon + 1)};
+}
+
+Signer::Signer() : pubkey(this->getPublicKey()) { }
+Signer::Signer(PublicKey && pubkey) : pubkey(pubkey) { }
+
+bool Signer::verifyDetached(const std::string & data, const std::string & sig) {
+    auto ss = split(sig);
+
+    if (std::string(ss.first) != pubkey.key) return false;
+
+    auto sig2 = base64Decode(ss.second);
+    if (sig2.size() != crypto_sign_BYTES)
+        throw Error("signature is not valid");
+
+    return crypto_sign_verify_detached((unsigned char *) sig2.data(),
+        (unsigned char *) data.data(), data.size(),
+        (unsigned char *) pubkey.key.data()) == 0;
+}
+
+const PublicKey& Signer::getPublicKey() const {
+    return pubkey;
+}
+
+LocalSigner::LocalSigner(SecretKey && privkey) : Signer(privkey.toPublicKey()), privkey(privkey) {
+}
+
+std::string LocalSigner::signDetached(std::string_view s) const {
+    return privkey.signDetached(s);
+}
+
+RemoteSigner::RemoteSigner(const std::string & remoteServerPath) : Signer(),
+    serverPath(remoteServerPath),
+    _curl_handle(std::unique_ptr<CURL, decltype(&curl_easy_cleanup)>(curl_easy_init(), curl_easy_cleanup)) {
+    if (!_curl_handle) {
+        throw Error("Failed to initialize curl");
     }
 
-    Signer::Signer() : pubkey(this->getPublicKey()) { }
-    Signer::Signer(PublicKey && pubkey) : pubkey(pubkey) { }
+    set_user_agent(_curl_handle.get(), "");
+    // Signing should be very fast, 5s is already very long.
+    curl_easy_setopt(_curl_handle.get(), CURLOPT_TIMEOUT, 5);
 
-    bool Signer::verifyDetached(const std::string & data, const std::string & sig) {
-        auto ss = split(sig);
-
-        if (std::string(ss.first) != pubkey.key) return false;
-
-        auto sig2 = base64Decode(ss.second);
-        if (sig2.size() != crypto_sign_BYTES)
-            throw Error("signature is not valid");
-
-        return crypto_sign_verify_detached((unsigned char *) sig2.data(),
-            (unsigned char *) data.data(), data.size(),
-            (unsigned char *) pubkey.key.data()) == 0;
+    // The remote URL should not have no trailing /.
+    if (serverPath.back() == '/') {
+        throw Error("Remote signing path `%s` contains a trailing `/`", serverPath);
     }
 
-    const PublicKey& Signer::getPublicKey() const {
-        return pubkey;
+    curl_easy_setopt(_curl_handle.get(), CURLOPT_UNIX_SOCKET_PATH, serverPath.c_str());
+}
+
+// Write the HTTP response inside the userdata string buffer provided.
+static size_t _writeResponse(void* ptr, size_t size, size_t nmemb, void* userdata) {
+    size_t real_size = size *nmemb;
+    std::string* response = static_cast<std::string*>(userdata);
+    response->append(static_cast<char*>(ptr), real_size);
+    return real_size;
+}
+
+std::string RemoteSigner::signDetached(std::string_view fingerprint) const {
+    std::string detached_signature;
+
+    // Since cURL 7.50, a valid URL must always be passed, we use a dummy hostname here.
+    curl_easy_setopt(_curl_handle.get(), CURLOPT_URL, "http://localhost/sign");
+    curl_easy_setopt(_curl_handle.get(), CURLOPT_WRITEFUNCTION, &_writeResponse);
+    curl_easy_setopt(_curl_handle.get(), CURLOPT_WRITEDATA, &detached_signature);
+    curl_easy_setopt(_curl_handle.get(), CURLOPT_POSTFIELDS, fingerprint.data());
+
+    CURLcode res = curl_easy_perform(_curl_handle.get());
+    if (res != CURLE_OK) {
+        // log the error?
+        throw Error("failed to sign remotely (curl error)");
     }
 
-    LocalSigner::LocalSigner(SecretKey && privkey) : Signer(privkey.toPublicKey()), privkey(privkey) {
+    long http_code = 0;
+    curl_easy_getinfo(_curl_handle.get(), CURLINFO_RESPONSE_CODE, &http_code);
+    if (http_code != 200) {
+        throw Error("failed to sign remotely (non-200 error code from server)");
     }
 
-    std::string LocalSigner::signDetached(std::string_view s) const {
-        return privkey.signDetached(s);
-    }
-
-    RemoteSigner::RemoteSigner(const std::string & remoteServerPath) : Signer(),
-        serverPath(remoteServerPath),
-        _curl_handle(std::unique_ptr<CURL, decltype(&curl_easy_cleanup)>(curl_easy_init(), curl_easy_cleanup)) {
-        if (!_curl_handle) {
-            throw Error("Failed to initialize curl");
-        }
-
-        set_user_agent(_curl_handle.get(), "");
-        // Signing should be very fast, 5s is already very long.
-        curl_easy_setopt(_curl_handle.get(), CURLOPT_TIMEOUT, 5);
-
-        // The remote URL should not have no trailing /.
-        if (serverPath.back() == '/') {
-            throw Error("Remote signing path `%s` contains a trailing `/`", serverPath);
-        }
-
-        curl_easy_setopt(_curl_handle.get(), CURLOPT_UNIX_SOCKET_PATH, serverPath.c_str());
-    }
-
-    // Write the HTTP response inside the userdata string buffer provided.
-    static size_t _writeResponse(void* ptr, size_t size, size_t nmemb, void* userdata) {
-        size_t real_size = size *nmemb;
-        std::string* response = static_cast<std::string*>(userdata);
-        response->append(static_cast<char*>(ptr), real_size);
-        return real_size;
-    }
-
-    std::string RemoteSigner::signDetached(std::string_view fingerprint) const {
-        std::string detached_signature;
-
-        // Since cURL 7.50, a valid URL must always be passed, we use a dummy hostname here.
-        curl_easy_setopt(_curl_handle.get(), CURLOPT_URL, "http://localhost/sign");
-        curl_easy_setopt(_curl_handle.get(), CURLOPT_WRITEFUNCTION, &_writeResponse);
-        curl_easy_setopt(_curl_handle.get(), CURLOPT_WRITEDATA, &detached_signature);
-        curl_easy_setopt(_curl_handle.get(), CURLOPT_POSTFIELDS, fingerprint.data());
-
-        CURLcode res = curl_easy_perform(_curl_handle.get());
-        if (res != CURLE_OK) {
-            // log the error?
-            throw Error("failed to sign remotely (curl error)");
-        }
-
-        long http_code = 0;
-        curl_easy_getinfo(_curl_handle.get(), CURLINFO_RESPONSE_CODE, &http_code);
-        if (http_code != 200) {
-            throw Error("failed to sign remotely (non-200 error code from server)");
-        }
-
-        return detached_signature;
-    }
+    return detached_signature;
+}
 }
