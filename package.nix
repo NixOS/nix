9b9ecdee3424056cb854bc8f1aa49fe330c08c83
John Ericson <John.Ericson@Obsidian.Systems>
2024-01-11 23:50:03 -0500

    Simplify RapidCheck configure
    No more `RAPIDCHECK_HEADERS`!

--- package.nix
+++ package.nix
@@ -309,7 +309,7 @@ in {
   ] ++ lib.optional (doBuild && stdenv.isLinux && !(stdenv.hostPlatform.isStatic && stdenv.system == "aarch64-linux"))
        "LDFLAGS=-fuse-ld=gold"
     ++ lib.optional (doBuild && stdenv.hostPlatform.isStatic) "--enable-embedded-sandbox-shell"
-    ++ lib.optional buildUnitTests "RAPIDCHECK_HEADERS=${lib.getDev rapidcheck}/extras/gtest/include";
+    ;
 
   enableParallelBuilding = true;
 
