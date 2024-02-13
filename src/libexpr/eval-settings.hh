e8aec317f09af20336d25819e7f46de891df01c4
Valentin Gagarin <valentin.gagarin@tweag.io>
2024-02-13 13:44:59 +0100

    add link

--- src/libexpr/eval-settings.hh
+++ src/libexpr/eval-settings.hh
@@ -21,7 +21,7 @@ struct EvalSettings : Config
     Setting<Strings> nixPath{
         this, getDefaultNixPath(), "nix-path",
         R"(
-          List of search paths to use for lookup paths.
+          List of search paths to use for [lookup paths](@docroot@/language/constructs/lookup-path.md).
           This setting determines the value of
           [`builtins.nixPath`](@docroot@/language/builtin-constants.md#builtins-nixPath).
 
