fd82ba0985aefc2a5498045f0caf16f8b2566cf1
Valentin Gagarin <valentin.gagarin@tweag.io>
2024-02-13 14:13:56 +0100

    extract reference documentation on remote builds (#9526)
    - move all reference documentation to the `builders` configuration setting
    - reword documentation on machine specification, add examples
    - disable showing the default value, as it rendered as `@/dummy/machines`, which is wrong
    - highlight the examples
    - link to the configuration docs for distributed builds
    - builder -> build machine
    
    Co-authored-by: Janik H <janik@aq0.de>

--- src/libstore/build/worker.cc
+++ src/libstore/build/worker.cc
@@ -331,13 +331,23 @@ void Worker::run(const Goals & _topGoals)
             if (awake.empty() && 0U == settings.maxBuildJobs)
             {
                 if (getMachines().empty())
-                   throw Error("unable to start any build; either increase '--max-jobs' "
-                            "or enable remote builds."
-                            "\nhttps://nixos.org/manual/nix/stable/advanced-topics/distributed-builds.html");
+                   throw Error(
+                        R"(
+                        Unable to start any build;
+                        either increase '--max-jobs' or enable remote builds.
+
+                        For more information run 'man nix.conf' and search for '/machines'.
+                        )"
+                    );
                 else
-                   throw Error("unable to start any build; remote machines may not have "
-                            "all required system features."
-                            "\nhttps://nixos.org/manual/nix/stable/advanced-topics/distributed-builds.html");
+                   throw Error(
+                        R"(
+                        Unable to start any build;
+                        remote machines may not have all required system features.
+
+                        For more information run 'man nix.conf' and search for '/machines'.
+                        )"
+                    );
 
             }
             assert(!awake.empty());
