NixOS/nix

commit bef68e53b9d08c99873a5bc60568b7351934ad51

Fix symlink handling
This restores the symlink handling behaviour prior to
94812cca98fbb157e5f64a15a85a2b852d289feb.

Fixes #9298.

(cherry picked from commit 31ebc6028b3682969d86a7b39ae87131c41cc604)

src/libexpr/parser.y
@@ -682,17 +682,25 @@ Expr * EvalState::parse(
 }
 
 
-SourcePath resolveExprPath(const SourcePath & path)
+SourcePath resolveExprPath(SourcePath path)
 {
+    unsigned int followCount = 0, maxFollow = 1024;
+
     /* If `path' is a symlink, follow it.  This is so that relative
        path references work. */
-    auto path2 = path.resolveSymlinks();
+    while (true) {
+        // Basic cycle/depth limit to avoid infinite loops.
+        if (++followCount >= maxFollow)
+            throw Error("too many symbolic links encountered while traversing the path '%s'", path);
+        if (path.lstat().type != InputAccessor::tSymlink) break;
+        path = {path.accessor, CanonPath(path.readLink(), path.path.parent().value_or(CanonPath::root))};
+    }
 
     /* If `path' refers to a directory, append `/default.nix'. */
-    if (path2.lstat().type == InputAccessor::tDirectory)
-        return path2 + "default.nix";
+    if (path.lstat().type == InputAccessor::tDirectory)
+        return path + "default.nix";
 
-    return path2;
+    return path;
 }
 
 

src/libexpr/eval.hh
@@ -802,7 +802,7 @@ std::string showType(const Value & v);
 /**
  * If `path` refers to a directory, then append "/default.nix".
  */
-SourcePath resolveExprPath(const SourcePath & path);
+SourcePath resolveExprPath(SourcePath path);
 
 struct InvalidPathError : EvalError
 {

tests/functional/lang/eval-okay-symlink-resolution.exp
@@ -1,0 +1,1 @@
+"test"

tests/functional/lang/eval-okay-symlink-resolution.nix
@@ -1,0 +1,1 @@
+import symlink-resolution/foo/overlays/overlay.nix

tests/functional/lang/symlink-resolution/foo/lib/default.nix
@@ -1,0 +1,1 @@
+"test"

tests/functional/lang/symlink-resolution/foo/overlays
@@ -1,0 +1,1 @@
+../overlays

tests/functional/lang/symlink-resolution/overlays/overlay.nix
@@ -1,0 +1,1 @@
+import ../lib

commit f36d4aefd3bb2112527d11d23eae994db5c320f1

Fix the build
bef68e53b9d08c99873a5bc60568b7351934ad51 (backport of
31ebc6028b3682969d86a7b39ae87131c41cc604) accidentally broke the build
because of a change in the constructor of `SourcePath` between 2.18 and
master. Fix that.

src/libexpr/parser.y
@@ -693,7 +693,7 @@ SourcePath resolveExprPath(SourcePath path)
         if (++followCount >= maxFollow)
             throw Error("too many symbolic links encountered while traversing the path '%s'", path);
         if (path.lstat().type != InputAccessor::tSymlink) break;
-        path = {path.accessor, CanonPath(path.readLink(), path.path.parent().value_or(CanonPath::root))};
+        path = {CanonPath(path.readLink(), path.path.parent().value_or(CanonPath::root))};
     }
 
     /* If `path' refers to a directory, append `/default.nix'. */