de36c639ea7277047cb76b568feb8f84943743cf
Simon Žlender <pub.git@zlender.si>
2024-03-21 22:36:02 +0100

    call-flake: test equivalent input overriding

--- tests/functional/flakes/equivalent-inputs.sh
+++ tests/functional/flakes/equivalent-inputs.sh
@@ -1,39 +1,40 @@
 source ./common.sh
+baseDir=$TEST_ROOT/$RANDOM
 
-test_equivalent_inputs() {
-    baseDir=$TEST_ROOT/$RANDOM
-    sharedFlake=$baseDir/shared
-    mkdir -p $sharedFlake
-
-    cat > $sharedFlake/flake.nix <<EOF
+# Create the common flake
+commonFlake=$baseDir/common
+mkdir -p $commonFlake
+cat > $commonFlake/flake.nix <<EOF
 {
   outputs = inputs: {
     legacyPackages.$system = {
-      default = builtins.trace "evaluating shared flake input" 1;
+      default = builtins.trace "evaluating common flake input" 1;
     };
   };
 }
 EOF
-    (cd $sharedFlake && nix flake lock)
 
-    flakeA=$baseDir/flakeA
-    mkdir -p $flakeA
-    cat > $flakeA/flake.nix <<EOF
+# Create flake A that imports the common flake
+flakeA=$baseDir/flakeA
+mkdir -p $flakeA
+cat > $flakeA/flake.nix <<EOF
 {
-  inputs.shared.url = "$sharedFlake";
-  outputs = { shared, ... }: {
-    legacyPackages.$system.default = shared.legacyPackages.$system.default;
+  inputs.common.url = "$commonFlake";
+  outputs = { common, ... }: {
+    legacyPackages.$system.default = common.legacyPackages.$system.default;
   };
 }
 EOF
-    (cd $flakeA && nix flake lock)
 
-    flakeB=$baseDir/flakeB
-    cp -r $flakeA $flakeB
+# Create flake B that also imports the common flake
+flakeB=$baseDir/flakeB
+cp -r $flakeA $flakeB
 
-    flakeRoot=$baseDir/flakeRoot
-    mkdir -p $flakeRoot
-    cat > $flakeRoot/flake.nix <<EOF
+# Create the root flake that imports flake A, flake B
+# and sums their outputs
+flakeRoot=$baseDir/flakeRoot
+mkdir -p $flakeRoot
+cat > $flakeRoot/flake.nix <<EOF
 {
   inputs.flakeA.url = "$flakeA";
   inputs.flakeB.url = "$flakeB";
@@ -44,11 +45,41 @@ EOF
   };
 }
 EOF
-    (cd $flakeRoot && nix flake lock)
-    (cd $flakeRoot && nix eval .#default) 1> stdout 2> stderr
-    [[ $(cat stdout) = 2 ]]
-    [[ $(cat stderr | grep "trace: evaluating shared flake input" | wc -l) = 1 ]]
-}
-test_equivalent_inputs
 
-# TODO(szlend): Test overrides
+(cd $commonFlake && nix flake lock)
+(cd $flakeA && nix flake lock)
+(cd $flakeRoot && nix flake lock)
+
+# Evaluate the root flake. The common flake input should only be evaluated once
+(cd $flakeRoot && nix eval .#default) 1> stdout 2> stderr
+[[ $(cat stdout) = 2 ]]
+[[ $(cat stderr | grep "trace: evaluating common flake input" | wc -l) = 1 ]]
+
+# Create the override flake
+overrideFlake=$baseDir/override
+mkdir -p $overrideFlake
+
+cat > $overrideFlake/flake.nix <<EOF
+{
+  outputs = inputs: {
+    legacyPackages.$system = {
+      default = builtins.trace "evaluating override flake input" 2;
+    };
+  };
+}
+EOF
+
+(cd $overrideFlake && nix flake lock)
+
+# Evaluate the root flake with overrides. Both the common and
+# overriden flakes should be evaluated
+(cd $flakeRoot && nix eval --override-input flakeB $overrideFlake .#default) 1> stdout 2> stderr
+[[ $(cat stdout) = 3 ]]
+[[ $(cat stderr | grep "trace: evaluating common flake input" | wc -l) = 1 ]]
+[[ $(cat stderr | grep "trace: evaluating override flake input" | wc -l) = 1 ]]
+
+# Same when overriding other inputs' inputs
+(cd $flakeRoot && nix eval --override-input flakeB/common $overrideFlake .#default) 1> stdout 2> stderr
+[[ $(cat stdout) = 3 ]]
+[[ $(cat stderr | grep "trace: evaluating common flake input" | wc -l) = 1 ]]
+[[ $(cat stderr | grep "trace: evaluating override flake input" | wc -l) = 1 ]]
