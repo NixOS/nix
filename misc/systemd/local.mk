d2421ae573e3d8ce86d3338e3cdb7c87326dc340
polar <polar@ever3st.com>
2024-03-24 17:44:51 +0000

    removed make buildsystem

--- misc/systemd/local.mk
+++
@@ -1,8 +0,0 @@
-ifdef HOST_LINUX
-
-  $(foreach n, nix-daemon.socket nix-daemon.service, $(eval $(call install-file-in, $(d)/$(n), $(prefix)/lib/systemd/system, 0644)))
-  $(foreach n, nix-daemon.conf, $(eval $(call install-file-in, $(d)/$(n), $(prefix)/lib/tmpfiles.d, 0644)))
-
-  clean-files += $(d)/nix-daemon.socket $(d)/nix-daemon.service $(d)/nix-daemon.conf
-
-endif
