930bb21893da7ed837d6db88b6611ec988c83f17
Théophane Hufschmitt <theophane.hufschmitt@tweag.io>
2024-04-02 17:06:48 +0200

    Run the builds in a daemon-controled directory
    Instead of running the builds under
    `$TMPDIR/{unique-build-directory-owned-by-the-build-user}`, run them
    under `$TMPDIR/{unique-build-directory-owned-by-the-daemon}/{subdir-owned-by-the-build-user}`
    where the build directory is only readable and traversable by the daemon user.
    
    This achieves two things:
    
    1. It prevents builders from making their build directory world-readable
       (or even writeable), which would allow the outside world to interact
       with them.
    2. It prevents external processes running as the build user (either
       because that somehow leaked, maybe as a consequence of 1., or because
       `build-users` isn't in use) from gaining access to the build
       directory.
    
    (cherry picked from commit 1d3696f0fb88d610abc234a60e0d6d424feafdf1)

--- src/libutil/file-system.cc
+++ src/libutil/file-system.cc
@@ -412,6 +412,11 @@ void deletePath(const fs::path & path)
     deletePath(path, dummy);
 }
 
+void createDir(const Path &path, mode_t mode)
+{
+    if (mkdir(path.c_str(), mode) == -1)
+        throw SysError("creating directory '%1%'", path);
+}
 
 Paths createDirs(const Path & path)
 {
--- tests/functional/check.sh
+++ tests/functional/check.sh
@@ -46,7 +46,7 @@ test_custom_build_dir() {
       --no-out-link --keep-failed --option build-dir "$TEST_ROOT/custom-build-dir" 2> $TEST_ROOT/log || status=$?
   [ "$status" = "100" ]
   [[ 1 == "$(count "$customBuildDir/nix-build-"*)" ]]
-  local buildDir="$customBuildDir/nix-build-"*
+  local buildDir="$customBuildDir/nix-build-"*"/build"
   grep $checkBuildId $buildDir/checkBuildId
 }
 test_custom_build_dir
