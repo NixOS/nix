bd7c26bc7bb7af37c21d46faf28aa0e88606b98f
John Ericson <John.Ericson@Obsidian.Systems>
2024-04-07 21:37:30 -0400

    Add comment explaining `LIBMOUNT_FORCE_MOUNT2=always`

--- tests/functional/local-overlay-store/common.sh
+++ tests/functional/local-overlay-store/common.sh
@@ -1,5 +1,26 @@
 source ../common.sh
 
+# The new Linux mount interface does not seem to support remounting
+# OverlayFS mount points.
+#
+# It is not clear whether this is intentional or not:
+#
+# The  kernel source code [1] would seem to indicate merely remounting
+# while *changing* mount options is now an error because it erroneously
+# succeeded (by ignoring those new options) before. However, we are
+# *not* trying to remount with changed options, and are still hitting
+# the failure when using the new interface.
+#
+# For further details, see these `util-linux` issues:
+#
+#  - https://github.com/util-linux/util-linux/issues/2528
+#  - https://github.com/util-linux/util-linux/issues/2576
+#
+# In the meantime, setting this environment variable to "always" will
+# force the use of the old mount interface, keeping the remounting
+# working and these tests passing.
+#
+# [1]: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/tree/fs/overlayfs/params.c?id=3006adf3be79cde4d14b1800b963b82b6e5572e0#n549
 export LIBMOUNT_FORCE_MOUNT2=always
 
 requireEnvironment () {
