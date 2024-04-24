36cc8d5f4b221ad27e1a172148363e9780329277
Eelco Dolstra <edolstra@gmail.com>
2024-04-24 15:26:18 +0200

    Run the flake-regressions test suite

---
+++ scripts/flake-regressions.sh
@@ -0,0 +1,27 @@
+#! /usr/bin/env bash
+
+set -e
+
+echo "Nix version:"
+nix --version
+
+cd flake-regressions
+
+status=0
+
+flakes=$(ls -d tests/*/*/* | head -n25)
+
+echo "Running flake tests..."
+
+for flake in $flakes; do
+
+    if ! REGENERATE=0 ./eval-flake.sh $flake; then
+        status=1
+        echo "❌ $flake"
+    else
+        echo "✅ $flake"
+    fi
+
+done
+
+exit "$status"
