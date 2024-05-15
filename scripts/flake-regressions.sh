b6be10dcf98e8509c729fb890eb621c289a973ce
Eelco Dolstra <edolstra@gmail.com>
2024-05-15 14:41:13 +0200

    Skip a broken test

--- scripts/flake-regressions.sh
+++ scripts/flake-regressions.sh
@@ -15,6 +15,12 @@ echo "Running flake tests..."
 
 for flake in $flakes; do
 
+    # This test has a bad flake.lock that doesn't include
+    # `lastModified` for its nixpkgs input. (#10612)
+    if [[ $flake == tests/the-nix-way/nome/0.1.2 ]]; then
+        continue
+    fi
+
     if ! REGENERATE=0 ./eval-flake.sh $flake; then
         status=1
         echo "❌ $flake"
