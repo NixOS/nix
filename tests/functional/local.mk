c8f8368a6271dfcbd6afd42cdde66753c5b07429
Eelco Dolstra <edolstra@gmail.com>
2024-05-24 16:16:38 +0200

    Add test for the evaluation cache

--- tests/functional/local.mk
+++ tests/functional/local.mk
@@ -16,6 +16,7 @@ nix_tests = \
   flakes/build-paths.sh \
   flakes/flake-in-submodule.sh \
   flakes/prefetch.sh \
+  flakes/eval-cache.sh \
   gc.sh \
   nix-collect-garbage-d.sh \
   remote-store.sh \
