65884201035a3771040d462538072b2d7a0a2c63
John Ericson <John.Ericson@Obsidian.Systems>
2024-06-04 10:17:58 +0200

    Bring back FreeBSD

--- flake.nix
+++ flake.nix
@@ -46,6 +46,7 @@
         "armv7l-unknown-linux-gnueabihf"
         "riscv64-unknown-linux-gnu"
         "x86_64-unknown-netbsd"
+        "x86_64-unknown-freebsd"
         "x86_64-w64-mingw32"
       ];
 
