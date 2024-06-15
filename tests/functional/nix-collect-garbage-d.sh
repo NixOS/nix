c45d66bad1a1cbf3c7a702e02b2b9461d31c1405
Michael Oultram <michael@oultr.am>
2024-06-15 23:02:09 +0100

    nix-collect-garbage: extend test coverage
    * check exact generation ids instead of just the count
    * test --delete-older-than days

--- tests/functional/nix-collect-garbage-d.sh
+++ tests/functional/nix-collect-garbage-d.sh
@@ -22,20 +22,38 @@ testCollectGarbageSetup () {
     nix-env -f ./user-envs.nix -i foo-2.0 "$@"
     nix-env -f ./user-envs.nix -i bar-0.1.1 "$@"
     nix-env -f ./user-envs.nix -i foo-0.1 "$@"
-    [[ $(nix-env --list-generations "$@" | wc -l) -eq 6 ]]
+    testCollectGarbageCheckGenerations "1 2 3 4 5 6" "$@"
+}
+
+testCollectGarbageMockTimestamps () {
+    # Modifies the date the generations were activated
+    profiles_prefix=${1:-"${profiles}/profile"}
+    touch -h -m -d "  -4 days           -1 seconds" "${profiles_prefix}-1-link"
+    touch -h -m -d "  -3 days           -1 seconds" "${profiles_prefix}-2-link"
+    touch -h -m -d "  -2 days -12 hours -1 seconds" "${profiles_prefix}-3-link"
+    touch -h -m -d "  -2 days           -1 seconds" "${profiles_prefix}-4-link"
+    touch -h -m -d "  -1 days -16 hours -1 seconds" "${profiles_prefix}-5-link"
+    touch -h -m -d "  -1 days  -8 hours -1 seconds" "${profiles_prefix}-6-link"
+}
+
+testCollectGarbageCheckGenerations () {
+    # expected should be a space separated list of generation ids
+    expected="$1"
+    actual=$(nix-env --list-generations "${@:2}" | awk '{{print $1}}' | tr '\n' ' ')
+    [[ "${actual%" "}" == "${expected}" ]]
 }
 
 # Basic test that should leave 1 generation
 testCollectGarbageSetup
 nix-collect-garbage -d
-[[ $(nix-env --list-generations | wc -l) -eq 1 ]]
+testCollectGarbageCheckGenerations "6"
 
 # Run the same test, but forcing the profiles an arbitrary location.
 rm ~/.nix-profile
 ln -s $TEST_ROOT/blah ~/.nix-profile
 testCollectGarbageSetup
 nix-collect-garbage -d
-[[ $(nix-env --list-generations | wc -l) -eq 1 ]]
+testCollectGarbageCheckGenerations "6"
 
 # Run the same test, but forcing the profiles at their legacy location under
 # /nix/var/nix.
@@ -48,4 +66,44 @@ nix-collect-garbage -d
 rm ~/.nix-profile
 testCollectGarbageSetup --profile "$NIX_STATE_DIR/profiles/per-user/me"
 nix-collect-garbage -d
-[[ $(nix-env --list-generations --profile "$NIX_STATE_DIR/profiles/per-user/me" | wc -l) -eq 1 ]]
+testCollectGarbageCheckGenerations "6" --profile "$NIX_STATE_DIR/profiles/per-user/me"
+
+# Delete all except latest 4 generations
+testCollectGarbageSetup
+nix-collect-garbage --delete-older-than "+4"
+testCollectGarbageCheckGenerations "3 4 5 6"
+
+# Delete everything older than 5 days
+# All profiles are younger than 5 days
+testCollectGarbageSetup
+testCollectGarbageMockTimestamps
+nix-collect-garbage --delete-older-than "5d"
+testCollectGarbageCheckGenerations "1 2 3 4 5 6"
+
+# Delete everything older than 4 days
+# Profile 1 was active at 4 days ago so it will be kept
+testCollectGarbageSetup
+testCollectGarbageMockTimestamps
+nix-collect-garbage --delete-older-than "4d"
+testCollectGarbageCheckGenerations "1 2 3 4 5 6"
+
+# Delete everything older than 3 days
+# Profile 2 was active at 3 days ago so it will be kept
+testCollectGarbageSetup
+testCollectGarbageMockTimestamps
+nix-collect-garbage --delete-older-than "3d"
+testCollectGarbageCheckGenerations "2 3 4 5 6"
+
+# Delete everything older than 2 days
+# Profile 4 was active at 2 days ago so it will be kept
+testCollectGarbageSetup
+testCollectGarbageMockTimestamps
+nix-collect-garbage --delete-older-than "2d"
+testCollectGarbageCheckGenerations "4 5 6"
+
+# Delete everything older than 1 day
+# Profile 6 is current and was active at 1 day ago so it will be kept
+testCollectGarbageSetup
+testCollectGarbageMockTimestamps
+nix-collect-garbage --delete-older-than "1d"
+testCollectGarbageCheckGenerations "6"
