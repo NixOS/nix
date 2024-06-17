245dbb7a61e47cf7673c21544cbc7e6158667900
Eelco Dolstra <edolstra@gmail.com>
2024-06-17 15:22:53 +0200

    Revert "Add plumbing for the DetSys installer flake"
    This reverts commit 0f9ea197055eabe5fac3ef93e49ba5552668fa08.

--- flake.nix
+++ flake.nix
@@ -22,6 +22,7 @@
 
   outputs = inputs@{ self, nixpkgs, nixpkgs-regression, libgit2, ... }:
 
+
     let
       inherit (nixpkgs) lib;
 
@@ -356,9 +357,5 @@
               default = self.devShells.${system}.native-stdenvPackages;
             }
         );
-
-      # Expected by the DeterminateSystems/nix-installer flake.
-      tarballs_indirect = forAllSystems (system: self.checks."${system}".binaryTarball);
-      tarballs_direct = forAllSystems (system: "${self.checks."${system}".binaryTarball}/nix-${self.packages."${system}".default.version}-${system}.tar.xz");
-    };
+  };
 }
