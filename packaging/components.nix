451f8a8c19e2ab95999553f5bf3a1fb056877933
John Ericson <John.Ericson@Obsidian.Systems>
2024-07-01 00:25:57 -0400

    Put back files for now
    We'll revert this sometime later

--- packaging/components.nix
+++ packaging/components.nix
@@ -9,24 +9,24 @@ in
 
   nix-util = callPackage ../src/libutil/package.nix { };
   nix-util-c = callPackage ../src/libutil-c/package.nix { };
-  nix-util-test-support = callPackage ../src/nix-util-test-support/package.nix { };
-  nix-util-tests = callPackage ../src/nix-util-tests/package.nix { };
+  nix-util-test-support = callPackage ../tests/unit/libutil-support/package.nix { };
+  nix-util-tests = callPackage ../tests/unit/libutil/package.nix { };
 
   nix-store = callPackage ../src/libstore/package.nix { };
   nix-store-c = callPackage ../src/libstore-c/package.nix { };
-  nix-store-test-support = callPackage ../src/nix-store-test-support/package.nix { };
-  nix-store-tests = callPackage ../src/nix-store-tests/package.nix { };
+  nix-store-test-support = callPackage ../tests/unit/libstore-support/package.nix { };
+  nix-store-tests = callPackage ../tests/unit/libstore/package.nix { };
 
   nix-fetchers = callPackage ../src/libfetchers/package.nix { };
-  nix-fetchers-tests = callPackage ../src/nix-fetchers-tests/package.nix { };
+  nix-fetchers-tests = callPackage ../tests/unit/libfetchers/package.nix { };
 
   nix-expr = callPackage ../src/libexpr/package.nix { };
   nix-expr-c = callPackage ../src/libexpr-c/package.nix { };
-  nix-expr-test-support = callPackage ../src/nix-expr-test-support/package.nix { };
-  nix-expr-tests = callPackage ../src/nix-expr-tests/package.nix { };
+  nix-expr-test-support = callPackage ../tests/unit/libexpr-support/package.nix { };
+  nix-expr-tests = callPackage ../tests/unit/libexpr/package.nix { };
 
   nix-flake = callPackage ../src/libflake/package.nix { };
-  nix-flake-tests = callPackage ../src/nix-flake-tests/package.nix { };
+  nix-flake-tests = callPackage ../tests/unit/libflake/package.nix { };
 
   nix-internal-api-docs = callPackage ../src/internal-api-docs/package.nix { };
   nix-external-api-docs = callPackage ../src/external-api-docs/package.nix { };
---
+++ tests/unit/libutil/package.nix
@@ -0,0 +1,100 @@
+{ lib
+, stdenv
+, mkMesonDerivation
+, releaseTools
+
+, meson
+, ninja
+, pkg-config
+
+, nix-util
+, nix-util-c
+, nix-util-test-support
+
+, rapidcheck
+, gtest
+, runCommand
+
+# Configuration Options
+
+, versionSuffix ? ""
+}:
+
+let
+  inherit (lib) fileset;
+
+  version = lib.fileContents ./.version + versionSuffix;
+in
+
+mkMesonDerivation (finalAttrs: {
+  pname = "nix-util-tests";
+  inherit version;
+
+  workDir = ./.;
+  fileset = fileset.unions [
+    ../../../build-utils-meson
+    ./build-utils-meson
+    ../../../.version
+    ./.version
+    ./meson.build
+    # ./meson.options
+    (fileset.fileFilter (file: file.hasExt "cc") ./.)
+    (fileset.fileFilter (file: file.hasExt "hh") ./.)
+  ];
+
+  outputs = [ "out" "dev" ];
+
+  nativeBuildInputs = [
+    meson
+    ninja
+    pkg-config
+  ];
+
+  buildInputs = [
+    nix-util
+    nix-util-c
+    nix-util-test-support
+    rapidcheck
+    gtest
+  ];
+
+  preConfigure =
+    # "Inline" .version so it's not a symlink, and includes the suffix.
+    # Do the meson utils, without modification.
+    ''
+      chmod u+w ./.version
+      echo ${version} > ../../../.version
+    '';
+
+  mesonFlags = [
+  ];
+
+  env = lib.optionalAttrs (stdenv.isLinux && !(stdenv.hostPlatform.isStatic && stdenv.system == "aarch64-linux")) {
+    LDFLAGS = "-fuse-ld=gold";
+  };
+
+  enableParallelBuilding = true;
+
+  separateDebugInfo = !stdenv.hostPlatform.isStatic;
+
+  strictDeps = true;
+
+  hardeningDisable = lib.optional stdenv.hostPlatform.isStatic "pie";
+
+  passthru = {
+    tests = {
+      run = runCommand "${finalAttrs.pname}-run" {
+      } ''
+        PATH="${lib.makeBinPath [ finalAttrs.finalPackage ]}:$PATH"
+        export _NIX_TEST_UNIT_DATA=${./data}
+        nix-util-tests
+        touch $out
+      '';
+    };
+  };
+
+  meta = {
+    platforms = lib.platforms.unix ++ lib.platforms.windows;
+  };
+
+})
