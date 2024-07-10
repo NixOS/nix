c4e3e2dc27da93cae22cd35a11ee1ef87e23eb57
Robert Hensing <robert@roberthensing.nl>
2024-07-10 16:24:31 +0200

    Soft-deprecate the compatibility settings

--- src/libcmd/compatibility-settings.hh
+++ src/libcmd/compatibility-settings.hh
@@ -7,14 +7,18 @@ struct CompatibilitySettings : public Config
 
     CompatibilitySettings() = default;
 
+    // Added in Nix 2.24, July 2024.
     Setting<bool> nixShellAlwaysLooksForShellNix{this, true, "nix-shell-always-looks-for-shell-nix", R"(
         Before Nix 2.24, [`nix-shell`](@docroot@/command-ref/nix-shell.md) would only look at `shell.nix` if it was in the working directory - when no file was specified.
 
         Since Nix 2.24, `nix-shell` always looks for a `shell.nix`, whether that's in the working directory, or in a directory that was passed as an argument.
 
         You may set this to `false` to revert to the Nix 2.3 behavior.
+
+        This setting is not recommended, and will be deprecated and later removed in the future.
     )"};
 
+    // Added in Nix 2.24, July 2024.
     Setting<bool> nixShellShebangArgumentsRelativeToScript{
         this, true, "nix-shell-shebang-arguments-relative-to-script", R"(
         Before Nix 2.24, the arguments in a `nix-shell` shebang - as well as `--arg` - were relative to working directory.
@@ -22,6 +26,8 @@ struct CompatibilitySettings : public Config
         Since Nix 2.24, the arguments are relative to the [base directory](@docroot@/glossary.md#gloss-base-directory) defined as the script's directory.
 
         You may set this to `false` to revert to the Nix 2.3 behavior.
+
+        This setting is not recommended, and will be deprecated and later removed in the future.
     )"};
 };
 
