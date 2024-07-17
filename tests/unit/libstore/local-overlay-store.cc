8886d948a0d580bf517c4a91f7672cea5f596451
John Ericson <John.Ericson@Obsidian.Systems>
2024-07-17 17:12:10 -0400

    Comment out another test

--- tests/unit/libstore/local-overlay-store.cc
+++ tests/unit/libstore/local-overlay-store.cc
@@ -1,6 +1,9 @@
-#include <gtest/gtest.h>
+// FIXME: Odd failures for templates that are causing the PR to break
+// for now with discussion with @Ericson2314 to comment out.
+#if 0
+#  include <gtest/gtest.h>
 
-#include "local-overlay-store.hh"
+#  include "local-overlay-store.hh"
 
 namespace nix {
 
@@ -28,3 +31,4 @@ TEST(LocalOverlayStore, constructConfig_rootPath)
 }
 
 } // namespace nix
+#endif
