474387991b7d49231c998ad54992fa09665cdd52
Jörg Thalheim <joerg@thalheim.io>
2024-09-25 14:13:33 +0200

    netsbsd: disable cross-compilation
    Cross-compilation of curl is broken in nixpkgs. Therefore we disable it
    until nixpkgs fixes the underlying issues.

--- flake.nix
+++ flake.nix
@@ -36,7 +36,8 @@
         "armv6l-unknown-linux-gnueabihf"
         "armv7l-unknown-linux-gnueabihf"
         "riscv64-unknown-linux-gnu"
-        "x86_64-unknown-netbsd"
+        # Disabled because of https://github.com/NixOS/nixpkgs/issues/344423
+        # "x86_64-unknown-netbsd"
         "x86_64-unknown-freebsd"
         "x86_64-w64-mingw32"
       ];
