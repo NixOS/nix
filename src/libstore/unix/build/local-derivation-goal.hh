71ab474b49e6dff41e6f8853fcc63a896b7110d6
Bryan Honof <bryanhonof@gmail.com>
2024-10-01 21:56:47 +0200

    fix: use portable C++ RNG
    I used "A Tour of C++, Third Edition" (ISBN-10 0136816487) as inspiration for the Random class.
    This replaces all occurrences of `rand()` and `srand()`.
    
    Co-authored-by: John Ericson <ericson2314@users.noreply.github.com>
    Co-authored-by: Eelco Dolstra <edolstra@gmail.com>

--- src/libstore/unix/build/local-derivation-goal.hh
+++ src/libstore/unix/build/local-derivation-goal.hh
@@ -307,6 +307,8 @@ struct LocalDerivationGoal : public DerivationGoal
      * rewrites caught everything
      */
     StorePath makeFallbackPath(OutputNameView outputName);
+
+    void replaceValidPath(const Path & storePath, const Path & tmpPath);
 };
 
 }
