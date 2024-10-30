21ddd181fe6d4b8d345b783e72aa54b2340fb541
Eelco Dolstra <edolstra@gmail.com>
2024-10-30 12:53:05 +0100

    Disable mingw32 jobs
    This gets rid of an evaluation error in
    devShells.aarch64-linux.x86_64-w64-mingw32.
    
    https://github.com/DeterminateSystems/nix-priv/actions/runs/11576785241/job/32226923043?pr=39

--- flake.nix
+++ flake.nix
@@ -44,7 +44,7 @@
         "riscv64-unknown-linux-gnu"
         "x86_64-unknown-netbsd"
         "x86_64-unknown-freebsd"
-        "x86_64-w64-mingw32"
+        #"x86_64-w64-mingw32"
       ];
 
       stdenvs = [
