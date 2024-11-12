9fd776200bdaabf8199d8f82c16e082d651fce1a
John Ericson <John.Ericson@Obsidian.Systems>
2024-11-12 00:48:40 -0500

    Fix argument order in the Windows implementation of `getEnvOs`
    See the build failure in https://github.com/msys2/MINGW-packages/pull/22499
    
    (cherry picked from commit 355f08a728f73f21e287926c3fded19ed975e40d)
    
    # Conflicts:
    #	src/libutil/windows/environment-variables.cc

--- src/libutil/windows/environment-variables.cc
+++ src/libutil/windows/environment-variables.cc
@@ -4,7 +4,34 @@
 
 namespace nix {
 
+<<<<<<< HEAD
 int unsetenv(const char *name)
+=======
+std::optional<OsString> getEnvOs(const OsString & key)
+{
+    // Determine the required buffer size for the environment variable value
+    DWORD bufferSize = GetEnvironmentVariableW(key.c_str(), nullptr, 0);
+    if (bufferSize == 0) {
+        return std::nullopt;
+    }
+
+    // Allocate a buffer to hold the environment variable value
+    std::wstring value{bufferSize, L'\0'};
+
+    // Retrieve the environment variable value
+    DWORD resultSize = GetEnvironmentVariableW(key.c_str(), &value[0], bufferSize);
+    if (resultSize == 0) {
+        return std::nullopt;
+    }
+
+    // Resize the string to remove the extra null characters
+    value.resize(resultSize);
+
+    return value;
+}
+
+int unsetenv(const char * name)
+>>>>>>> 355f08a72 (Fix argument order in the Windows implementation of `getEnvOs`)
 {
     return -SetEnvironmentVariableA(name, nullptr);
 }
