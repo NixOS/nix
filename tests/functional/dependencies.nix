1485937b897bc88cda0848b54b8ad9310230c47b
Robert Hensing <robert@roberthensing.nl>
2024-11-20 11:16:39 +0100

    tests/functional/dependencies.nix: Check that we tolerate syntax and type errors

--- tests/functional/dependencies.nix
+++ tests/functional/dependencies.nix
@@ -40,6 +40,8 @@ let
         echo "@nix 1"
         echo "@nix {}"
         echo '@nix {"action": null}'
+        echo '@nix {"action": 123}'
+        echo '@nix ]['
       } >&$NIX_LOG_FD
       touch $out
     '';
