1deca0cd048bbbd1a3c6fd03ce53d8cbd98a5bf2
Eelco Dolstra <edolstra@gmail.com>
2024-11-25 15:11:40 +0100

    builtins.getFlake: Allow inputs to overridden
    This uses the same syntax as flake inputs in flake.nix, e.g.
    
      builtins.getFlake {
        url = "github:NixOS/nix/55bc52401966fbffa525c574c14f67b00bc4fb3a";
        inputs.nixpkgs.url = "github:NixOS/nixpkgs/c69a9bffbecde46b4b939465422ddc59493d3e4d";
      }
    
    Note that currently it's not supported to set `flake = false` or to
    use `follows` (because lockFlake() doesn't support that for CLI
    overrides), but it could be implemented in the future.
    
    Fixes #9154.

--- tests/functional/flakes/get-flake.sh
+++ tests/functional/flakes/get-flake.sh
@@ -21,3 +21,23 @@ nix build -o "$TEST_ROOT/result" --expr "(builtins.getFlake \"git+file://$flake1
 
 # But should succeed in impure mode.
 nix eval --expr "builtins.getFlake \"$flake2Dir\"" --impure
+
+# Test overrides in getFlake.
+flake1Copy="$flake1Dir-copy"
+rm -rf "$flake1Copy"
+cp -r "$flake1Dir" "$flake1Copy"
+sed -i "$flake1Copy/simple.builder.sh" -e 's/World/Universe/'
+
+# Should fail in pure mode since the override is unlocked.
+(! nix build -o "$TEST_ROOT/result" --expr "(builtins.getFlake { url = \"$flake2Dir\"; inputs.flake1.url = \"$flake1Copy\"; }).packages.$system.bar")
+
+# Should succeed in impure mode.
+nix build -o "$TEST_ROOT/result" --expr "(builtins.getFlake { url = \"$flake2Dir\"; inputs.flake1.url = \"$flake1Copy\"; }).packages.$system.bar" --impure
+[[ $(cat "$TEST_ROOT/result/hello") = 'Hello Universe!' ]]
+
+# Should succeed if we lock the override.
+git -C "$flake1Copy" commit -a -m 'bla'
+
+flake1CopyLocked="$(nix flake metadata --json "$flake1Copy" | jq -r .url)"
+
+nix build -o "$TEST_ROOT/result" --expr "(builtins.getFlake { url = \"$flake2Dir\"; inputs.flake1.url = \"$flake1CopyLocked\"; }).packages.$system.bar"
