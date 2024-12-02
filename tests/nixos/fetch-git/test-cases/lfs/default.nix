85d6efb40db95181ee23e307da61758f016f22a0
Brian Camacho <hello@briancamacho.me>
2024-12-02 14:52:51 -0500

    trim storepath in test

--- tests/nixos/fetch-git/test-cases/lfs/default.nix
+++ tests/nixos/fetch-git/test-cases/lfs/default.nix
@@ -196,7 +196,10 @@
         nix eval {tempdir}#.outPath
       """)
 
-    assert fetched_lfs == fetched_flake, \
-      f"fetching as flake input (store path {fetched_flake}) yielded a different result than using fetchGit (store path {fetched_lfs})"
+    # strip leading and trailing quotes
+    fetched_flake_trimmed = fetched_flake.strip()[1:-1]
+
+    assert fetched_lfs == fetched_flake_trimmed, \
+      f"fetching as flake input (store path {fetched_flake_trimmed}) yielded a different result than using fetchGit (store path {fetched_lfs})"
   '';
 }
