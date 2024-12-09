b32f6047ce6a5b1c7908d19b43b1378ea6d1f18b
Jörg Thalheim <joerg@thalheim.io>
2024-12-09 16:03:36 +0100

    tests/nixos: disable nixos-option
    (cherry picked from commit 20ee83fffde3d5892cf012dedeacaf11ce45cf7c)
    
    # Conflicts:
    #	tests/nixos/default.nix

--- tests/nixos/default.nix
+++ tests/nixos/default.nix
@@ -23,6 +23,14 @@ let
         nix.checkAllErrors = false;
         # TODO: decide which packaging stage to use. `nix-cli` is efficient, but not the same as the user-facing `everything.nix` package (`default`). Perhaps a good compromise is `everything.nix` + `noTests` defined above?
         nix.package = nixpkgsFor.${system}.native.nixComponents.nix-cli;
+<<<<<<< HEAD
+=======
+
+        # Evaluate VMs faster
+        documentation.enable = false;
+        # this links against nix and might break with our git version.
+        system.tools.nixos-option.enable = false;
+>>>>>>> 20ee83fff (tests/nixos: disable nixos-option)
       };
       _module.args.nixpkgs = nixpkgs;
       _module.args.system = system;
