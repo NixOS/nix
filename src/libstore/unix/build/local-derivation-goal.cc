55c1e4454ed08fa5c8cec2b3d1ed13de27a61d97
Aiden Fox Ivey <aiden@aidenfoxivey.com>
2025-02-10 11:37:37 -0500

    Update src/libstore/unix/build/local-derivation-goal.cc
    Co-authored-by: Robert Hensing <roberth@users.noreply.github.com>

--- src/libstore/unix/build/local-derivation-goal.cc
+++ src/libstore/unix/build/local-derivation-goal.cc
@@ -534,7 +534,7 @@ void LocalDerivationGoal::startBuilder()
     if (!parsedDrv->canBuildLocally(worker.store)) {
         // since aarch64-darwin has Rosetta 2, this user can actually run x86_64-darwin on their hardware - we should tell them to run the command to install Darwin 2
         if (drv->platform == "x86_64-darwin" && settings.thisSystem == "aarch64-darwin") {
-            throw Error("run `/usr/sbin/softwareupdate --install-rosetta --agree-to-license` to enable your %s to run programs for %s", settings.thisSystem, drv->platform);
+            throw Error("run `/usr/sbin/softwareupdate --install-rosetta` to enable your %s to run programs for %s", settings.thisSystem, drv->platform);
         } else {
             throw Error("a '%s' with features {%s} is required to build '%s', but I am a '%s' with features {%s}",
                 drv->platform,
