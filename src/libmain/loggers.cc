c0c14698a37e9dc3159b8edad28b92f18add0e09
Eelco Dolstra <edolstra@gmail.com>
2025-02-17 18:09:59 +0100

    Use getStandardError()

--- src/libmain/loggers.cc
+++ src/libmain/loggers.cc
@@ -27,7 +27,7 @@ Logger * makeDefaultLogger() {
     case LogFormat::rawWithLogs:
         return makeSimpleLogger(true);
     case LogFormat::internalJSON:
-        return makeJSONLogger(STDERR_FILENO);
+        return makeJSONLogger(getStandardError());
     case LogFormat::bar:
         return makeProgressBar();
     case LogFormat::barWithLogs: {
