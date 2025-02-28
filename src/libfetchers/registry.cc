e3300d4cde0455305410a270b00b819c78cd1394
Eelco Dolstra <edolstra@gmail.com>
2025-02-28 14:39:14 +0100

    nix flake pin: Mark the resulting registry entry as exact
    Since the resulting target flakeref contains attributes like
    "lastModified" or "narHash", it's not suitable for overriding. This
    led to errors like
    
      $ nix build nixpkgs/24.05#hello
      error: 'lastModified' attribute mismatch in input 'github:NixOS/nixpkgs/63dacb46bf939521bdc93981b4cbb7ecb58427a0', expected 1728538411, got 1717179513
    
    after doing `nix registry pin nixpkgs`.

--- src/libfetchers/registry.cc
+++ src/libfetchers/registry.cc
@@ -84,13 +84,15 @@ void Registry::write(const Path & path)
 void Registry::add(
     const Input & from,
     const Input & to,
-    const Attrs & extraAttrs)
+    const Attrs & extraAttrs,
+    bool exact)
 {
     entries.emplace_back(
         Entry {
             .from = from,
             .to = to,
-            .extraAttrs = extraAttrs
+            .extraAttrs = extraAttrs,
+            .exact = exact
         });
 }
 
@@ -144,7 +146,7 @@ void overrideRegistry(
     const Input & to,
     const Attrs & extraAttrs)
 {
-    getFlagRegistry(*from.settings)->add(from, to, extraAttrs);
+    getFlagRegistry(*from.settings)->add(from, to, extraAttrs, false);
 }
 
 static std::shared_ptr<Registry> getGlobalRegistry(const Settings & settings, ref<Store> store)
