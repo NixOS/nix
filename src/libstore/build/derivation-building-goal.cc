8a9bde362a77caea525d5a3b65c736ddd7cd1c19
John Ericson <John.Ericson@Obsidian.Systems>
2025-03-03 10:33:54 -0500

    Get rid of `addWantedOutputs`
    Due this by introducing `DerivationCreationAndRealisationGoal`.
    
    `DerivationGoal` now only tracks a single output, and is back to
    tracking a plain store path `drvPath`, not a deriving path one. Its
    `addWantedOutputs` method is gone. These changes will allow subsequent
    PRs to simplify it greatly.
    
    Because the purpose of each goal is back to being immutable, we can also
    once again make `Goal::buildResult` a public field, and get rid of the
    `getBuildResult` method. This simplifies things also.
    
    `DerivationCreationAndRealisationGoal` is a cheap "trampoline" goal. It
    takes immutable sets of wanted outputs, and just kicks of
    `DerivationGoal`s for them. Since now "actual work" is done in these
    goals, it is not wasteful to have separate ones for separate sets of
    outputs, even if those outputs (and the derivations they are from)
    overlap.
    
    This separation of concerns will make it possible for future work on
    issues like #11928, and to continue the path of having more goal types,
    but each goal type does fewer things (issue #12628).
    
    This commit in some sense reverts
    f4f28cdd0e01a9bfb0901e8a53ead719265fa9b8, but that one kept around
    `addWantedOutputs`. I am quite sure it was having two layers of goals
    with `addWantedOutputs` that caused the issues --- restarting logic like
    `addWantedOutputs` has is very tempermental! In this version of the
    change, we have *zero* layers of `addWantedOutputs` --- no goal type
    needs it, or otherwise has a mutable objective --- and so I think this
    change is safe.

--- src/libstore/build/derivation-building-goal.cc
+++ src/libstore/build/derivation-building-goal.cc
@@ -1,5 +1,5 @@
 #include "nix/store/build/derivation-building-goal.hh"
-#include "nix/store/build/derivation-goal.hh"
+#include "nix/store/build/derivation-creation-and-realisation-goal.hh"
 #ifndef _WIN32 // TODO enable build hook on Windows
 #  include "nix/store/build/hook-instance.hh"
 #  include "nix/store/build/derivation-builder.hh"
@@ -264,7 +264,7 @@ Goal::Co DerivationBuildingGoal::gaveUpOnSubstitution()
                     auto mEntry = get(inputGoals, drvPath);
                     if (!mEntry) return std::nullopt;
 
-                    auto buildResult = (*mEntry)->getBuildResult(DerivedPath::Built{drvPath, OutputsSpec::Names{outputName}});
+                    auto & buildResult = (*mEntry)->buildResult;
                     if (!buildResult.success()) return std::nullopt;
 
                     auto i = get(buildResult.builtOutputs, outputName);
@@ -295,8 +295,8 @@ Goal::Co DerivationBuildingGoal::gaveUpOnSubstitution()
                    });
 
             // FIXME wanted outputs
-            auto resolvedDrvGoal = worker.makeDerivationGoal(
-                makeConstantStorePathRef(pathResolved), OutputsSpec::All{}, buildMode);
+            auto resolvedDrvGoal = worker.makeDerivationCreationAndRealisationGoal(
+                pathResolved, OutputsSpec::All{}, drvResolved, buildMode);
             {
                 Goals waitees{resolvedDrvGoal};
                 co_await await(std::move(waitees));
@@ -304,20 +304,16 @@ Goal::Co DerivationBuildingGoal::gaveUpOnSubstitution()
 
             trace("resolved derivation finished");
 
-            auto resolvedDrv = *resolvedDrvGoal->drv;
-            auto resolvedResult = resolvedDrvGoal->getBuildResult(DerivedPath::Built{
-                .drvPath = makeConstantStorePathRef(pathResolved),
-                .outputs = OutputsSpec::All{},
-            });
+            auto resolvedResult = resolvedDrvGoal->buildResult;
 
             SingleDrvOutputs builtOutputs;
 
             if (resolvedResult.success()) {
-                auto resolvedHashes = staticOutputHashes(worker.store, resolvedDrv);
+                auto resolvedHashes = staticOutputHashes(worker.store, drvResolved);
 
                 StorePathSet outputPaths;
 
-                for (auto & outputName : resolvedDrv.outputNames()) {
+                for (auto & outputName : drvResolved.outputNames()) {
                     auto initialOutput = get(initialOutputs, outputName);
                     auto resolvedHash = get(resolvedHashes, outputName);
                     if ((!initialOutput) || (!resolvedHash))
@@ -338,7 +334,7 @@ Goal::Co DerivationBuildingGoal::gaveUpOnSubstitution()
 
                       throw Error(
                           "derivation '%s' doesn't have expected output '%s' (derivation-goal.cc/realisation)",
-                          resolvedDrvGoal->drvReq->to_string(worker.store), outputName);
+                          worker.store.printStorePath(pathResolved), outputName);
                     }();
 
                     if (!drv->type().isImpure()) {
