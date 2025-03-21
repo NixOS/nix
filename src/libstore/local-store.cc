0178975340a5739cb09e348b91fa7557df46ac8b
Jörg Thalheim <joerg@thalheim.io>
2025-03-21 15:43:58 +0100

    libstore/local-store: fix linting warning about unused variable
    (cherry picked from commit 05082ea1c5b6cb1cc1a6bfc50f9d9c81052cbfe8)

--- src/libstore/local-store.cc
+++ src/libstore/local-store.cc
@@ -248,7 +248,7 @@ LocalStore::LocalStore(
     else if (curSchema == 0) { /* new store */
         curSchema = nixSchemaVersion;
         openDB(*state, true);
-        writeFile(schemaPath, fmt("%1%", nixSchemaVersion), 0666, true);
+        writeFile(schemaPath, fmt("%1%", curSchema), 0666, true);
     }
 
     else if (curSchema < nixSchemaVersion) {
