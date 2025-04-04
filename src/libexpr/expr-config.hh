2b51250534899329906273ae80463ccfe8455d08
Robert Hensing <robert@roberthensing.nl>
2025-04-05 00:46:06 +0200

    Hack together a fix for the public headers
    Please fix this.

---
+++ src/libexpr/expr-config.hh
@@ -0,0 +1,3 @@
+// TODO: Remove this damn file while keeping public config headers working
+#error \
+    "This file is a placeholder. It only exists so that meson accepts the symbolic link include/nix/expr/config.hh to this file, but we expect meson to overwrite it with the real file. Apparently that did not happen. I deeply apologize for this mess."
