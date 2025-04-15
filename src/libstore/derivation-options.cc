32409dd7d750576153657beb075bb303840c0c3a
John Ericson <John.Ericson@Obsidian.Systems>
2025-04-15 11:54:11 -0400

    Remove stray assignment side affect in lambda
    This was almost a bug! It wasn't simply because another assignment would
    clobber it later.

--- src/libstore/derivation-options.cc
+++ src/libstore/derivation-options.cc
@@ -70,7 +70,6 @@ DerivationOptions DerivationOptions::fromParsedDerivation(const ParsedDerivation
                                         throw Error("attribute '%s' must be a list of strings", name);
                                     res.insert(j->get<std::string>());
                                 }
-                                checks.disallowedRequisites = res;
                                 return res;
                             }
                             return {};
