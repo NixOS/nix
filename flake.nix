d9839cc92283aa1529d2aa39f0a49f136dd6354d
Luc Perkins <lucperkins@gmail.com>
2025-05-05 14:15:20 -0300

    Remove flake-compat input

--- flake.nix
+++ flake.nix
@@ -5,10 +5,6 @@
 
   inputs.nixpkgs-regression.url = "github:NixOS/nixpkgs/215d4d0fd80ca5163643b03a33fde804a29cc1e2";
   inputs.nixpkgs-23-11.url = "github:NixOS/nixpkgs/a62e6edd6d5e1fa0329b8653c801147986f8d446";
-  inputs.flake-compat = {
-    url = "github:edolstra/flake-compat";
-    flake = false;
-  };
 
   # dev tooling
   inputs.flake-parts.url = "github:hercules-ci/flake-parts";
@@ -18,7 +14,6 @@
   inputs.git-hooks-nix.inputs.nixpkgs.follows = "nixpkgs";
   inputs.git-hooks-nix.inputs.nixpkgs-stable.follows = "nixpkgs";
   # work around 7730 and https://github.com/NixOS/nix/issues/7807
-  inputs.git-hooks-nix.inputs.flake-compat.follows = "";
   inputs.git-hooks-nix.inputs.gitignore.follows = "";
 
   outputs =
