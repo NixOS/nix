2bc6c3395b11abb2697efbfd5c725595ca71a364
Eelco Dolstra <edolstra@gmail.com>
2025-05-16 18:20:16 +0200

    Disable cross builds
    Mostly because they're causing an evaluation error in `nix flake
    check` on macOS, and we don't need them right now for Determinate Nix.

--- flake.nix
+++ flake.nix
@@ -42,12 +42,12 @@
       systems = linuxSystems ++ darwinSystems;
 
       crossSystems = [
-        "armv6l-unknown-linux-gnueabihf"
-        "armv7l-unknown-linux-gnueabihf"
-        "riscv64-unknown-linux-gnu"
+        #"armv6l-unknown-linux-gnueabihf"
+        #"armv7l-unknown-linux-gnueabihf"
+        #"riscv64-unknown-linux-gnu"
         # Disabled because of https://github.com/NixOS/nixpkgs/issues/344423
         # "x86_64-unknown-netbsd"
-        "x86_64-unknown-freebsd"
+        #"x86_64-unknown-freebsd"
       ];
 
       stdenvs = [
