e920e287347481f8bcfee3d126d0193678b2c619
Sergei Zimmerman <sergei@zimmerman.foo>
2025-07-20 21:16:20 +0300

    libutil: Speed up builds by using precompiled headers

---
+++ src/libutil/pch/precompiled-headers.hh
@@ -0,0 +1,7 @@
+#include "nix/util/util.hh"
+#include "nix/util/file-system.hh"
+#include "nix/util/serialise.hh"
+#include "nix/util/signals.hh"
+#include "nix/util/source-accessor.hh"
+
+#include <nlohmann/json.hpp>
