ddd30349094aab896fcddfa9609ba0d8f862d381
Jörg Thalheim <joerg@thalheim.io>
2025-08-07 22:42:24 +0200

    single-user-install: fix cp flags on freebsd

--- scripts/install-nix-from-tarball.sh
+++ scripts/install-nix-from-tarball.sh
@@ -169,7 +169,7 @@ for i in $(cd "$self/store" >/dev/null && echo ./*); do
         rm -rf "$i_tmp"
     fi
     if ! [ -e "$dest/store/$i" ]; then
-        if [ "$OS" = "Darwin" ]; then
+        if [ "$OS" = "Darwin" ] || [ "$OS" = "FreeBSD" ]; then
             cp -RPp "$self/store/$i" "$i_tmp"
         else
             cp -RP --preserve=ownership,timestamps "$self/store/$i" "$i_tmp"
