8be1cc6e96a95a634c73caf2bf545373c992b128
Sergei Zimmerman <sergei@zimmerman.foo>
2025-08-17 01:08:31 +0300

    libstore-tests: Improve compile times with PCH
    (Before)
    
    **** Time summary:
    Compilation (25 times):
      Parsing (frontend):          145.5 s
      Codegen & opts (backend):     65.9 s
    
    (After)
    
    **** Time summary:
    Compilation (26 times):
      Parsing (frontend):           39.9 s
      Codegen & opts (backend):     55.1 s

---
+++ src/libstore-tests/pch/precompiled-headers.hh
@@ -0,0 +1,9 @@
+#include "nix/store/store-api.hh"
+#include "nix/store/tests/libstore.hh"
+#include "nix/util/util.hh"
+
+#include <gtest/gtest.h>
+#include <nlohmann/json.hpp>
+#include <rapidcheck/gtest.h>
+
+#include <regex>
