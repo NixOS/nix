62018b3a7fdb39a8b8bd01489f01dd32216f55f8
Sergei Zimmerman <sergei@zimmerman.foo>
2025-08-18 23:24:07 +0300

    libstore: Fix formatting
    Wasn't caught by CI because the PR wasn't fresh enough
    and didn't have formatting checks enabled.

--- src/libstore/local-store.cc
+++ src/libstore/local-store.cc
@@ -232,7 +232,8 @@ LocalStore::LocalStore(ref<const Config> config)
             globalLock = openLockFile(globalLockPath.c_str(), true);
         } catch (SysError & e) {
             if (e.errNo == EACCES || e.errNo == EPERM) {
-                e.addTrace({},
+                e.addTrace(
+                    {},
                     "This command may have been run as non-root in a single-user Nix installation,\n"
                     "or the Nix daemon may have crashed.");
             }
