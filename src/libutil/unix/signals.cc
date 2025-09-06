a44dcbff13b7c70aaefd9e99517b30c0546f36d9
Eelco Dolstra <edolstra@gmail.com>
2025-09-06 16:32:19 +0200

    Remove unused function setChildSignalMask()

--- src/libutil/unix/signals.cc
+++ src/libutil/unix/signals.cc
@@ -99,26 +99,6 @@ void unix::triggerInterrupt()
 static sigset_t savedSignalMask;
 static bool savedSignalMaskIsSet = false;
 
-void unix::setChildSignalMask(sigset_t * sigs)
-{
-    assert(sigs); // C style function, but think of sigs as a reference
-
-#if (defined(_POSIX_C_SOURCE) && _POSIX_C_SOURCE >= 1) || (defined(_XOPEN_SOURCE) && _XOPEN_SOURCE) \
-    || (defined(_POSIX_SOURCE) && _POSIX_SOURCE)
-    sigemptyset(&savedSignalMask);
-    // There's no "assign" or "copy" function, so we rely on (math) idempotence
-    // of the or operator: a or a = a.
-    sigorset(&savedSignalMask, sigs, sigs);
-#else
-    // Without sigorset, our best bet is to assume that sigset_t is a type that
-    // can be assigned directly, such as is the case for a sigset_t defined as
-    // an integer type.
-    savedSignalMask = *sigs;
-#endif
-
-    savedSignalMaskIsSet = true;
-}
-
 void unix::saveSignalMask()
 {
     if (sigprocmask(SIG_BLOCK, nullptr, &savedSignalMask))
