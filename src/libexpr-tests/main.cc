2d1b412e5b34aa17b6012a484621e8a6e5e10679
Robert Hensing <robert@roberthensing.nl>
2025-09-19 23:35:31 +0200

    libexpr-tests: Enable when test setup for building succeeds
    Accidentally disabled by 9bc218ca3fc98889719684abba73b5d8a168cf3c

--- src/libexpr-tests/main.cc
+++ src/libexpr-tests/main.cc
@@ -1,14 +1,18 @@
 #include <gtest/gtest.h>
 
 #include "nix/store/tests/test-main.hh"
+#include "nix/util/config-global.hh"
 
 using namespace nix;
 
 int main(int argc, char ** argv)
 {
     auto res = testMainForBuidingPre(argc, argv);
-    if (!res)
+    if (res)
         return res;
+
+    // For pipe operator tests in trivial.cc
+    experimentalFeatureSettings.set("experimental-features", "pipe-operators");
 
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
