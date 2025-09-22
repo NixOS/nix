43ec36cddf6c1e3cca38cd2eb3710c242b0054f2
Robert Hensing <robert@roberthensing.nl>
2025-09-22 19:21:06 +0200

    pre-commit: Remove exclude that passes

--- maintainers/flake-module.nix
+++ maintainers/flake-module.nix
@@ -106,7 +106,6 @@
             enable = true;
             excludes = [
               # We haven't linted these files yet
-              ''^scripts/install-darwin-multi-user\.sh$''
               ''^scripts/install-multi-user\.sh$''
               ''^scripts/install-systemd-multi-user\.sh$''
               ''^src/nix/get-env\.sh$''
