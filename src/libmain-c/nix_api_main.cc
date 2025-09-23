cf595b81d53a4fdeb30694a271bf1cfe1bd55c34
Tristan Ross <tristan.ross@determinate.systems>
2025-09-23 12:04:06 -0700

    libmain-c: add nix_set_log_format function

--- src/libmain-c/nix_api_main.cc
+++ src/libmain-c/nix_api_main.cc
@@ -4,6 +4,7 @@
 #include "nix_api_util_internal.h"
 
 #include "nix/main/plugin.hh"
+#include "nix/main/loggers.hh"
 
 extern "C" {
 
@@ -13,6 +14,18 @@ nix_err nix_init_plugins(nix_c_context * context)
         context->last_err_code = NIX_OK;
     try {
         nix::initPlugins();
+    }
+    NIXC_CATCH_ERRS
+}
+
+nix_err nix_set_log_format(nix_c_context * context, const char * format)
+{
+    if (context)
+        context->last_err_code = NIX_OK;
+    if (format == nullptr)
+        return nix_set_err_msg(context, NIX_ERR_UNKNOWN, "Log format is null");
+    try {
+        nix::setLogFormat(format);
     }
     NIXC_CATCH_ERRS
 }
