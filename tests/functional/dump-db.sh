6e2c11e296a6b52c33c8d276796c568460889ef8
Farid Zakaria <farid.m.zakaria@gmail.com>
2025-09-24 19:15:53 -0700

    shellcheck fix functional/dump-db.sh
    Add back the path variable

--- tests/functional/dump-db.sh
+++ tests/functional/dump-db.sh
@@ -8,19 +8,18 @@ needLocalStore "--dump-db requires a local store"
 
 clearStore
 
-path=$(nix-build dependencies.nix -o $TEST_ROOT/result)
+nix-build dependencies.nix -o "$TEST_ROOT"/result
+deps="$(nix-store -qR "$TEST_ROOT"/result)"
 
-deps="$(nix-store -qR $TEST_ROOT/result)"
+nix-store --dump-db > "$TEST_ROOT"/dump
 
-nix-store --dump-db > $TEST_ROOT/dump
+rm -rf "$NIX_STATE_DIR"/db
 
-rm -rf $NIX_STATE_DIR/db
+nix-store --load-db < "$TEST_ROOT"/dump
 
-nix-store --load-db < $TEST_ROOT/dump
-
-deps2="$(nix-store -qR $TEST_ROOT/result)"
+deps2="$(nix-store -qR "$TEST_ROOT"/result)"
 
 [ "$deps" = "$deps2" ];
 
-nix-store --dump-db > $TEST_ROOT/dump2
-cmp $TEST_ROOT/dump $TEST_ROOT/dump2
+nix-store --dump-db > "$TEST_ROOT"/dump2
+cmp "$TEST_ROOT"/dump "$TEST_ROOT"/dump2
