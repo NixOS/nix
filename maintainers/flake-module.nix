c09cf33a3ac25291a4e4c095ee3e898f57187445
Farid Zakaria <farid.m.zakaria@gmail.com>
2025-09-29 10:04:14 -0700

    shellcheck fix: tests/functional/output-normalization.sh

--- maintainers/flake-module.nix
+++ maintainers/flake-module.nix
@@ -106,7 +106,6 @@
             enable = true;
             excludes = [
               # We haven't linted these files yet
-              ''^tests/functional/output-normalization\.sh$''
               ''^tests/functional/parallel\.builder\.sh$''
               ''^tests/functional/parallel\.sh$''
               ''^tests/functional/pass-as-file\.sh$''
