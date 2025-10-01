bcd8311ec6b9893697e42eb44f3f205a121673ed
Farid Zakaria <farid.m.zakaria@gmail.com>
2025-09-30 19:55:03 -0700

    shellcheck fix: tests/functional/post-hook.sh

--- maintainers/flake-module.nix
+++ maintainers/flake-module.nix
@@ -106,7 +106,6 @@
             enable = true;
             excludes = [
               # We haven't linted these files yet
-              ''^tests/functional/post-hook\.sh$''
               ''^tests/functional/pure-eval\.sh$''
               ''^tests/functional/push-to-store-old\.sh$''
               ''^tests/functional/push-to-store\.sh$''
