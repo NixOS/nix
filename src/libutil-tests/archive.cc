0619351326bb7b7aa2a05d7e97a71ea61f8a7bff
Sergei Zimmerman <sergei@zimmerman.foo>
2025-10-08 01:59:04 +0300

    tests: Move invalid nar tests from tests/functional to libutil-tests
    Since 242f3625675cc06069edfd0936ad6f42acb068a8 we have better infrastructure
    for this kind of tests.

--- src/libutil-tests/archive.cc
+++ src/libutil-tests/archive.cc
@@ -42,6 +42,20 @@ INSTANTIATE_TEST_SUITE_P(
     NarTest,
     InvalidNarTest,
     ::testing::Values(
-        std::pair{"invalid-tag-instead-of-contents", "bad archive: expected tag 'contents', got 'AAAAAAAA'"}));
+        std::pair{"invalid-tag-instead-of-contents", "bad archive: expected tag 'contents', got 'AAAAAAAA'"},
+        // Unpacking a NAR with a NUL character in a file name should fail.
+        std::pair{"nul-character", "bad archive: NAR contains invalid file name 'f"},
+        // Likewise for a '.' filename.
+        std::pair{"dot", "bad archive: NAR contains invalid file name '.'"},
+        // Likewise for a '..' filename.
+        std::pair{"dotdot", "bad archive: NAR contains invalid file name '..'"},
+        // Likewise for a filename containing a slash.
+        std::pair{"slash", "bad archive: NAR contains invalid file name 'x/y'"},
+        // Likewise for an empty filename.
+        std::pair{"empty", "bad archive: NAR contains invalid file name ''"},
+        // Test that the 'executable' field cannot come before the 'contents' field.
+        std::pair{"executable-after-contents", "bad archive: expected tag ')', got 'executable'"},
+        // Test that the 'name' field cannot come before the 'node' field in a directory entry.
+        std::pair{"name-after-node", "bad archive: expected tag 'name'"}));
 
 } // namespace nix
