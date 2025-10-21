34de68d2604f12ab625b1207fef949db2bc284da
John Ericson <John.Ericson@Obsidian.Systems>
2025-10-21 11:37:14 -0400

    Optimize `DummyStore::isValidPathUncached`
    See the API docs for the rationale of why this is needed.

--- src/libstore/dummy-store.cc
+++ src/libstore/dummy-store.cc
@@ -169,6 +169,15 @@ struct DummyStoreImpl : DummyStore
     }
 
     /**
+     * Do this to avoid `queryPathInfoUncached` computing `PathInfo`
+     * that we don't need just to return a `bool`.
+     */
+    bool isValidPathUncached(const StorePath & path) override
+    {
+        return path.isDerivation() ? derivations.contains(path) : Store::isValidPathUncached(path);
+    }
+
+    /**
      * The dummy store is incapable of *not* trusting! :)
      */
     virtual std::optional<TrustedFlag> isTrustedClient() override
