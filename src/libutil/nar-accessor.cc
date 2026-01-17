NixOS/nix

commit b9b6defca693e6413668647d216e3aa3c90a7465

nix nar {ls,cat}: Optimize
The whole NarAccessor -> listing -> lazy NarAccessor is very weird. Source
can now be seek-ed over when supported, so we can support it pretty easily.
Alternatively we could also make it single-pass very easily with a custom
FileSystemObjectSink. It will get removed in a follow-up commit anyway.

src/libutil/nar-accessor.cc
@@ -111,6 +111,7 @@ struct NarAccessor : public SourceAccessor
                 path,
                 NarMember{.stat = {.type = Type::tRegular, .fileSize = 0, .isExecutable = false, .narOffset = 0}});
             NarMemberConstructor nmc{nm, pos};
+            nmc.skipContents = true; /* Don't care about contents. */
             func(nmc);
         }
 
@@ -136,6 +137,13 @@ struct NarAccessor : public SourceAccessor
     }
 
     NarAccessor(Source & source)
+    {
+        NarIndexer indexer(*this, source);
+        parseDump(indexer, indexer);
+    }
+
+    NarAccessor(Source & source, GetNarBytes getNarBytes)
+        : getNarBytes(std::move(getNarBytes))
     {
         NarIndexer indexer(*this, source);
         parseDump(indexer, indexer);
@@ -249,24 +257,35 @@ ref<SourceAccessor> makeLazyNarAccessor(const nlohmann::json & listing, GetNarBy
     return make_ref<NarAccessor>(listing, getNarBytes);
 }
 
+ref<SourceAccessor> makeLazyNarAccessor(Source & source, GetNarBytes getNarBytes)
+{
+    return make_ref<NarAccessor>(source, getNarBytes);
+}
+
 GetNarBytes seekableGetNarBytes(const Path & path)
 {
-    return [path](uint64_t offset, uint64_t length) {
-        AutoCloseFD fd = toDescriptor(open(
-            path.c_str(),
-            O_RDONLY
-#ifndef _WIN32
-                | O_CLOEXEC
+    AutoCloseFD fd = toDescriptor(open(
+        path.c_str(),
+        O_RDONLY
+#ifdef O_CLOEXEC
+            | O_CLOEXEC
 #endif
-            ));
-        if (!fd)
-            throw SysError("opening NAR cache file '%s'", path);
+        ));
+    if (!fd)
+        throw SysError("opening NAR cache file '%s'", path);
 
-        if (lseek(fromDescriptorReadOnly(fd.get()), offset, SEEK_SET) != (off_t) offset)
-            throw SysError("seeking in '%s'", path);
+    return [inner = seekableGetNarBytes(fd.get()), fd = make_ref<AutoCloseFD>(std::move(fd))](
+               uint64_t offset, uint64_t length) { return inner(offset, length); };
+}
+
+GetNarBytes seekableGetNarBytes(Descriptor fd)
+{
+    return [fd](uint64_t offset, uint64_t length) {
+        if (::lseek(fromDescriptorReadOnly(fd), offset, SEEK_SET) == -1)
+            throw SysError("seeking in file");
 
         std::string buf(length, 0);
-        readFull(fd.get(), buf.data(), length);
+        readFull(fd, buf.data(), length);
 
         return buf;
     };

src/libutil/include/nix/util/nar-accessor.hh
@@ -32,7 +32,15 @@ using GetNarBytes = std::function<std::string(uint64_t, uint64_t)>;
  */
 GetNarBytes seekableGetNarBytes(const Path & path);
 
+GetNarBytes seekableGetNarBytes(Descriptor fd);
+
 ref<SourceAccessor> makeLazyNarAccessor(const nlohmann::json & listing, GetNarBytes getNarBytes);
+
+/**
+ * Creates a NAR accessor from a given stream and a GetNarBytes getter.
+ * @param source Consumed eagerly. References to it are not persisted in the resulting SourceAccessor.
+ */
+ref<SourceAccessor> makeLazyNarAccessor(Source & source, GetNarBytes getNarBytes);
 
 struct NarListingRegularFile
 {

src/nix/cat.cc
@@ -79,9 +79,7 @@ struct CmdCatNar : StoreCommand, MixCat
         if (!fd)
             throw SysError("opening NAR file '%s'", narPath);
         auto source = FdSource{fd.get()};
-        auto narAccessor = makeNarAccessor(source);
-        nlohmann::json listing = listNarDeep(*narAccessor, CanonPath::root);
-        cat(makeLazyNarAccessor(listing, seekableGetNarBytes(narPath)), CanonPath{path});
+        cat(makeLazyNarAccessor(source, seekableGetNarBytes(fd.get())), CanonPath{path});
     }
 };
 

src/nix/ls.cc
@@ -154,9 +154,7 @@ struct CmdLsNar : Command, MixLs
         if (!fd)
             throw SysError("opening NAR file '%s'", narPath);
         auto source = FdSource{fd.get()};
-        auto narAccessor = makeNarAccessor(source);
-        nlohmann::json listing = listNarDeep(*narAccessor, CanonPath::root);
-        list(makeLazyNarAccessor(listing, seekableGetNarBytes(narPath)), CanonPath{path});
+        list(makeLazyNarAccessor(source, seekableGetNarBytes(fd.get())), CanonPath{path});
     }
 };
 

commit 65b2421cdd6c7e0cbda01b6cbd9d859710a67d07

nar-accessor: Fix thread safety of seekableGetNarBytes
Instead of mutating the file pointer we can instead safely do
preads. That makes the local-nar-info cache once again thread safe
without the overhead of reopening the file that we used to have prior
to b9b6defca693e6413668647d216e3aa3c90a7465 which broke the thread safety
by persisting the file descriptor.

src/libutil/nar-accessor.cc
@@ -1,6 +1,7 @@
 #include "nix/util/nar-accessor.hh"
 #include "nix/util/file-descriptor.hh"
 #include "nix/util/error.hh"
+#include "nix/util/signals.hh"
 
 namespace nix {
 
@@ -164,12 +165,35 @@ GetNarBytes seekableGetNarBytes(const std::filesystem::path & path)
 
 GetNarBytes seekableGetNarBytes(Descriptor fd)
 {
-    return [fd](uint64_t offset, uint64_t length) {
-        if (lseek(fd, offset, SEEK_SET) == -1)
-            throw SysError("seeking in file");
+    return [fd](uint64_t offset_, uint64_t left) {
+        std::string buf(left, 0);
 
-        std::string buf(length, 0);
-        readFull(fd, buf.data(), length);
+        off_t offset = offset_;
+        std::string::size_type pos = 0;
+
+        while (left) {
+            checkInterrupt();
+#ifdef _WIN32
+            OVERLAPPED ov = {};
+            ov.Offset = static_cast<DWORD>(offset);
+            ov.OffsetHigh = static_cast<DWORD>(offset >> 32);
+            DWORD n;
+            if (!ReadFile(fd, buf.data() + pos, static_cast<DWORD>(std::min(left, uint64_t{MAXDWORD})), &n, &ov))
+                throw nix::windows::WinError("reading %1% NAR bytes at offset %2%", left, offset);
+            if (n == 0)
+                throw EndOfFile("unexpected end-of-file");
+#else
+            ssize_t n = pread(fd, buf.data() + pos, left, offset);
+            if (n == 0)
+                throw EndOfFile("unexpected end-of-file");
+            if (n == -1 && errno != EINTR)
+                throw SysError("reading %1% NAR bytes at offset %2%", left, offset);
+#endif
+            assert(static_cast<uint64_t>(n) <= left);
+            pos += n;
+            offset += n;
+            left -= n;
+        }
 
         return buf;
     };