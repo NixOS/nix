0f17a1f655c719a15ce383b91fe9bf0910083748
Sergei Zimmerman <sergei@zimmerman.foo>
2026-01-18 20:30:18 +0300

    libutil-tests: Add unit tests for https binary cache stores with mTLS
    This addresses the concerns with network isolation that have been raised
    previously [1] by only running the tests by default in a network namespace.
    This way all networks tests are independent of each other and do not bind
    to ports in the host namespace.
    
    This is much neater than doing these sorts of tests in functional suite.
    
    [1]: https://github.com/NixOS/nix/pull/14266#issuecomment-3411261285

--- src/libstore-tests/main.cc
+++ src/libstore-tests/main.cc
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 
 #include "nix/store/tests/test-main.hh"
+#include "nix/store/tests/libstore-network.hh"
 
 using namespace nix;
 
@@ -10,6 +11,7 @@ int main(int argc, char ** argv)
     if (res)
         return res;
 
+    nix::testing::setupNetworkTests();
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
