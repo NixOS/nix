ed28ceb12fbd739713a0a39c1eb5f34ce694b495
Bernardo Meurer Costa <beme@anthropic.com>
2026-01-22 06:24:41 +0000

    fix(libstore/filetransfer): skip Accept-Encoding header for S3 SigV4 requests
    Some S3-compatible services (like GCS) modify the Accept-Encoding header
    in transit, which breaks AWS SigV4 signature verification since curl's
    implementation signs all headers including Accept-Encoding.
    
    Fixes: #15019
    (cherry picked from commit fcfa1dc8ab37a5a5ca987a52f964c2e6cb565028)

--- src/libstore/filetransfer.cc
+++ src/libstore/filetransfer.cc
@@ -131,7 +131,16 @@ struct curlFileTransfer : public FileTransfer
         {
             result.urls.push_back(request.uri.to_string());
 
-            requestHeaders = curl_slist_append(requestHeaders, "Accept-Encoding: zstd, br, gzip, deflate, bzip2, xz");
+            /* Don't set Accept-Encoding for S3 requests that use AWS SigV4 signing.
+               curl's SigV4 implementation signs all headers including Accept-Encoding,
+               but some S3-compatible services (like GCS) modify this header in transit,
+               causing signature verification to fail.
+               See https://github.com/NixOS/nix/issues/15019 */
+#if NIX_WITH_AWS_AUTH
+            if (!request.awsSigV4Provider)
+#endif
+                requestHeaders =
+                    curl_slist_append(requestHeaders, "Accept-Encoding: zstd, br, gzip, deflate, bzip2, xz");
             if (!request.expectedETag.empty())
                 requestHeaders = curl_slist_append(requestHeaders, ("If-None-Match: " + request.expectedETag).c_str());
             if (!request.mimeType.empty())
