1b5d4258ec76f383ba291ac7fd9b359471179edb
John Ericson <John.Ericson@Obsidian.Systems>
2026-01-26 11:54:40 -0500

    Move cgroup functionality inside `nix::linux` namespace
    This is what we do for other OS-specific functionality.

--- src/libstore/unix/build/linux-derivation-builder.cc
+++ src/libstore/unix/build/linux-derivation-builder.cc
@@ -26,6 +26,8 @@
 
 namespace nix {
 
+using namespace nix::linux;
+
 static void setupSeccomp(const LocalSettings & localSettings)
 {
     if (!localSettings.filterSyscalls)
--- src/nix/unix/daemon.cc
+++ src/nix/unix/daemon.cc
@@ -251,8 +251,8 @@ static void daemonLoop(ref<const StoreConfig> storeConfig, std::optional<Trusted
         experimentalFeatureSettings.require(Xp::Cgroups);
 
         //  This also sets the root cgroup to the current one.
-        auto rootCgroup = getRootCgroup();
-        auto cgroupFS = getCgroupFS();
+        auto rootCgroup = linux::getRootCgroup();
+        auto cgroupFS = linux::getCgroupFS();
         if (!cgroupFS)
             throw Error("cannot determine the cgroups file system");
         auto rootCgroupPath = *cgroupFS / rootCgroup.rel();
