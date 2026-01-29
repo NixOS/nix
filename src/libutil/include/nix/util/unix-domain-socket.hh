3351d5576860f229265b57c6aeaa9084e122c29d
John Ericson <John.Ericson@Obsidian.Systems>
2026-01-29 12:28:20 -0500

    Introduce `unix::sendMessageWithFds` and `unix::receiveMessageWithFds`
    Add functions for sending and receiving messages with file descriptors
    over Unix domain sockets using `SCM_RIGHTS`.
    
    Co-authored-by: Sergei Zimmerman <sergei@zimmerman.foo>

--- src/libutil/include/nix/util/unix-domain-socket.hh
+++ src/libutil/include/nix/util/unix-domain-socket.hh
@@ -5,9 +5,11 @@
 #include "nix/util/file-descriptor.hh"
 #include "nix/util/socket.hh"
 
-#include <unistd.h>
-
 #include <filesystem>
+#include <span>
+#include <vector>
+
+#include <unistd.h>
 
 namespace nix {
 
@@ -35,5 +37,61 @@ void connect(Socket fd, const std::filesystem::path & path);
  * Connect to a Unix domain socket.
  */
 AutoCloseFD connect(const std::filesystem::path & path);
+
+#ifndef _WIN32
+namespace unix {
+
+/**
+ * Send a message with file descriptors over a Unix domain socket using
+ * sendmsg with SCM_RIGHTS.
+ *
+ * On SOCK_STREAM sockets, `data` must be non-empty when `fds` is
+ * non-empty, because the kernel silently drops ancillary data on
+ * zero-length stream sends.
+ *
+ * @param sockfd The socket file descriptor to send the message on
+ * @param data The data to send
+ * @param fds A span of file descriptors to pass via SCM_RIGHTS
+ *
+ * @throws SysError on failure
+ */
+void sendMessageWithFds(Descriptor sockfd, std::span<const std::byte> data, std::span<const Descriptor> fds);
+
+/**
+ * Result of receiving a message with file descriptors.
+ */
+struct ReceivedMessage
+{
+    /**
+     * Number of bytes received into the data buffer
+     */
+    size_t bytesReceived;
+    /**
+     * The file descriptors received via SCM_RIGHTS, wrapped in
+     * AutoCloseFD for RAII.
+     */
+    std::vector<AutoCloseFD> fds;
+};
+
+/**
+ * Receive a message with file descriptors over a Unix domain socket
+ * using recvmsg with SCM_RIGHTS.
+ *
+ * All file descriptors associated with the message will be returned. This
+ * avoids unrecoverably dropping file descriptors with a messages. This is why
+ * a vector is returned, as opposed to the caller passing in a `std::span` with
+ * length of their choosing, as that may not be long enough.
+ *
+ * @param sockfd The socket file descriptor to receive the message on
+ * @param data Buffer to receive data into
+ *
+ * @return A ReceivedMessage containing the bytes received count and file descriptors
+ * @throws SysError on failure
+ * @throws EndOfFile if the connection is closed
+ */
+ReceivedMessage receiveMessageWithFds(Descriptor sockfd, std::span<std::byte> data);
+
+} // namespace unix
+#endif
 
 } // namespace nix
