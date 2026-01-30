b026649c6258c0a14272a0d94e1f2eac0773c14a
Artemis Tosini <artemis.tosini@obsidian.systems>
2026-01-30 10:27:16 -0500

    libstore: fix runtime gc roots on non-standard store paths
    Due to a typo in quoteRegexChars, finding runtime garbage collection roots
    was failing on paths that contained a dot, or any other regex chars that would
    have to be replaced.
    
    When fixing that error, also add tests to make sure gc continues to
    work.

--- tests/functional/gc-runtime.sh
+++ tests/functional/gc-runtime.sh
@@ -16,26 +16,38 @@ TODO_NixOS
 profiles="$NIX_STATE_DIR"/profiles
 rm -rf "$profiles"
 
-nix-env -p "$profiles/test" -f ./gc-runtime.nix -i gc-runtime
+nix-env -p "$profiles/test" -f ./gc-runtime.nix -i gc-runtime-{program,environ,open}
 
-outPath=$(nix-env -p "$profiles/test" -q --no-name --out-path gc-runtime)
-echo "$outPath"
+programPath=$(nix-env -p "$profiles/test" -q --no-name --out-path gc-runtime-program)
+environPath=$(nix-env -p "$profiles/test" -q --no-name --out-path gc-runtime-environ)
+openPath=$(nix-env -p "$profiles/test" -q --no-name --out-path gc-runtime-open)
 
 echo "backgrounding program..."
-"$profiles"/test/program &
+export environPath
+"$profiles"/test/program "$openPath"/open &
 sleep 2 # hack - wait for the program to get started
 child=$!
 echo PID=$child
 
-nix-env -p "$profiles/test" -e gc-runtime
+nix-env -p "$profiles/test" -e gc-runtime-{program,environ,open}
 nix-env -p "$profiles/test" --delete-generations old
 
 nix-store --gc
 
 kill -- -$child
 
-if ! test -e "$outPath"; then
+if ! test -e "$programPath"; then
     echo "running program was garbage collected!"
+    exit 1
+fi
+
+if ! test -e "$environPath"; then
+    echo "file in environment variable was garbage collected!"
+    exit 1
+fi
+
+if ! test -e "$openPath"; then
+    echo "opened file was garbage collected!"
     exit 1
 fi
 
