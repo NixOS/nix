d77c131df33d88c555593692c291f4198e074a82
Sergei Zimmerman <sergei@zimmerman.foo>
2026-02-05 02:01:50 +0300

    libutil: Add overflow check to alignUp
    Old code with size + (size % 8 ? 8 - (size % 8) : 0) also suffered from this.

--- src/libutil-tests/alignment.cc
+++ src/libutil-tests/alignment.cc
@@ -15,4 +15,32 @@ TEST(alignUp, notAPowerOf2)
     ASSERT_DEATH({ alignUp(1u, 42); }, "alignment must be a power of 2");
 }
 
+template<typename T>
+class alignUpOverflowTest : public ::testing::Test
+{};
+
+using UnsignedTypes = ::testing::Types<uint8_t, uint16_t, uint32_t, uint64_t>;
+TYPED_TEST_SUITE(alignUpOverflowTest, UnsignedTypes);
+
+TYPED_TEST(alignUpOverflowTest, lastSafeValue)
+{
+    constexpr auto max = std::numeric_limits<TypeParam>::max();
+    ASSERT_EQ(alignUp<TypeParam>(max - 15, 16), (max - 15) & ~TypeParam{15});
+    ASSERT_NO_THROW(alignUp<TypeParam>(max - 15, 16));
+}
+
+TYPED_TEST(alignUpOverflowTest, overflowThrows)
+{
+    constexpr auto max = std::numeric_limits<TypeParam>::max();
+    ASSERT_THROW(alignUp<TypeParam>(max - 14, 16), Error);
+    ASSERT_THROW(alignUp<TypeParam>(max, 16), Error);
+    ASSERT_THROW(alignUp<TypeParam>(max, 2), Error);
+}
+
+TYPED_TEST(alignUpOverflowTest, alignmentOneNeverOverflows)
+{
+    constexpr auto max = std::numeric_limits<TypeParam>::max();
+    ASSERT_EQ(alignUp<TypeParam>(max, 1), max);
+}
+
 } // namespace nix
