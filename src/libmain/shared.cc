940e8db2842c8e2beadbccdead9ed14e49b8e539
Lily Foster <lily.foster@shopify.com>
2026-02-18 13:11:09 -0500

    libmain: keep determinate nix in version string

--- src/libmain/shared.cc
+++ src/libmain/shared.cc
@@ -294,7 +294,7 @@ void parseCmdLine(
 
 std::string version()
 {
-    return fmt("(Tecnix %s) %s", determinateNixVersion, nixVersion);
+    return fmt("(Tecnix, based on Determinate Nix %s) %s", determinateNixVersion, nixVersion);
 }
 
 void printVersion(const std::string & programName)
