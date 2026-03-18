NixOS/nix

commit 71a97bce2706edc175c779f55d3138af8b0bf158

libutil,libstore: Get rid of CompressedSource
This was already pretty redundant. While we are at it, also make
StringSource also handle taking a std::string && by storing it inline.

src/libutil/include/nix/util/serialise.hh
@@ -4,7 +4,6 @@
 #include <memory>
 #include <type_traits>
 
-#include "nix/util/compression-algo.hh"
 #include "nix/util/fun.hh"
 #include "nix/util/types.hh"
 #include "nix/util/util.hh"
@@ -264,16 +263,20 @@ struct StringSink : Sink
  */
 struct StringSource : RestartableSource
 {
+    std::optional<std::string> sOwned;
     std::string_view s;
     size_t pos;
 
-    // NOTE: Prevent unintentional dangling views when an implicit conversion
-    // from std::string -> std::string_view occurs when the string is passed
-    // by rvalue.
-    StringSource(std::string &&) = delete;
+    StringSource(std::string && s)
+        : sOwned(std::move(s))
+        , s(*sOwned)
+        , pos(0)
+    {
+    }
 
     StringSource(std::string_view s)
-        : s(s)
+        : sOwned(std::nullopt)
+        , s(s)
         , pos(0)
     {
     }
@@ -290,45 +293,6 @@ struct StringSource : RestartableSource
     void restart() override
     {
         pos = 0;
-    }
-};
-
-/**
- * Compresses a RestartableSource using the specified compression method.
- *
- * @note currently this buffers the entire compressed data stream in memory. In the future it may instead compress data
- * on demand, lazily pulling from the original `RestartableSource`. In that case, the `size()` method would go away
- * because we would not in fact know the compressed size in advance.
- */
-struct CompressedSource : RestartableSource
-{
-private:
-    std::string compressedData;
-    CompressionAlgo compressionMethod;
-    StringSource stringSource;
-
-public:
-    /**
-     * Compress a RestartableSource using the specified compression method.
-     *
-     * @param source The source data to compress
-     * @param compressionMethod The compression method to use
-     */
-    CompressedSource(RestartableSource & source, CompressionAlgo compressionMethod);
-
-    size_t read(char * data, size_t len) override
-    {
-        return stringSource.read(data, len);
-    }
-
-    void restart() override
-    {
-        stringSource.restart();
-    }
-
-    uint64_t size() const
-    {
-        return compressedData.size();
     }
 };
 

src/libstore/http-binary-cache-store.cc
@@ -7,6 +7,7 @@
 #include "nix/util/closure.hh"
 #include "nix/store/store-registration.hh"
 #include "nix/store/globals.hh"
+#include "nix/util/compression.hh"
 #include "nix/util/topo-sort.hh"
 
 namespace nix {
@@ -202,11 +203,11 @@ void HttpBinaryCacheStore::upsertFile(
 {
     try {
         if (auto compressionMethod = getCompressionMethod(path)) {
-            CompressedSource compressed(source, *compressionMethod);
+            StringSource compressed(compress(*compressionMethod, source));
             /* TODO: Validate that this is a valid content encoding. We probably shouldn't set non-standard values here.
              */
             Headers headers = {{"Content-Encoding", showCompressionAlgo(*compressionMethod)}};
-            upload(path, compressed, compressed.size(), mimeType, std::move(headers));
+            upload(path, compressed, compressed.s.size(), mimeType, std::move(headers));
         } else {
             upload(path, source, sizeHint, mimeType, std::nullopt);
         }

src/libstore/s3-binary-cache-store.cc
@@ -1,6 +1,7 @@
 #include "nix/store/s3-binary-cache-store.hh"
 #include "nix/store/http-binary-cache-store.hh"
 #include "nix/store/store-registration.hh"
+#include "nix/util/compression.hh"
 #include "nix/util/error.hh"
 #include "nix/util/logging.hh"
 #include "nix/util/serialise.hh"
@@ -161,11 +162,11 @@ void S3BinaryCacheStore::upsertFile(
 
     try {
         if (auto compressionMethod = getCompressionMethod(path)) {
-            CompressedSource compressed(source, *compressionMethod);
+            StringSource compressed(compress(*compressionMethod, source));
             /* TODO: Validate that this is a valid content encoding. We probably shouldn't set non-standard values here.
              */
             Headers headers = {{"Content-Encoding", showCompressionAlgo(*compressionMethod)}};
-            doUpload(compressed, compressed.size(), std::move(headers));
+            doUpload(compressed, compressed.s.size(), std::move(headers));
         } else {
             doUpload(source, sizeHint, std::nullopt);
         }

src/libutil/compression.cc
@@ -336,9 +336,15 @@ ref<CompressionSink> makeCompressionSink(CompressionAlgo method, Sink & nextSink
 
 std::string compress(CompressionAlgo method, std::string_view in, const bool parallel, int level)
 {
+    StringSource source(in);
+    return compress(method, source, parallel, level);
+}
+
+std::string compress(CompressionAlgo method, Source & in, const bool parallel, int level)
+{
     StringSink ssink;
     auto sink = makeCompressionSink(method, ssink, parallel, level);
-    (*sink)(in);
+    in.drainInto(*sink);
     sink->finish();
     return std::move(ssink.s);
 }

src/libutil/include/nix/util/compression.hh
@@ -23,6 +23,8 @@ std::unique_ptr<FinishSink> makeDecompressionSink(const std::string & method, Si
 
 std::string compress(CompressionAlgo method, std::string_view in, const bool parallel = false, int level = -1);
 
+std::string compress(CompressionAlgo method, Source & in, const bool parallel = false, int level = -1);
+
 ref<CompressionSink>
 makeCompressionSink(CompressionAlgo method, Sink & nextSink, const bool parallel = false, int level = -1);
 

src/libutil/serialise.cc
@@ -1,6 +1,5 @@
 #include "nix/util/serialise.hh"
 #include "nix/util/file-descriptor.hh"
-#include "nix/util/compression.hh"
 #include "nix/util/signals.hh"
 #include "nix/util/socket.hh"
 #include "nix/util/util.hh"
@@ -304,19 +303,6 @@ void StringSource::skip(size_t len)
         throw EndOfFile("end of string reached");
     }
     pos += len;
-}
-
-CompressedSource::CompressedSource(RestartableSource & source, CompressionAlgo compressionMethod)
-    : compressedData([&]() {
-        StringSink sink;
-        auto compressionSink = makeCompressionSink(compressionMethod, sink);
-        source.drainInto(*compressionSink);
-        compressionSink->finish();
-        return std::move(sink.s);
-    }())
-    , compressionMethod(compressionMethod)
-    , stringSource(compressedData)
-{
 }
 
 std::unique_ptr<FinishSink> sourceToSink(fun<void(Source &)> reader)

commit a9ff4de24571d1cdf203d689de8c6b92b5fc33e6

libutil: Fixup owned case in StringSource to never invalidate pointers
Fixes a potential bug I introduced in 71a97bce2706edc175c779f55d3138af8b0bf158.
Not yet an issue, because StringSource doesn't get copied or moved, but still
good to be completely correct about it.

std::string has a small buffer optimisation, which means that references
can be invalidated on copies and moves.

src/libutil/include/nix/util/serialise.hh
@@ -263,19 +263,21 @@ struct StringSink : Sink
  */
 struct StringSource : RestartableSource
 {
-    std::optional<std::string> sOwned;
+    /* Put behind a shared_ptr to make sure that copies and moves don't invalidate pointers
+       into a small string buffer. */
+    std::shared_ptr<std::string> sOwned;
     std::string_view s;
     size_t pos;
 
     StringSource(std::string && s)
-        : sOwned(std::move(s))
+        : sOwned(std::make_shared<std::string>(std::move(s)))
         , s(*sOwned)
         , pos(0)
     {
     }
 
     StringSource(std::string_view s)
-        : sOwned(std::nullopt)
+        : sOwned(nullptr)
         , s(s)
         , pos(0)
     {