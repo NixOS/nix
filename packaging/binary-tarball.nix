811d7a5c83c51fc0abe9cd556a03e407ab05bf4f
Sergei Zimmerman <sergei@zimmerman.foo>
2026-03-26 12:02:21 +0300

    packaging: Make binaryTarball more reproducible
    While debugging https://github.com/NixOS/nix/issues/15564 I noticed that
    our release tarball don't seem to be reproducible. This doesn't explain
    why hydra calculated the different hash of the tarball, which should only
    be built once (???).
    
    Regardless, I noticed that checking the build reproducibility against hydra
    built tarballs fails with e.g.
    
    nix build .\#hydraJobs.binaryTarball.x86_64-linux -L --keep-failed --rebuild
    
    Looking with diffoscope, it was the entry ordering issue. Let's nip this in the bud.
    
    (cherry picked from commit 4c41c5f4ad8135a21d9b49a92438408163f9a9ff)

--- packaging/binary-tarball.nix
+++ packaging/binary-tarball.nix
@@ -74,7 +74,8 @@ runCommand "nix-binary-tarball-${version}" env ''
   fn=$out/$dir.tar.xz
   mkdir -p $out/nix-support
   echo "file binary-dist $fn" >> $out/nix-support/hydra-build-products
-  tar cfJ $fn \
+  tar cf - \
+    --sort=name \
     --owner=0 --group=0 --mode=u+rw,uga+r \
     --mtime='1970-01-01' \
     --absolute-names \
@@ -90,5 +91,5 @@ runCommand "nix-binary-tarball-${version}" env ''
     $TMPDIR/install-freebsd-multi-user.sh \
     $TMPDIR/install-multi-user \
     $TMPDIR/reginfo \
-    $(cat ${installerClosureInfo}/store-paths)
+    $(cat ${installerClosureInfo}/store-paths) | xz --threads=1 > $fn
 ''
