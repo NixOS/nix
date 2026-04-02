e7bf1cdda30f2856fc8a0a81f4722bc4ef6f3002
Sergei Zimmerman <sergei@zimmerman.foo>
2026-04-03 00:21:31 +0300

    derivation-builder: Don't use copyFile for FOD output copying, put the output in a temporary directory in the store
    Puts the temporary FOD output copies in a temporary directory inside the
    store instead of the (for Linux sandboxed builds) chroot.
    This prevents file overwrite due to symlink following that std::filesystem::copy_file does.
    
    Also applies the same output copying approach for impure derivations that don't
    have network sandboxing and thus are subject to FD smuggling.
    
    Fixes GHSA-g3g9-5vj6-r3gj.
    
    (cherry picked from commit a760af86b3a42aa5ac9d9002929107fe357bf128)

--- src/libstore/include/nix/store/local-store.hh
+++ src/libstore/include/nix/store/local-store.hh
@@ -459,6 +459,8 @@ private:
 
     friend struct PathSubstitutionGoal;
     friend struct DerivationGoal;
+    /* Only used for createTempDirInStore. */
+    friend class DerivationBuilderImpl;
 };
 
 } // namespace nix
