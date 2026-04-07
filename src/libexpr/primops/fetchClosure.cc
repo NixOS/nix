136e1a2c93e153eb05518f4175aa453071a443b6
dramforever <dramforever@live.com>
2026-04-07 15:53:57 +0800

    fetchClosure: Add temproot before checking path
    This avoids the fetched path from getting garbage collected if it is
    already valid.

--- src/libexpr/primops/fetchClosure.cc
+++ src/libexpr/primops/fetchClosure.cc
@@ -23,6 +23,8 @@ static void runFetchClosureWithRewrite(
     const std::optional<StorePath> & toPathMaybe,
     Value & v)
 {
+    if (toPathMaybe)
+        state.store->addTempRoot(*toPathMaybe);
 
     // establish toPath or throw
 
@@ -74,6 +76,7 @@ static void runFetchClosureWithRewrite(
 static void runFetchClosureWithContentAddressedPath(
     EvalState & state, const PosIdx pos, Store & fromStore, const StorePath & fromPath, Value & v)
 {
+    state.store->addTempRoot(fromPath);
 
     if (!state.store->isValidPath(fromPath))
         copyClosure(fromStore, *state.store, RealisedPath::Set{fromPath});
@@ -103,6 +106,7 @@ static void runFetchClosureWithContentAddressedPath(
 static void runFetchClosureWithInputAddressedPath(
     EvalState & state, const PosIdx pos, Store & fromStore, const StorePath & fromPath, Value & v)
 {
+    state.store->addTempRoot(fromPath);
 
     if (!state.store->isValidPath(fromPath))
         copyClosure(fromStore, *state.store, RealisedPath::Set{fromPath});
