58ed3e8056ddf6310197375c58f45235ea6419e8
dramforever <dramforever@live.com>
2026-04-07 15:02:07 +0800

    worker-protocol: Add AddTempRootsFlag to QueryValidPaths
    This asks the QueryValidPaths operation to add temproots on the remote,
    saving roundtrips in RemoteStore::queryValidPaths.

--- src/libstore/remote-store.cc
+++ src/libstore/remote-store.cc
@@ -190,11 +190,8 @@ bool RemoteStore::isValidPathUncached(const StorePath & path)
 StorePathSet RemoteStore::queryValidPaths(
     const StorePathSet & paths, SubstituteFlag maybeSubstitute, AddTempRootsFlag maybeAddTempRoots)
 {
-    if (maybeAddTempRoots)
-        addTempRoots(paths);
-
     auto conn(getConnection());
-    return conn->queryValidPaths(*this, &conn.daemonException, paths, maybeSubstitute);
+    return conn->queryValidPaths(*this, &conn.daemonException, paths, maybeSubstitute, maybeAddTempRoots);
 }
 
 StorePathSet RemoteStore::queryAllValidPaths()
