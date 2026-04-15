828ffb210af7a8d82081217d41b06cbf151a6ed9
Riccardo Mazzarini <me@noib3.dev>
2026-04-15 19:50:47 +0200

    build: embed C API symbols in release binaries

--- tests/functional/plugins.sh
+++ tests/functional/plugins.sh
@@ -2,11 +2,26 @@
 
 source common.sh
 
-for ext in so dylib; do
-    plugin="${_NIX_TEST_BUILD_DIR}/plugins/libplugintest.$ext"
-    [[ -f "$plugin" ]] && break
-done
+findPlugin() {
+    local name="$1"
+
+    for ext in so dylib; do
+        local plugin="${_NIX_TEST_BUILD_DIR}/plugins/${name}.$ext"
+        if [[ -f "$plugin" ]]; then
+            printf '%s\n' "$plugin"
+            return 0
+        fi
+    done
+
+    return 1
+}
+
+plugin=$(findPlugin libplugintest)
 
 res=$(nix --option setting-set true --option plugin-files "$plugin" eval --expr builtins.anotherNull)
 
 [ "$res"x = "nullx" ]
+
+if plugin_dlsym=$(findPlugin libplugintest_dlsym); then
+    nix --option plugin-files "$plugin_dlsym" eval --expr 0 >/dev/null
+fi
