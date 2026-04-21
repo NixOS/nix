b74401e8714151ef0e961ca7cd13bb7e2c9cc316
Sergei Zimmerman <sergei@zimmerman.foo>
2026-04-21 22:07:57 +0300

    Fix functional_root tests
    This check isn't reliable with CAP_DAC_OVERRIDE. It's also not very critical
    anyway.

--- tests/functional/flakes/edit.sh
+++ tests/functional/flakes/edit.sh
@@ -9,4 +9,3 @@ nix edit "$flake1Dir#" | grepQuiet simple.builder.sh
 tar --exclude=".git*" -czf "$TEST_ROOT"/flake1Dir.tar.gz -C "$(dirname "$flake1Dir")" "$(basename "$flake1Dir")"
 # Test that editing a file from a tarball flake works and the file is readonly.
 nix edit "file://$TEST_ROOT/flake1Dir.tar.gz" | grepQuiet simple.builder.sh
-EDITOR='test ! -w' nix edit "file://$TEST_ROOT/flake1Dir.tar.gz"
