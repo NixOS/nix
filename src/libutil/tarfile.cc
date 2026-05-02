cf7b3818a5f0bfed8792180f0a019222aa377e9f
Sergei Zimmerman <sergei@zimmerman.foo>
2026-05-02 23:50:00 +0300

    libutil: Fix path traversal in unpackTarfile
    Fixes GHSA-gr92-w2r5-qw5p. The primary fix is .relative_path() calls, everything
    else is making sure we use native path handling in libarchive.
    
    (cherry picked from commit 94e24ddea9d2e8fb9099a95e8f8c0ea4f9d9bff7)

--- src/libutil/tarfile.cc
+++ src/libutil/tarfile.cc
@@ -5,6 +5,7 @@
 #include "nix/util/serialise.hh"
 #include "nix/util/tarfile.hh"
 #include "nix/util/file-system.hh"
+#include "nix/util/os-string.hh"
 
 namespace nix {
 
@@ -127,7 +128,13 @@ TarArchive::~TarArchive()
         archive_read_free(this->archive);
 }
 
-static void extract_archive(TarArchive & archive, const fs::path & destDir)
+#ifndef _WIN32
+#  define NIX_LIBARCHIVE_NATIVE_PATH_FUNC(func) func
+#else
+#  define NIX_LIBARCHIVE_NATIVE_PATH_FUNC(func) func##_w
+#endif
+
+static void extract_archive(TarArchive & archive, const std::filesystem::path & destDir)
 {
     int flags = ARCHIVE_EXTRACT_TIME | ARCHIVE_EXTRACT_SECURE_SYMLINKS | ARCHIVE_EXTRACT_SECURE_NODOTDOT;
 
@@ -136,24 +143,34 @@ static void extract_archive(TarArchive & archive, const fs::path & destDir)
         int r = archive_read_next_header(archive.archive, &entry);
         if (r == ARCHIVE_EOF)
             break;
-        auto name = archive_entry_pathname(entry);
-        if (!name)
-            throw Error("cannot get archive member name: %s", archive_error_string(archive.archive));
-        if (r == ARCHIVE_WARN)
-            warn(archive_error_string(archive.archive));
-        else
-            archive.check(r);
 
-        archive_entry_copy_pathname(entry, (destDir / name).string().c_str());
+        const auto relPath = [&]() -> std::filesystem::path {
+            /* Some archives might lack a pathname https://github.com/libarchive/libarchive/issues/2089. */
+            auto * name = NIX_LIBARCHIVE_NATIVE_PATH_FUNC(archive_entry_pathname)(entry);
+            if (!name)
+                throw Error("cannot get archive member name: %s", archive_error_string(archive.archive));
+            if (r == ARCHIVE_WARN)
+                warn(
+                    "getting archive member '%1%': %2%",
+                    os_string_to_string(OsStringView(name)),
+                    archive_error_string(archive.archive));
+            else
+                archive.check(r);
+
+            return std::filesystem::path(name).relative_path();
+        }();
+
+        NIX_LIBARCHIVE_NATIVE_PATH_FUNC(archive_entry_copy_pathname)(entry, (destDir / relPath).c_str());
 
         // sources can and do contain dirs with no rx bits
         if (archive_entry_filetype(entry) == AE_IFDIR && (archive_entry_mode(entry) & 0500) != 0500)
             archive_entry_set_mode(entry, archive_entry_mode(entry) | 0500);
 
         // Patch hardlink path
-        const char * original_hardlink = archive_entry_hardlink(entry);
-        if (original_hardlink) {
-            archive_entry_copy_hardlink(entry, (destDir / original_hardlink).string().c_str());
+        const auto * originalHardlink = NIX_LIBARCHIVE_NATIVE_PATH_FUNC(archive_entry_hardlink)(entry);
+        if (originalHardlink) {
+            auto hardlinkPath = std::filesystem::path(originalHardlink).relative_path();
+            NIX_LIBARCHIVE_NATIVE_PATH_FUNC(archive_entry_copy_hardlink)(entry, (destDir / hardlinkPath).c_str());
         }
 
         archive.check(archive_read_extract(archive.archive, entry, flags));
@@ -162,13 +179,7 @@ static void extract_archive(TarArchive & archive, const fs::path & destDir)
     archive.close();
 }
 
-void unpackTarfile(Source & source, const fs::path & destDir)
-{
-    auto archive = TarArchive(source);
-
-    createDirs(destDir);
-    extract_archive(archive, destDir);
-}
+#undef NIX_LIBARCHIVE_NATIVE_PATH_FUNC
 
 void unpackTarfile(const fs::path & tarFile, const fs::path & destDir)
 {
