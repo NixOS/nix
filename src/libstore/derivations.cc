faa09a784fe517b219fe0c92c6168177211b0af5
Ben Siraphob <bensiraphob@gmail.com>
2026-05-04 15:04:33 -0700

    libstore: Speed up printString in Derivation::unparse
    BM_UnparseRealDerivationFile:
      hello:    1,744 ns ->  1,456 ns (-16.5%)
      firefox: 33,967 ns -> 33,344 ns (-1.8%)

--- src/libstore/derivations.cc
+++ src/libstore/derivations.cc
@@ -524,32 +524,46 @@ Derivation parseDerivation(
  */
 static void printString(std::string & res, std::string_view s)
 {
-    res += '"';
-    static constexpr auto chunkSize = 1024;
-    std::array<char, 2 * chunkSize + 2> buffer;
-    while (!s.empty()) {
-        auto chunk = s.substr(0, /*n=*/chunkSize);
-        s.remove_prefix(chunk.size());
-        char * buf = buffer.data();
-        char * p = buf;
-        for (auto c : chunk)
-            if (c == '\"' || c == '\\') {
-                *p++ = '\\';
-                *p++ = c;
-            } else if (c == '\n') {
-                *p++ = '\\';
+    /* Bulk-memcpy unescaped runs and handle escapes inline; write straight
+       into res via resize_and_overwrite (no zero-fill, no scratch buffer). */
+    auto oldSize = res.size();
+    res.resize_and_overwrite(oldSize + s.size() * 2 + 2, [&](char * data, size_t) -> size_t {
+        char * p = data + oldSize;
+        *p++ = '"';
+        while (!s.empty()) {
+            size_t i = 0;
+            while (i < s.size()) {
+                char c = s[i];
+                if (c == '"' || c == '\\' || c == '\n' || c == '\r' || c == '\t')
+                    break;
+                ++i;
+            }
+            if (i > 0) {
+                std::memcpy(p, s.data(), i);
+                p += i;
+            }
+            if (i == s.size())
+                break;
+            *p++ = '\\';
+            switch (s[i]) {
+            case '\n':
                 *p++ = 'n';
-            } else if (c == '\r') {
-                *p++ = '\\';
+                break;
+            case '\r':
                 *p++ = 'r';
-            } else if (c == '\t') {
-                *p++ = '\\';
+                break;
+            case '\t':
                 *p++ = 't';
-            } else
-                *p++ = c;
-        res.append(buf, p - buf);
-    }
-    res += '"';
+                break;
+            default:
+                *p++ = s[i];
+                break;
+            }
+            s.remove_prefix(i + 1);
+        }
+        *p++ = '"';
+        return p - data;
+    });
 }
 
 static void printUnquotedString(std::string & res, std::string_view s)
