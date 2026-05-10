a76001b47f633a0ea072ca5d43f360147202c487
Sergei Zimmerman <sergei@zimmerman.foo>
2026-05-11 01:28:30 +0300

    libfetchers/git-utils: Clean up nix-git-submodules temp file

--- src/libfetchers/git-utils.cc
+++ src/libfetchers/git-utils.cc
@@ -1463,6 +1463,7 @@ std::vector<std::tuple<GitRepoImpl::Submodule, Hash>> GitRepoImpl::getSubmodules
     auto configS = accessor->readFile(modulesFile);
 
     auto [fdTemp, pathTemp] = createTempFile("nix-git-submodules");
+    AutoDelete delTemp(pathTemp, /*recursive=*/false);
     try {
         writeFull(fdTemp.get(), configS);
     } catch (SystemError & e) {
@@ -1481,6 +1482,7 @@ std::vector<std::tuple<GitRepoImpl::Submodule, Hash>> GitRepoImpl::getSubmodules
             result.push_back({std::move(submodule), *rev});
     }
 
+    delTemp.deletePath();
     return result;
 }
 
