351706a90914c425775752fe80f6bdc8b38d0d07
Eelco Dolstra <edolstra@gmail.com>
2026-05-27 17:19:29 +0200

    Use the unpolluted outputs

--- packaging/secure-packages/flake.nix
+++ packaging/secure-packages/flake.nix
@@ -2,5 +2,5 @@
   inputs.nix.url = "../..";
   inputs.nix.inputs.nixpkgs.url = "https://flakehub.com/f/DeterminateSystems/secure-packages-25.11/0";
 
-  outputs = { self, nix }: nix;
+  outputs = { self, nix }: nix.outputs;
 }
