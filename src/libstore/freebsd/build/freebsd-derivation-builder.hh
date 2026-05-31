65f15363415146e9cf97959548618ae8460e6157
Sergei Zimmerman <sergei@zimmerman.foo>
2026-05-31 14:08:34 +0300

    Fix freebsd build

--- src/libstore/freebsd/build/freebsd-derivation-builder.hh
+++ src/libstore/freebsd/build/freebsd-derivation-builder.hh
@@ -7,6 +7,13 @@ namespace nix {
 struct FreeBSDDerivationBuilder : virtual DerivationBuilderImpl
 {
     using DerivationBuilderImpl::DerivationBuilderImpl;
+
+    FreeBSDDerivationBuilder(FreeBSDDerivationBuilder &&) = delete;
+    FreeBSDDerivationBuilder(const FreeBSDDerivationBuilder &) = delete;
+    FreeBSDDerivationBuilder & operator=(FreeBSDDerivationBuilder &&) = delete;
+    FreeBSDDerivationBuilder & operator=(const FreeBSDDerivationBuilder &) = delete;
+    /* To appease Wweak-vtables. */
+    ~FreeBSDDerivationBuilder() override;
 };
 
 } // namespace nix
