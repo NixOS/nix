b104a21b59e1b4621196ca72fdab935308037432
Eelco Dolstra <edolstra@gmail.com>
2026-06-03 16:27:16 +0200

    copyPaths(): Register temp roots on the destination store
    Note: this only prevents the paths from being GCed during the
    copy. You still need to use `nix copy --out-link ...` to prevent GC
    afterwards.
    
    Less ambitious fix than https://github.com/NixOS/nix/pull/15719.

--- src/libstore/store-api.cc
+++ src/libstore/store-api.cc
@@ -1029,6 +1029,9 @@ std::map<StorePath, StorePath> copyPaths(
     CheckSigsFlag checkSigs,
     SubstituteFlag substitute)
 {
+    for (auto & path : storePaths)
+        dstStore.addTempRoot(path);
+
     auto valid = dstStore.queryValidPaths(storePaths, substitute);
 
     StorePathSet missing;
