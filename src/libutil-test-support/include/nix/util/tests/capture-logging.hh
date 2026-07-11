98a3bcd44b721d91436ae7d56511ef1558737fff
Sergei Zimmerman <sergei@zimmerman.foo>
2026-07-12 01:02:04 +0300

    libutil-tests: Factor out CaptureLogging, make more tests capture warning output
    This is certainly useful in more places. libutil-tests was being too
    loud with warnings, so that's my motivation for doing this. Ideally we'd
    test that the intended warnings are produced too, but that can be done
    in a follow-up.

---
+++ src/libutil-test-support/include/nix/util/tests/capture-logging.hh
@@ -0,0 +1,61 @@
+#pragma once
+
+#include "nix/util/logging.hh"
+
+#include <sstream>
+
+namespace nix::testing {
+
+class CaptureLogger : public Logger
+{
+    std::ostringstream oss;
+
+public:
+    CaptureLogger() {}
+
+    std::string get() const
+    {
+        return oss.str();
+    }
+
+    void log(Verbosity lvl, std::string_view s) override
+    {
+        oss << s << std::endl;
+    }
+
+    void logEI(const ErrorInfo & ei) override
+    {
+        showErrorInfo(oss, ei, loggerSettings.showTrace.get());
+    }
+};
+
+class CaptureLogging
+{
+    std::unique_ptr<CaptureLogger> logger;
+    Logger * oldLogger;
+
+public:
+    CaptureLogging()
+    {
+        oldLogger = nix::logger;
+        logger = std::make_unique<CaptureLogger>();
+        nix::logger = logger.get();
+    }
+
+    std::string get() const
+    {
+        return logger->get();
+    }
+
+    CaptureLogging(CaptureLogging &&) = delete;
+    CaptureLogging(const CaptureLogging &) = delete;
+    CaptureLogging & operator=(CaptureLogging &&) = delete;
+    CaptureLogging & operator=(const CaptureLogging &) = delete;
+
+    ~CaptureLogging()
+    {
+        nix::logger = oldLogger;
+    }
+};
+
+} // namespace nix::testing
