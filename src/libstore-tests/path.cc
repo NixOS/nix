40c62739648f9204c26c13579c455d68154318b3
Sergei Zimmerman <sergei@zimmerman.foo>
2026-08-04 00:05:20 +0300

    libstore: Actually check that hash part is followed by a dash in StorePath
    This was missing validation and meant that potentially any byte could
    be included after the dash (even NUL one - though it probably would have
    segfaulted earlier due to std::filesystem::path). The latter is also
    very much necessary to fix, but let's start here.
    
    (cherry picked from commit 089ac360af56c8812546de6b93af687ab723d8a6)

--- src/libstore-tests/path.cc
+++ src/libstore-tests/path.cc
@@ -139,6 +139,15 @@ RC_GTEST_FIXTURE_PROP(StorePathTest, prop_check_regex_eq_parse, ())
     RC_ASSERT(parsed == std::regex_match(std::string{name}, nameRegex));
 }
 
+TEST_F(StorePathTest, mustBeDashAfterHashPart)
+{
+    std::string hashPart = "575s52sh487i0ylmbs9pvi606ljdszr0";
+    for (char c : {'_', '\0'}) {
+        EXPECT_THROW(StorePath(hashPart + c + "name"), BadStorePath);
+    }
+    EXPECT_NO_THROW(StorePath(hashPart + "-" + "name"));
+}
+
 /* ----------------------------------------------------------------------------
  * JSON
  * --------------------------------------------------------------------------*/
